0d9e53197deeb8ef6a91bb441aa6b4dc7ac6d713
Marcel Bochtler <marcel.bochtler@bosch.io>
2022-08-18 12:20:10 +0200

    Docker: Add KeyCloak to the compose configuration
    The entrypoint is changed to a custom script which imports a realm with
    some default settings:
    * A realm called `master`.
    * An administrative user with username `admin` and password `admin`.
    * A client called `ort-server`.
    * A client role called `FULL_ACCESS`.
    * And a user with the username `ort-admin`, with the `FULL_ACCESS` role
      assigned.
    
    Signed-off-by: Marcel Bochtler <marcel.bochtler@bosch.io>

---
+++ scripts/docker/keycloak/init-keycloak.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Copyright (C) 2022 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+# License-Filename: LICENSE
+
+# This script is used in a docker compose setup to initialize default KeyCloak entities.
+
+set -eu
+
+# Import the preconfigured realm.
+# To add new entities, do the following:
+# 1. Start the container.
+# 2. Get a shell in the container using `docker exec -it ort-server-keycloak-1 /bin/bash`.
+# 3. Configure the new entities in the admin UI: `http://localhost:8081/admin` (username: `admin`, password: `admin`).
+# 4. Export the configuration:
+#    `KC_HTTP_PORT=8081 /opt/keycloak/bin/kc.sh export --dir /opt/keycloak_init --users realm_file
+/opt/keycloak/bin/kc.sh build
+/opt/keycloak/bin/kc.sh import --file /opt/keycloak_init/master-realm.json
+
+# Start KeyCloak
+/opt/keycloak/bin/kc.sh "$@"
