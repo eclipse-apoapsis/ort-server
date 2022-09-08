4cf94471dadf6532326044da7ddb28bc4c3e197c
Marcel Bochtler <marcel.bochtler@bosch.io>
2022-09-08 10:40:10 +0200

    Serialization: Use the injectable `Json` for `ContentNegotiation`
    This ensures that the same `Json` configuration is being used for request
    deserialization as the configuration used for injected `Json` instances.
    
    Signed-off-by: Marcel Bochtler <marcel.bochtler@bosch.io>

--- core/src/main/kotlin/plugins/Serialization.kt
+++ core/src/main/kotlin/plugins/Serialization.kt
@@ -19,13 +19,20 @@
 
 package org.ossreviewtoolkit.server.core.plugins
 
-import io.ktor.serialization.kotlinx.json.json
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.serialization
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 
+import kotlinx.serialization.json.Json
+
+import org.koin.ktor.ext.inject
+
 fun Application.configureSerialization() {
+    val json: Json by inject()
+
     install(ContentNegotiation) {
-        json()
+        serialization(ContentType.Application.Json, json)
     }
 }
