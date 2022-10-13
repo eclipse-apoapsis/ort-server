f6beefe1668811ba2449b9ba19e7216e7396f21d
Oliver Heger <oliver.heger@bosch.io>
2022-10-13 12:23:36 +0200

    Transport: Add a JsonSerializer class
    Many concrete transport layer implementations are expected to use JSON
    as an interchange format for messages. Such implementations can make
    use of this serializer implementation, which also hides away some
    quirks of Kotlin serialization.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ transport/spi/src/main/kotlin/json/JsonSerializer.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2022 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.transport.json
+
+import kotlin.reflect.KClass
+
+import kotlinx.serialization.InternalSerializationApi
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.serializer
+
+/**
+ * An interface supporting the JSON serialization and deserialization of a specific type.
+ *
+ * The main purpose of this interface is to abstract away from some details of Kotlin serialization that arise when
+ * dealing with arbitrary types.
+ */
+interface JsonSerializer<T> {
+    companion object {
+        /**
+         * Return an instance of [JsonSerializer] that can handle instances of the provided data type.
+         */
+        inline fun <reified T : Any> forType(): JsonSerializer<T> = forClass(T::class)
+
+        /**
+         * Return an instance of [JsonSerializer] that can handle instances of the provided [class][clazz].
+         */
+        @OptIn(InternalSerializationApi::class)
+        fun <T : Any> forClass(clazz: KClass<T>): JsonSerializer<T> {
+            val serializer = clazz.serializer()
+
+            return object : JsonSerializer<T> {
+                override fun toJson(obj: T): String = Json.encodeToString(serializer, obj)
+
+                override fun fromJson(json: String): T {
+                    return Json.decodeFromString(serializer, json)
+                }
+            }
+        }
+    }
+
+    /**
+     * Generate a JSON string representation for [obj].
+     */
+    fun toJson(obj: T): String
+
+    /**
+     * Deserialize the given [json] string to an object of the managed type.
+     */
+    fun fromJson(json: String): T
+}
---
+++ transport/spi/src/test/kotlin/json/JsonSerializerTest.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2022 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.transport.json
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+import kotlinx.serialization.Serializable
+
+class JsonSerializerTest : StringSpec({
+    "Serialization should work" {
+        val data = TestStringData("some test data")
+        val serializer = JsonSerializer.forType<TestData>()
+
+        val json = serializer.toJson(data)
+        val data2 = serializer.fromJson(json)
+
+        data2 shouldBe data
+    }
+
+    "Serialization should work over all sealed classes" {
+        val data = TestIntData(42)
+        val serializer = JsonSerializer.forType<TestData>()
+
+        val json = serializer.toJson(data)
+        val data2 = serializer.fromJson(json)
+
+        data2 shouldBe data
+    }
+})
+
+@Serializable
+internal sealed class TestData
+
+@Serializable
+internal data class TestStringData(val data: String) : TestData()
+
+@Serializable
+internal data class TestIntData(val data: Int) : TestData()
