20b6011e30f428c34f1d4d9a3f7dbb5bc099acd8
Oliver Heger <oliver.heger@bosch.io>
2022-10-14 09:21:38 +0200

    Model: Add classes representing messages to the Orchestrator
    The classes define the API of the Orchestrator component. They
    represent the messages to trigger a new analysis run and the results
    that can be produced by the single workers.
    
    Since the workers are not yet implemented, the message classes are
    incomplete, and also their concrete payload still has to be defined.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ model/src/commonMain/kotlin/orchestrator/OrchestratorMessage.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2022 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.model.orchestrator
+
+import kotlinx.serialization.Serializable
+
+/**
+ * Base class for the hierarchy of messages that can be processed by the Orchestrator component.
+ */
+@Serializable
+sealed class OrchestratorMessage
+
+/**
+ * A message requesting an analysis run on a specific repository.
+ *
+ * TODO: The exact payload still has to be defined.
+ */
+@Serializable
+data class AnalyzeRepository(
+    val repositoryId: Long
+) : OrchestratorMessage()
+
+/**
+ * A message notifying the Orchestrator about a result produced by the Analyzer Worker.
+ *
+ * TODO: The exact payload still has to be defined.
+ */
+@Serializable
+data class AnalyzeResult(
+    val runId: Long
+) : OrchestratorMessage()
