8f20884f7ea721425b0dc983c8d6c906d03bf3a6
Oliver Heger <oliver.heger@bosch.io>
2022-10-17 14:42:46 +0200

    Transport: Add a MessageReceiverFactory interface
    This interface makes it possible to connect an endpoint to a concrete
    infrastructure for receiving messages.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ transport/spi/src/main/kotlin/MessageReceiverFactory.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2022 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.transport
+
+import com.typesafe.config.Config
+
+import java.util.ServiceLoader
+
+import org.slf4j.LoggerFactory
+
+/**
+ * Definition of a message handler function of an endpoint. The function is passed the message it should handle.
+ */
+typealias EndpointHandler<T> = (Message<T>) -> Unit
+
+/**
+ * Factory interface for setting up a receiver for an [Endpoint].
+ *
+ * Via this interface endpoints can be implemented that are independent on a concrete message transport mechanism. The
+ * idea is that the endpoint implementation just provides an [EndpointHandler] function. The factory constructs the
+ * necessary infrastructure to receive messages (e.g. by registering a listener on a message queue or starting an
+ * HTTP endpoint). When a message is received the infrastructure calls the [EndpointHandler], so that it can be
+ * processed. Note that no direct interactions are done with this infrastructure; therefore, there is no explicit
+ * _MessageReceiver_ type.
+ *
+ * A ServiceLoader is used to select the concrete factory implementation dynamically based on the configuration
+ * settings of the affected [Endpoint].
+ */
+interface MessageReceiverFactory {
+    companion object {
+        /**
+         * The configuration property that defines the type of the receiver implementation to be used. To create a
+         * receiver, a [MessageReceiverFactory] with a name matching the value of this configuration property is
+         * looked up.
+         */
+        const val RECEIVER_TYPE_PROPERTY = "sender.type"
+
+        /** The service loader to load [MessageReceiverFactory] implementations. */
+        private val LOADER = ServiceLoader.load(MessageReceiverFactory::class.java)
+
+        private val log = LoggerFactory.getLogger(MessageReceiverFactory::class.java)
+
+        /**
+         * Set up infrastructure to process messages for the given [endpoint] with the given [handler] function based
+         * on the provided [config]. Find the [MessageReceiverFactory] configured for this endpoint in the
+         * [RECEIVER_TYPE_PROPERTY] property via the Java Service Loader mechanism. Then create an instance using this
+         * factory.
+         */
+        fun <T : Any> createReceiver(endpoint: Endpoint<T>, config: Config, handler: EndpointHandler<T>) {
+            val factoryName =
+                config.getString("${endpoint.configPrefix}.$RECEIVER_TYPE_PROPERTY")
+            log.info("Setting up a MessageReceiver of type '{}' for endpoint '{}'.", factoryName, endpoint.configPrefix)
+
+            val factory = checkNotNull(LOADER.find { it.name == factoryName }) {
+                "No MessageReceiverFactory with name '$factoryName' found on classpath."
+            }
+
+            factory.createReceiver(endpoint, config, handler)
+        }
+    }
+
+    /**
+     * A unique name of this factory. Via this name a concrete sender implementation can be selected in the server's
+     * configuration.
+     */
+    val name: String
+
+    /**
+     * Set up infrastructure that is able to receive messages to the given [endpoint] and calls the provided [handler]
+     * function with the received messages. Use [config] as source of configuration settings for this infrastructure.
+     */
+    fun <T : Any> createReceiver(endpoint: Endpoint<T>, config: Config, handler: EndpointHandler<T>)
+}
---
+++ transport/spi/src/test/kotlin/MessageReceiverFactoryTest.kt
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2022 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.transport
+
+import com.typesafe.config.Config
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.contain
+
+import io.mockk.every
+import io.mockk.mockk
+
+import org.ossreviewtoolkit.server.model.orchestrator.OrchestratorMessage
+
+class MessageReceiverFactoryTest : StringSpec({
+    afterAny {
+        MessageReceiverFactoryForTesting.reset()
+    }
+
+    "The correct factory should be invoked" {
+        val handler = mockk<EndpointHandler<OrchestratorMessage>>()
+        val config = mockk<Config>()
+        every {
+            config.getString("orchestrator.${MessageReceiverFactory.RECEIVER_TYPE_PROPERTY}")
+        } returns TEST_FACTORY_NAME
+
+        MessageReceiverFactory.createReceiver(OrchestratorEndpoint, config, handler)
+
+        MessageReceiverFactoryForTesting.createdEndpoint shouldBe OrchestratorEndpoint
+        MessageReceiverFactoryForTesting.createdConfig shouldBe config
+        MessageReceiverFactoryForTesting.createdHandler shouldBe handler
+    }
+
+    "An exception should be thrown for a non-existing MessageReceiverFactory" {
+        val invalidFactoryName = "a non existing message receiver factory"
+        val config = mockk<Config>()
+        every {
+            config.getString("analyzer.${MessageReceiverFactory.RECEIVER_TYPE_PROPERTY}")
+        } returns invalidFactoryName
+
+        val exception = shouldThrow<IllegalStateException> {
+            MessageReceiverFactory.createReceiver(AnalyzerEndpoint, config, mockk())
+        }
+
+        exception.message should contain(invalidFactoryName)
+    }
+})
+
+/** The name reported by the test receiver factory. */
+private const val TEST_FACTORY_NAME = "testMessageReceiverFactory"
+
+/**
+ * A test [MessageReceiverFactory] implementation that is referenced by test cases.
+ */
+class MessageReceiverFactoryForTesting : MessageReceiverFactory {
+    companion object {
+        /** Stores the [Endpoint] passed to [createReceiver]. */
+        var createdEndpoint: Endpoint<*>? = null
+
+        /** Stores the config passed to [createReceiver]. */
+        var createdConfig: Config? = null
+
+        /** Stores the handler function passed to [createReceiver]. */
+        var createdHandler: Any? = null
+
+        /**
+         * Reset the properties passed from the last [createReceiver] call.
+         */
+        fun reset() {
+            createdEndpoint = null
+            createdConfig = null
+            createdHandler = null
+        }
+    }
+
+    override val name: String = TEST_FACTORY_NAME
+
+    override fun <T : Any> createReceiver(endpoint: Endpoint<T>, config: Config, handler: EndpointHandler<T>) {
+        check(createdEndpoint == null) { "Too many invocations of createReceiver." }
+
+        createdEndpoint = endpoint
+        createdConfig = config
+        createdHandler = handler
+    }
+}
