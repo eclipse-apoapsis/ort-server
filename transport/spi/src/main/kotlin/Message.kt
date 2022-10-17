afd93c476755a60571b8923cc7fda2ed1e35ed1e
Oliver Heger <oliver.heger@bosch.io>
2022-10-17 07:56:44 +0200

    Transport: Add classes that represent messages
    The Orchestrator and the workers are going to use instances of these
    classes to exchange data.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ transport/spi/src/main/kotlin/Message.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2022 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.transport
+
+/**
+ * A data class representing the header of messages passed between internal ORT server endpoints. Via the properties
+ * defined here, additional metadata about messages is provided.
+ */
+data class MessageHeader(
+    /**
+     * The access token associated with the current request. This property can be used to get information about the
+     * caller who triggered this request.
+     */
+    val token: String,
+
+    /**
+     * An identifier for the current request. This purpose of this string is to allow a correlation of multiple
+     * messages that are exchanged to handle a single request.
+     */
+    val traceId: String
+)
+
+/**
+ * A data class to describe the messages exchanged between the internal ORT server endpoints. Each endpoint can
+ * handle messages of a specific type.
+ */
+data class Message<out T>(
+    /** The header of this message defining additional metadata. */
+    val header: MessageHeader,
+
+    /** The actual payload of this message. */
+    val payload: T
+)
