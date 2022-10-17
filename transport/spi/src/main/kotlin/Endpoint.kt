e2919fe5ba85b935fd53fbabb248776ce9f9dc29
Oliver Heger <oliver.heger@bosch.io>
2022-10-17 07:42:13 +0200

    Transport: Add classes describing supported endpoints
    This is a hierarchy of sealed classes where each member represents an
    endpoint related to the Orchestrator and the workers. The classes are
    used in later commits to set up the communication infrastructure
    between these endpoints.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ transport/spi/src/main/kotlin/Endpoint.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2022 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.transport
+
+import kotlin.reflect.KClass
+
+import org.ossreviewtoolkit.server.model.orchestrator.AnalyzeRequest
+import org.ossreviewtoolkit.server.model.orchestrator.OrchestratorMessage
+
+/**
+ * A type describing the different endpoints supported by the ORT server.
+ *
+ * This type and its concrete subtypes are used to define the targets for the internal communication between the
+ * Orchestrator component and the different workers. Via the properties defined by the single members of this
+ * hierarchy, type-safe serialization of messages is possible.
+ */
+sealed class Endpoint<T : Any>(
+    /**
+     * The class of messages this endpoint can handle. Based on this property, a proper serialization infrastructure
+     * can be set up.
+     */
+    val messageClass: KClass<T>,
+
+    /**
+     * A prefix used for configuration properties related to this endpoint. This is used to configure concrete
+     * implementations of message senders and receivers that need to interact with this endpoint.
+     */
+    val configPrefix: String
+)
+
+/**
+ * A concrete [Endpoint] declaration representing the Orchestrator endpoint.
+ */
+object OrchestratorEndpoint : Endpoint<OrchestratorMessage>(OrchestratorMessage::class, "orchestrator")
+
+/**
+ * A concrete [Endpoint] declaration representing the Analyzer worker.
+ */
+object AnalyzerEndpoint : Endpoint<AnalyzeRequest>(AnalyzeRequest::class, "analyzer")
