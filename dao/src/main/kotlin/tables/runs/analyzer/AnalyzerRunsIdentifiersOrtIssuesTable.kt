6a250f4c2506b9e8e4bbd42a15d6604074d2968c
Onur Demirci <onur.demirci@bosch.io>
2022-10-25 15:22:00 +0200

    dao: Implement the dao classes for ort issues for an analyzer run
    Signed-off-by: Onur Demirci <onur.demirci@bosch.io>

---
+++ dao/src/main/kotlin/tables/runs/analyzer/AnalyzerRunsIdentifiersOrtIssuesTable.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2022 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.dao.tables.runs.analyzer
+
+import org.jetbrains.exposed.sql.Table
+
+import org.ossreviewtoolkit.server.dao.tables.runs.shared.IdentifiersOrtIssuesTable
+
+/**
+ * An intermediate table to store references from [AnalyzerRunsTable] and [IdentifiersOrtIssuesTable].
+ */
+object AnalyzerRunsIdentifiersOrtIssuesTable : Table("analyzer_runs_identifiers_ort_issues") {
+    val analyzerRun = reference("analyzer_run_id", AnalyzerRunsTable)
+    val identifierOrtIssue = reference("identifier_ort_issue_id", IdentifiersOrtIssuesTable)
+
+    override val primaryKey: PrimaryKey
+        get() = PrimaryKey(analyzerRun, identifierOrtIssue, name = "pk_analyzer_run_identifier_ort_issue")
+}
---
+++ dao/src/main/kotlin/tables/runs/shared/OrtIssuesTable.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2022 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.dao.tables.runs.shared
+
+import org.jetbrains.exposed.dao.LongEntity
+import org.jetbrains.exposed.dao.LongEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.LongIdTable
+import org.jetbrains.exposed.sql.kotlin.datetime.timestamp
+
+import org.ossreviewtoolkit.server.dao.utils.toDatabasePrecision
+
+/**
+ * A table to represent an ort issue.
+ */
+object OrtIssuesTable : LongIdTable("ort_issues") {
+    val timeStamp = timestamp("timestamp")
+    val issueSource = text("source")
+    val message = text("message")
+    val severity = text("severity")
+}
+
+class OrtIssueDao(id: EntityID<Long>) : LongEntity(id) {
+    companion object : LongEntityClass<OrtIssueDao>(OrtIssuesTable)
+
+    var timeStamp by OrtIssuesTable.timeStamp.transform({ it.toDatabasePrecision() }, { it })
+    var issueSource by OrtIssuesTable.issueSource
+    var message by OrtIssuesTable.message
+    var severity by OrtIssuesTable.severity
+}
