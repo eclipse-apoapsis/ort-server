93fdbd5687fa57bac0546c07b0059b831690703b
Onur Demirci <onur.demirci@bosch.io>
2022-10-28 09:58:45 +0200

    model: Add all needed model classes for an analyzer run
    The model classes for an analyzer run are needed for the mapping from the dao
    classes to business model classes. There is not a business model class for
    every dao class, because some represent a map or list with primitive data
    types. The reason why the ORT model classes were not used is:
    1. To have a stable API and not to adapt the database with every change in ORT
    2. To access the database entries somehow, their IDs have to be stored.
    
    Signed-off-by: Onur Demirci <onur.demirci@bosch.io>

---
+++ model/src/commonMain/kotlin/runs/PackageCurationData.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2022 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.model.runs
+
+data class PackageCurationData(
+    val id: Long,
+    val comment: String? = null,
+    val purl: String? = null,
+    val cpe: String? = null,
+    val authors: Set<Author>? = null,
+    val concludedLicense: LicenseSpdx? = null,
+    val description: String? = null,
+    val homepageUrl: String? = null,
+    val binaryArtifact: RemoteArtifact? = null,
+    val sourceArtifact: RemoteArtifact? = null,
+    val vcs: CurationVcsInfo? = null,
+    val isMetadataOnly: Boolean? = null,
+    val isModified: Boolean? = null
+)
