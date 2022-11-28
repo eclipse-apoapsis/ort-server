0bc7255ec41e13440482f351dc36f710fa9cc240
Martin Nonnenmacher <martin.nonnenmacher@bosch.io>
2022-11-28 13:09:19 +0100

    dao: Add a unique constraint for issues associated to identifiers
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.io>

--- dao/src/main/resources/db/migration/V9__analyzerIssues.sql
+++ dao/src/main/resources/db/migration/V9__analyzerIssues.sql
@@ -13,7 +13,9 @@ CREATE TABLE identifiers_ort_issues
 (
     id            bigserial PRIMARY KEY,
     identifier_id bigint REFERENCES identifiers NOT NULL,
-    ort_issue_id  bigint REFERENCES ort_issues  NOT NULL
+    ort_issue_id  bigint REFERENCES ort_issues  NOT NULL,
+
+    UNIQUE (identifier_id, ort_issue_id)
 );
 
 CREATE TABLE analyzer_runs_identifiers_ort_issues
