5dc2c5e0202fcb75290b3bc45582323b59029ff0
Onur Demirci <onur.demirci@bosch.io>
2022-12-16 15:30:54 +0100

    dao: Implement `DaoAdvisorRunRepository` to persist advisor runs
    Signed-off-by: Onur Demirci <onur.demirci@bosch.io>

---
+++ dao/src/main/kotlin/repositories/DaoAdvisorRunRepository.kt
@@ -0,0 +1,203 @@
+/*
+ * Copyright (C) 2022 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.dao.repositories
+
+import kotlinx.datetime.Instant
+
+import org.jetbrains.exposed.sql.SizedCollection
+
+import org.ossreviewtoolkit.server.dao.blockingQuery
+import org.ossreviewtoolkit.server.dao.tables.AdvisorJobDao
+import org.ossreviewtoolkit.server.dao.tables.runs.advisor.AdvisorConfigurationDao
+import org.ossreviewtoolkit.server.dao.tables.runs.advisor.AdvisorConfigurationsOptionDao
+import org.ossreviewtoolkit.server.dao.tables.runs.advisor.AdvisorResultDao
+import org.ossreviewtoolkit.server.dao.tables.runs.advisor.AdvisorRunDao
+import org.ossreviewtoolkit.server.dao.tables.runs.advisor.AdvisorRunIdentifierDao
+import org.ossreviewtoolkit.server.dao.tables.runs.advisor.AdvisorRunsTable
+import org.ossreviewtoolkit.server.dao.tables.runs.advisor.DefectDao
+import org.ossreviewtoolkit.server.dao.tables.runs.advisor.DefectLabelDao
+import org.ossreviewtoolkit.server.dao.tables.runs.advisor.GithubDefectsConfigurationDao
+import org.ossreviewtoolkit.server.dao.tables.runs.advisor.NexusIqConfigurationDao
+import org.ossreviewtoolkit.server.dao.tables.runs.advisor.OsvConfigurationDao
+import org.ossreviewtoolkit.server.dao.tables.runs.advisor.VulnerabilityDao
+import org.ossreviewtoolkit.server.dao.tables.runs.advisor.VulnerabilityReferenceDao
+import org.ossreviewtoolkit.server.dao.tables.runs.advisor.VulnerableCodeConfigurationDao
+import org.ossreviewtoolkit.server.dao.utils.getOrPutEnvironment
+import org.ossreviewtoolkit.server.dao.utils.getOrPutIdentifier
+import org.ossreviewtoolkit.server.dao.utils.getOrPutIssue
+import org.ossreviewtoolkit.server.model.repositories.AdvisorRunRepository
+import org.ossreviewtoolkit.server.model.runs.Environment
+import org.ossreviewtoolkit.server.model.runs.Identifier
+import org.ossreviewtoolkit.server.model.runs.advisor.AdvisorConfiguration
+import org.ossreviewtoolkit.server.model.runs.advisor.AdvisorResult
+import org.ossreviewtoolkit.server.model.runs.advisor.AdvisorRun
+import org.ossreviewtoolkit.server.model.runs.advisor.Defect
+import org.ossreviewtoolkit.server.model.runs.advisor.GithubDefectsConfiguration
+import org.ossreviewtoolkit.server.model.runs.advisor.NexusIqConfiguration
+import org.ossreviewtoolkit.server.model.runs.advisor.OsvConfiguration
+import org.ossreviewtoolkit.server.model.runs.advisor.Vulnerability
+import org.ossreviewtoolkit.server.model.runs.advisor.VulnerableCodeConfiguration
+
+/**
+ * An implementation of [AdvisorRunRepository] that stores the advisor runs in [AdvisorRunsTable].
+ */
+class DaoAdvisorRunRepository : AdvisorRunRepository {
+    override fun create(
+        advisorJobId: Long,
+        startTime: Instant,
+        endTime: Instant,
+        environment: Environment,
+        config: AdvisorConfiguration,
+        advisorRecords: Map<Identifier, List<AdvisorResult>>
+    ): AdvisorRun = blockingQuery {
+        val environmentDao = getOrPutEnvironment(environment)
+
+        val advisorRunDao = AdvisorRunDao.new {
+            this.advisorJob = AdvisorJobDao[advisorJobId]
+            this.startTime = startTime
+            this.endTime = endTime
+            this.environment = environmentDao
+        }
+
+        createAdvisorConfiguration(advisorRunDao, config)
+
+        advisorRecords.forEach { (id, results) ->
+            val identifierDao = getOrPutIdentifier(id)
+
+            val advisorRunIdentifierDao = AdvisorRunIdentifierDao.new {
+                this.advisorRun = advisorRunDao
+                this.identifier = identifierDao
+            }
+
+            results.forEach { result ->
+                val issues = result.issues.map(::getOrPutIssue)
+                val defects = result.defects.map(::getOrPutDefect)
+                val vulnerabilities = result.vulnerabilities.map(::getOrPutVulnerability)
+                AdvisorResultDao.new {
+                    this.advisorRunIdentifier = advisorRunIdentifierDao
+                    this.advisorName = result.advisorName
+                    this.capabilities = result.capabilities
+                    this.startTime = result.startTime
+                    this.endTime = result.endTime
+                    this.issues = SizedCollection(issues)
+                    this.defects = SizedCollection(defects)
+                    this.vulnerabilities = SizedCollection(vulnerabilities)
+                }
+            }
+        }
+
+        advisorRunDao.mapToModel()
+    }.getOrThrow()
+
+    override fun get(id: Long): AdvisorRun? = blockingQuery { AdvisorRunDao[id].mapToModel() }.getOrNull()
+}
+
+private fun createAdvisorConfiguration(
+    advisorRunDao: AdvisorRunDao,
+    advisorConfiguration: AdvisorConfiguration
+): AdvisorConfigurationDao {
+    val advisorConfigurationDao = AdvisorConfigurationDao.new {
+        advisorRun = advisorRunDao
+        osvConfiguration = advisorConfiguration.osvConfiguration?.let { createOsvConfiguration(it) }
+        nexusIqConfiguration = advisorConfiguration.nexusIqConfiguration?.let { createNexusIqConfiguration(it) }
+        vulnerableCodeConfiguration = advisorConfiguration.vulnerableCodeConfiguration?.let {
+            createVulnerableCodeConfiguration(it)
+        }
+
+        githubDefectsConfiguration = advisorConfiguration.githubDefectsConfiguration?.let {
+            createGitHubDefectsConfiguration(it)
+        }
+    }
+
+    advisorConfiguration.options.forEach { (key, value) ->
+        AdvisorConfigurationsOptionDao.new {
+            this.advisorConfiguration = advisorConfigurationDao
+            this.key = key
+            this.value = value
+        }
+    }
+
+    return advisorConfigurationDao
+}
+
+fun createVulnerableCodeConfiguration(
+    vulnerableCodeConfiguration: VulnerableCodeConfiguration
+): VulnerableCodeConfigurationDao =
+    VulnerableCodeConfigurationDao.new {
+        this.serverUrl = vulnerableCodeConfiguration.serverUrl
+    }
+
+fun createNexusIqConfiguration(nexusIqConfiguration: NexusIqConfiguration): NexusIqConfigurationDao =
+    NexusIqConfigurationDao.new {
+        serverUrl = nexusIqConfiguration.serverUrl
+        browseUrl = nexusIqConfiguration.browseUrl
+    }
+
+private fun createOsvConfiguration(osvConfiguration: OsvConfiguration): OsvConfigurationDao = OsvConfigurationDao.new {
+    this.serverUrl = osvConfiguration.serverUrl
+}
+
+private fun createGitHubDefectsConfiguration(
+    githubDefectsConfiguration: GithubDefectsConfiguration
+): GithubDefectsConfigurationDao = GithubDefectsConfigurationDao.new {
+    this.endpointUrl = githubDefectsConfiguration.endpointUrl
+    this.labelFilter = githubDefectsConfiguration.labelFilter
+    this.maxNumberOfIssuesPerRepository = githubDefectsConfiguration.maxNumberOfIssuesPerRepository
+    this.parallelRequests = githubDefectsConfiguration.parallelRequests
+}
+
+private fun getOrPutDefect(defect: Defect): DefectDao =
+    DefectDao.findByDefect(defect) ?: DefectDao.new {
+        externalId = defect.externalId
+        url = defect.url
+        title = defect.title
+        state = defect.state
+        severity = defect.severity
+        description = defect.description
+        creationTime = defect.creationTime
+        modificationTime = defect.modificationTime
+        closingTime = defect.closingTime
+        fixReleaseVersion = defect.fixReleaseVersion
+        fixReleaseUrl = defect.fixReleaseUrl
+    }.also {
+        defect.labels.forEach { (key, value) ->
+            DefectLabelDao.new {
+                this.defect = it
+                this.key = key
+                this.value = value
+            }
+        }
+    }
+
+private fun getOrPutVulnerability(vulnerability: Vulnerability): VulnerabilityDao =
+    VulnerabilityDao.findByVulnerability(vulnerability) ?: VulnerabilityDao.new {
+        externalId = vulnerability.externalId
+        summary = vulnerability.summary
+        description = vulnerability.description
+    }.also {
+        vulnerability.references.forEach { reference ->
+            VulnerabilityReferenceDao.new {
+                this.vulnerability = it
+                this.url = reference.url
+                this.scoringSystem = reference.scoringSystem
+                this.severity = reference.severity
+            }
+        }
+    }
---
+++ dao/src/test/kotlin/repositories/DaoAdvisorRunRepositoryTest.kt
@@ -0,0 +1,190 @@
+/*
+ * Copyright (C) 2022 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.dao.repositories
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+
+import kotlinx.datetime.Clock
+
+import org.ossreviewtoolkit.server.dao.test.DatabaseTestExtension
+import org.ossreviewtoolkit.server.dao.test.repositories.Fixtures
+import org.ossreviewtoolkit.server.dao.utils.toDatabasePrecision
+import org.ossreviewtoolkit.server.model.runs.Environment
+import org.ossreviewtoolkit.server.model.runs.Identifier
+import org.ossreviewtoolkit.server.model.runs.OrtIssue
+import org.ossreviewtoolkit.server.model.runs.advisor.AdvisorConfiguration
+import org.ossreviewtoolkit.server.model.runs.advisor.AdvisorResult
+import org.ossreviewtoolkit.server.model.runs.advisor.AdvisorRun
+import org.ossreviewtoolkit.server.model.runs.advisor.Defect
+import org.ossreviewtoolkit.server.model.runs.advisor.GithubDefectsConfiguration
+import org.ossreviewtoolkit.server.model.runs.advisor.NexusIqConfiguration
+import org.ossreviewtoolkit.server.model.runs.advisor.OsvConfiguration
+import org.ossreviewtoolkit.server.model.runs.advisor.Vulnerability
+import org.ossreviewtoolkit.server.model.runs.advisor.VulnerabilityReference
+import org.ossreviewtoolkit.server.model.runs.advisor.VulnerableCodeConfiguration
+
+class DaoAdvisorRunRepositoryTest : StringSpec() {
+    private val advisorRunRepository = DaoAdvisorRunRepository()
+
+    private lateinit var fixtures: Fixtures
+    private var advisorJobId = -1L
+
+    init {
+        extension(
+            DatabaseTestExtension {
+                fixtures = Fixtures()
+                advisorJobId = fixtures.advisorJob.id
+            }
+        )
+
+        "create should create an entry in the database" {
+            val variables = mapOf(
+                "SHELL" to "/bin/bash",
+                "TERM" to "xterm-256color"
+            )
+
+            val toolVersions = mapOf(
+                "Conan" to "1.53.0",
+                "NPM" to "8.15.1"
+            )
+
+            val environment = Environment(
+                ortVersion = "1.0",
+                javaVersion = "11.0.16",
+                os = "Linux",
+                processors = 8,
+                maxMemory = 8321499136,
+                variables = variables,
+                toolVersions = toolVersions
+            )
+
+            val advisorConfiguration = AdvisorConfiguration(
+                githubDefectsConfiguration = GithubDefectsConfiguration(
+                    endpointUrl = "https://github.com/defects",
+                    labelFilter = listOf("!any"),
+                    maxNumberOfIssuesPerRepository = 5,
+                    parallelRequests = 2
+                ),
+                nexusIqConfiguration = NexusIqConfiguration(
+                    serverUrl = "https://nexus-iq.com",
+                    browseUrl = "https://nexus-iq.com/browse"
+                ),
+                osvConfiguration = OsvConfiguration(
+                    serverUrl = "https://google.com/osv"
+                ),
+                vulnerableCodeConfiguration = VulnerableCodeConfiguration(
+                    serverUrl = "https://vulnerable-code.com"
+                ),
+                options = mapOf("config-key1" to "config-value1")
+            )
+
+            val identifier = Identifier(
+                type = "type",
+                namespace = "namespace",
+                name = "name",
+                version = "version"
+            )
+
+            val issue = OrtIssue(
+                timestamp = Clock.System.now(),
+                source = "source",
+                message = "message",
+                severity = "ERROR"
+            )
+
+            val defect = Defect(
+                externalId = "external-id",
+                url = "https://example.com/external-id",
+                title = "title",
+                state = "state",
+                severity = "ERROR",
+                description = "description",
+                creationTime = Clock.System.now(),
+                modificationTime = Clock.System.now(),
+                closingTime = Clock.System.now(),
+                fixReleaseVersion = "version",
+                fixReleaseUrl = "url",
+                labels = mapOf("key" to "value")
+            )
+
+            val vulnerability = Vulnerability(
+                externalId = "external-id",
+                summary = "summary",
+                description = "description",
+                references = listOf(
+                    VulnerabilityReference(
+                        url = "url",
+                        scoringSystem = "scoring-system",
+                        severity = "ERROR"
+                    )
+                )
+            )
+
+            val advisorResult = AdvisorResult(
+                advisorName = "NexusIQ",
+                capabilities = emptyList(),
+                startTime = Clock.System.now(),
+                endTime = Clock.System.now(),
+                issues = listOf(issue),
+                defects = listOf(defect),
+                vulnerabilities = listOf(vulnerability)
+            )
+
+            val createdAdvisorRun = advisorRunRepository.create(
+                advisorJobId = advisorJobId,
+                startTime = Clock.System.now(),
+                endTime = Clock.System.now(),
+                environment = environment,
+                config = advisorConfiguration,
+                advisorRecords = mapOf(identifier to listOf(advisorResult))
+            )
+
+            val dbEntry = advisorRunRepository.get(createdAdvisorRun.id)
+
+            dbEntry.shouldNotBeNull()
+            dbEntry shouldBe AdvisorRun(
+                id = createdAdvisorRun.id,
+                advisorJobId = advisorJobId,
+                startTime = createdAdvisorRun.startTime,
+                endTime = createdAdvisorRun.endTime,
+                environment = environment,
+                config = advisorConfiguration,
+                advisorRecords = mapOf(
+                    identifier to listOf(
+                        advisorResult.copy(
+                            startTime = advisorResult.startTime.toDatabasePrecision(),
+                            endTime = advisorResult.endTime.toDatabasePrecision(),
+                            issues = listOf(issue.copy(timestamp = issue.timestamp.toDatabasePrecision())),
+                            defects = listOf(
+                                defect.copy(
+                                    creationTime = defect.creationTime?.toDatabasePrecision(),
+                                    modificationTime = defect.modificationTime?.toDatabasePrecision(),
+                                    closingTime = defect.modificationTime?.toDatabasePrecision()
+                                )
+                            )
+                        )
+                    ).toList()
+                )
+            )
+        }
+    }
+}
