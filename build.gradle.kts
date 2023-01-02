0b22137b1a0bfb78d3a295343bb6416363d9ced6
Martin Nonnenmacher <martin.nonnenmacher@bosch.io>
2023-01-02 12:00:08 +0100

    Upgrade Kotlin to 1.8
    See [1].
    
    [1]: https://kotlinlang.org/docs/whatsnew18.html
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.io>

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,7 +88,7 @@ subprojects {
         kotlinOptions {
             allWarningsAsErrors = true
             jvmTarget = javaVersion.majorVersion
-            apiVersion = "1.7"
+            apiVersion = "1.8"
         }
     }
 
