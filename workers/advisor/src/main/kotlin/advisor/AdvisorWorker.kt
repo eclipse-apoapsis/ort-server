3f4dcc74ca31ef4441d612ab90c780621657812c
Martin Nonnenmacher <martin.nonnenmacher@bosch.io>
2023-01-17 16:22:51 +0100

    AdvisorWorker: Reduce the transaction scope
    Instead of one long-running transaction, use separate transactions when
    querying the advisor job at the start and storing the result at the end.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.io>

--- workers/advisor/src/main/kotlin/advisor/AdvisorWorker.kt
+++ workers/advisor/src/main/kotlin/advisor/AdvisorWorker.kt
@@ -23,7 +23,9 @@ import org.ossreviewtoolkit.model.OrtResult
 import org.ossreviewtoolkit.model.Repository
 import org.ossreviewtoolkit.model.VcsInfo
 import org.ossreviewtoolkit.server.dao.blockingQuery
+import org.ossreviewtoolkit.server.model.AdvisorJob
 import org.ossreviewtoolkit.server.model.JobStatus
+import org.ossreviewtoolkit.server.workers.common.JobIgnoredException
 import org.ossreviewtoolkit.server.workers.common.RunResult
 import org.ossreviewtoolkit.server.workers.common.mapToModel
 
@@ -40,33 +42,47 @@ internal class AdvisorWorker(
 ) {
     fun start() = receiver.receive(::run)
 
-    private fun run(advisorJobId: Long, traceId: String): RunResult = blockingQuery {
-        val advisorJob = dao.getAdvisorJob(advisorJobId)
-            ?: return@blockingQuery RunResult.Failed(
-                IllegalArgumentException("The advisor job '$advisorJobId' does not exist.")
-            )
+    private fun run(advisorJobId: Long, traceId: String): RunResult = runCatching {
+        val advisorJob = blockingQuery { getValidAdvisorJob(advisorJobId) }.getOrThrow()
 
-        if (advisorJob.status in invalidStates) {
-            logger.warn(
-                "Advisor job '$advisorJobId' status is already set to '${advisorJob.status}. Ignoring messages with " +
-                        "traceId '$traceId'."
-            )
-
-            return@blockingQuery RunResult.Ignored
-        }
+        logger.debug("Advisor job with id '${advisorJob.id}' started at ${advisorJob.startedAt}.")
 
         // TODO: Add more arguments to this function/class to retrieve more information for the construction of the
         //       OrtResult (e.g. AnalyzerRunRepository, OrtRunRepository, RepositoryRepository).
         val ortResult = OrtResult(Repository(VcsInfo.EMPTY))
 
-        logger.debug("Advisor job with id '$advisorJobId' started at ${advisorJob.startedAt}.")
         val advisorRun = runner.run(ortResult, advisorJob.configuration).advisor
             ?: throw AdvisorException("ORT Advisor failed to create a result.")
 
-        dao.storeAdvisorRun(advisorRun.mapToModel(advisorJobId))
+        blockingQuery {
+            getValidAdvisorJob(advisorJobId)
+            dao.storeAdvisorRun(advisorRun.mapToModel(advisorJobId))
+        }
 
         RunResult.Success
-    }.getOrElse { RunResult.Failed(it) }
+    }.getOrElse {
+        when (it) {
+            is JobIgnoredException -> {
+                logger.warn("Message with traceId '$traceId' ignored: ${it.message}")
+                RunResult.Ignored
+            }
+
+            else -> {
+                logger.error("Error while processing message with traceId '$traceId': ${it.message}")
+                RunResult.Failed(it)
+            }
+        }
+    }
+
+    private fun getValidAdvisorJob(jobId: Long) =
+        dao.getAdvisorJob(jobId)?.validate()
+            ?: throw IllegalArgumentException("The advisor job '$jobId' does not exist.")
+
+    private fun AdvisorJob.validate() = apply {
+        if (status in invalidStates) {
+            throw JobIgnoredException("Advisor job '$id' status is already set to '$status'.")
+        }
+    }
 }
 
 private class AdvisorException(message: String) : Exception(message)
