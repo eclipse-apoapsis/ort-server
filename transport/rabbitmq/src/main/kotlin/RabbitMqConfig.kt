9f1cd60864809c693adaeb7609915431b9853f3b
Marcel Bochtler <marcel.bochtler@bosch.com>
2023-01-23 09:38:38 +0100

    feat(transport): Add transport module for RabbitMQ
    Add a new transport module for RabbitMQ [1] and implement the
    configuration for it.
    
    [1]: https://www.rabbitmq.com
    
    Signed-off-by: Marcel Bochtler <marcel.bochtler@bosch.com>

---
+++ transport/rabbitmq/src/main/kotlin/RabbitMqConfig.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.transport.rabbitmq
+
+import com.typesafe.config.Config
+
+import org.ossreviewtoolkit.server.transport.Endpoint
+
+/**
+ * A class defining the configuration settings used by the RabbitMQ Transport implementation.
+ */
+class RabbitMqConfig(
+    /** The URI where the server broker is running. */
+    val serverUri: String,
+
+    /** The name of the queue that is used for sending and receiving messages. */
+    val queueName: String
+) {
+    companion object {
+        /**
+         * Constant for the name of this transport implementation. This name is used for both the message sender and
+         * receiver factories.
+         */
+        const val TRANSPORT_NAME = "rabbitMQ"
+
+        /** Name of the configuration property for the server URI. */
+        private const val SERVER_URI_PROPERTY = "serverUri"
+
+        /** Name of the configuration property for the queue name. */
+        private const val QUEUE_NAME_PROPERTY = "queueName"
+
+        /**
+         * Create a [RabbitMqConfig] object for a sender to the given [endpoint] from the provided [config].
+         */
+        fun createSenderConfig(endpoint: Endpoint<*>, config: Config): RabbitMqConfig =
+            createConfig(endpoint, "sender", config)
+
+        /**
+         * Create a [RabbitMqConfig] object for a receiver of the given [endpoint] from the provided [config].
+         */
+        fun createReceiverConfig(endpoint: Endpoint<*>, config: Config): RabbitMqConfig =
+            createConfig(endpoint, "receiver", config)
+
+        /**
+         * Create a [RabbitMqConfig] object for the given [endpoint] and [transportType] (sender or receiver) from the
+         * provided [config].
+         */
+        private fun createConfig(endpoint: Endpoint<*>, transportType: String, config: Config): RabbitMqConfig {
+            val prefix = "${endpoint.configPrefix}.$transportType"
+
+            return RabbitMqConfig(
+                serverUri = config.getString("$prefix.$SERVER_URI_PROPERTY"),
+                queueName = config.getString("$prefix.$QUEUE_NAME_PROPERTY")
+            )
+        }
+    }
+}
---
+++ transport/rabbitmq/src/test/kotlin/RabbitMqConfigTest.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.transport.rabbitmq
+
+import com.typesafe.config.Config
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+
+import io.mockk.every
+import io.mockk.mockk
+
+import org.ossreviewtoolkit.server.transport.AnalyzerEndpoint
+
+class RabbitMqConfigTest : WordSpec({
+    "createSenderConfig" should {
+        "create an instance from a Config object" {
+            val serverUri = "tcp://example.org:5445"
+            val queueName = "testQueue"
+
+            val config = mockk<Config> {
+                every { getString("${AnalyzerEndpoint.configPrefix}.sender.serverUri") } returns serverUri
+                every { getString("${AnalyzerEndpoint.configPrefix}.sender.queueName") } returns queueName
+            }
+
+            val rabbitMqConfig = RabbitMqConfig.createSenderConfig(AnalyzerEndpoint, config)
+
+            rabbitMqConfig.serverUri shouldBe serverUri
+            rabbitMqConfig.queueName shouldBe queueName
+        }
+    }
+
+    "createReceiverConfig" should {
+        "create an instance from a Config object" {
+            val serverUri = "tcp://example.org:5445"
+            val queueName = "testQueue"
+
+            val config = mockk<Config> {
+                every { getString("${AnalyzerEndpoint.configPrefix}.receiver.serverUri") } returns serverUri
+                every { getString("${AnalyzerEndpoint.configPrefix}.receiver.queueName") } returns queueName
+            }
+
+            val rabbitMqConfig = RabbitMqConfig.createReceiverConfig(AnalyzerEndpoint, config)
+
+            rabbitMqConfig.serverUri shouldBe serverUri
+            rabbitMqConfig.queueName shouldBe queueName
+        }
+    }
+})
