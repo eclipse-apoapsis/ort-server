424091ffe19bb956b6e5314def3d30d15ecfb802
Marcel Bochtler <marcel.bochtler@bosch.com>
2023-02-20 14:30:59 +0100

    feat(dao): Add database migration script for reporter jobs
    Signed-off-by: Marcel Bochtler <marcel.bochtler@bosch.com>

---
+++ dao/src/main/resources/db/migration/V17__reporterJobs.sql
@@ -0,0 +1,10 @@
+CREATE TABLE reporter_jobs
+(
+    id            bigserial PRIMARY KEY,
+    ort_run_id    bigint REFERENCES ort_runs NOT NULL,
+    created_at    timestamp                  NOT NULL,
+    started_at    timestamp                  NULL,
+    finished_at   timestamp                  NULL,
+    configuration jsonb                      NOT NULL,
+    status        text                       NOT NULL
+);
