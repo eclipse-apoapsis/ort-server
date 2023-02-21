5863a06b36614bb561e3ff4e0d74e4caa3f1768d
Marcel Bochtler <marcel.bochtler@bosch.com>
2023-02-21 13:06:56 +0100

    refactor(api): Move JobConfigurations to separate file
    Signed-off-by: Marcel Bochtler <marcel.bochtler@bosch.com>

--- api-v1/src/commonMain/kotlin/OrtRun.kt
+++ api-v1/src/commonMain/kotlin/OrtRun.kt
@@ -51,58 +51,6 @@ data class OrtRun(
 )
 
 /**
- * The configurations for the jobs in an [OrtRun].
- */
-@Serializable
-data class JobConfigurations(
-    val analyzer: AnalyzerJobConfiguration = AnalyzerJobConfiguration(),
-    val advisor: AdvisorJobConfiguration? = null,
-    val scanner: ScannerJobConfiguration? = null,
-    val evaluator: EvaluatorJobConfiguration? = null
-)
-
-/**
- * The configuration for an analyzer job.
- */
-@Serializable
-data class AnalyzerJobConfiguration(
-    val allowDynamicVersions: Boolean = false
-)
-
-/**
- * The configuration for an advisor job.
- */
-@Serializable
-data class AdvisorJobConfiguration(
-    /**
-     * The Advisors to use (e.g. NexusIQ, VulnerableCode, DefectDB).
-     */
-    val advisors: List<String> = emptyList()
-)
-
-/**
- * The configuration for a scanner job.
- */
-@Serializable
-data class ScannerJobConfiguration(
-    /**
-     * Do not scan excluded projects or packages.
-     */
-    val skipExcluded: Boolean = false
-)
-
-/**
- * The configuration for an evaluator job.
- */
-@Serializable
-data class EvaluatorJobConfiguration(
-    /**
-     * The id of the rule set to use for the evaluation.
-     */
-    val ruleSet: String? = null
-)
-
-/**
  * Request object for the create ort run endpoint.
  */
 @Serializable
