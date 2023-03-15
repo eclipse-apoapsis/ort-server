704dc218d581325f0b4c52fc7c2aec6680205708
Oliver Heger <oliver.heger@bosch.io>
2023-03-15 16:21:40 +0100

    feat(secrets): Add a SecretStorage class
    This class is responsible for creating a SecretsProvider based on the
    current configuration. It wraps this provider and implements a richter
    API on top of it.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ secrets/spi/src/main/kotlin/SecretStorage.kt
@@ -0,0 +1,162 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.secrets
+
+import com.typesafe.config.Config
+
+import java.util.ServiceLoader
+
+/**
+ * A class providing convenient access to secrets based on a [SecretsProvider].
+ *
+ * This class takes care of the instantiation of a [SecretsProvider] based on the application configuration via the
+ * [createStorage] function. This provider is then wrapped, and a richer API to deal with [Secret]s is implemented on
+ * top of it.
+ *
+ * The extended functionality compared to [SecretsProvider] is mainly related to the handling of missing [Secret]s
+ * and exception handling. There are functions that require a [Secret] to exist or throw an exception otherwise.
+ * With regard to exception handling, in general all exceptions thrown by the underlying [SecretsProvider] are caught
+ * and wrapped in a [SecretStorageException]; so, it should be sufficient to catch this exception type. Alternatively,
+ * consumers can choose to use functions that return [Result] objects.
+ */
+class SecretStorage(
+    /** The underlying provider for secrets. */
+    private val provider: SecretsProvider
+) {
+    companion object {
+        /** A prefix used for configuration properties related to the secrets provider implementation. */
+        const val CONFIG_PREFIX = "secretsProvider"
+
+        /** The name of the configuration property defining the name of the secrets provider implementation. */
+        const val NAME_PROPERTY = "name"
+
+        /** The service loader for loading [SecretsProviderFactory] implementations available on the classpath. */
+        private val LOADER = ServiceLoader.load(SecretsProviderFactory::class.java)
+
+        /**
+         * Return an initialized [SecretStorage] implementation based on the given [config]. This function obtains
+         * the sub configuration defined by [CONFIG_PREFIX] from the given [config]. There it looks up the name of
+         * the desired [SecretsProviderFactory] via the [NAME_PROPERTY] property. It then tries to find a factory with
+         * this name via the service loader mechanism and uses this to create a [SecretsProvider]. A [SecretStorage]
+         * instance wrapping this [SecretsProvider] is returned.
+         */
+        fun createStorage(config: Config): SecretStorage {
+            if (!config.hasPath("$CONFIG_PREFIX.$NAME_PROPERTY")) {
+                throw SecretStorageException(
+                    """
+                    Configuration property '$CONFIG_PREFIX$NAME_PROPERTY' is not set. Please set it to the name of the
+                    SecretProvider implementation to use.
+                """.trimIndent()
+                )
+            }
+
+            val providerConfig = config.getConfig(CONFIG_PREFIX)
+            val factoryName = providerConfig.getString(NAME_PROPERTY)
+            val factory = LOADER.find { it.name == factoryName }
+                ?: throw SecretStorageException("SecretsProviderFactory '$factoryName' not found on classpath.")
+
+            val provider = factory.createProvider(providerConfig)
+            return SecretStorage(provider)
+        }
+    }
+
+    /**
+     * Return the [Secret] at the given [path] or `null` if the path cannot be resolved.
+     */
+    fun readSecret(path: Path): Secret? = wrapExceptions { provider.readSecret(path) }
+
+    /**
+     * Return the [Secret] at the given [path] or fail with a [SecretStorageException] if the path cannot be resolved.
+     */
+    fun getSecret(path: Path): Secret =
+        readSecret(path) ?: throw SecretStorageException("No secret found at path '$path'.")
+
+    /**
+     * Return a [Result] with a nullable [Secret] found at the given [path]. This function works like [readSecret],
+     * but wraps an occurring exception inside a [Result]. Exceptions from the underlying [SecretsProvider] are
+     * wrapped in a [SecretStorageException].
+     */
+    fun readSecretCatching(path: Path): Result<Secret?> = runCatching { readSecret(path) }
+
+    /**
+     * Return a [Result] with the [Secret] found at the given [path]. This function works like [getSecret], but
+     * wraps an occurring exception inside a [Result]. Exceptions from the underlying [SecretsProvider] are wrapped
+     * in a [SecretStorageException]. If the given [path] cannot be resolved, a failed [Result] is returned as well.
+     */
+    fun getSecretCatching(path: Path): Result<Secret> = runCatching { getSecret(path) }
+
+    /**
+     * Store the given [secret] under the given [path] in the underlying [SecretsProvider]. Throw a
+     * [SecretStorageException] if this fails.
+     */
+    fun writeSecret(path: Path, secret: Secret) {
+        wrapExceptions { provider.writeSecret(path, secret) }
+    }
+
+    /**
+     * Store the given [secret] under the given [path] in the underlying [SecretsProvider] and return a [Result] for
+     * the outcome of the operation. Exceptions thrown by the [SecretsProvider] are wrapped in a
+     * [SecretStorageException] and returned in the [Result].
+     */
+    fun writeSecretCatching(path: Path, secret: Secret): Result<Unit> = runCatching { writeSecret(path, secret) }
+
+    /**
+     * Remove the [Secret] under the given [path]. Throw a [SecretStorageException] if this fails.
+     */
+    fun removeSecret(path: Path) {
+        wrapExceptions { provider.removeSecret(path) }
+    }
+
+    /**
+     * Remove the [Secret] under the given [path] and return a [Result] for the outcome of the operation. Exceptions
+     * thrown by the [SecretsProvider] are wrapped in a [SecretStorageException] and returned in the [Result].
+     */
+    fun removeSecretCatching(path: Path): Result<Unit> = runCatching { removeSecret(path) }
+
+    /**
+     * Return a set with the sub paths existing under the given [path]. Throw a [SecretStorageException] if this
+     * fails.
+     */
+    fun listPaths(path: Path): Set<Path> = wrapExceptions { provider.listPaths(path) }
+
+    /**
+     * Return a [Result] with a set with the sub paths existing under the given [path]. Exceptions thrown by the
+     * [SecretsProvider] are wrapped in a [SecretStorageException].
+     */
+    fun listPathsCatching(path: Path): Result<Set<Path>> = runCatching { listPaths(path) }
+}
+
+/**
+ * An exception class for reporting problems related to [SecretStorage] and the interaction with the underlying
+ * [SecretsProvider].
+ */
+class SecretStorageException(message: String, cause: Throwable? = null) : Exception(message, cause)
+
+/**
+ * Execute the given [block] and catch all exceptions it might throw. A caught exception is then wrapped in a
+ * [SecretStorageException] which is rethrown.
+ */
+@Suppress("TooGenericExceptionCaught")
+private fun <T> wrapExceptions(block: () -> T): T =
+    try {
+        block()
+    } catch (e: Exception) {
+        throw SecretStorageException("Exception from SecretsProvider", e)
+    }
---
+++ secrets/spi/src/test/kotlin/SecretStorageTest.kt
@@ -0,0 +1,278 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.secrets
+
+import com.typesafe.config.ConfigFactory
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.nulls.beNull
+import io.kotest.matchers.result.shouldBeFailure
+import io.kotest.matchers.result.shouldBeSuccess
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import io.kotest.matchers.types.beInstanceOf
+
+import kotlin.IllegalArgumentException
+
+import org.ossreviewtoolkit.server.secrets.SecretsProviderFactoryForTesting.Companion.PASSWORD_PATH
+import org.ossreviewtoolkit.server.secrets.SecretsProviderFactoryForTesting.Companion.PASSWORD_SECRET
+
+class SecretStorageTest : WordSpec({
+    "createStorage" should {
+        "fail if the name property is not provided in the configuration" {
+            val config = ConfigFactory.empty()
+
+            val exception = shouldThrow<SecretStorageException> {
+                SecretStorage.createStorage(config)
+            }
+
+            exception.message shouldContain (SecretStorage.NAME_PROPERTY)
+        }
+
+        "fail if the configured SecretsProvider cannot be resolved" {
+            val providerName = "nonExistingProvider"
+            val config = ConfigFactory.parseMap(
+                mapOf("${SecretStorage.CONFIG_PREFIX}.${SecretStorage.NAME_PROPERTY}" to providerName)
+            )
+
+            val exception = shouldThrow<SecretStorageException> {
+                SecretStorage.createStorage(config)
+            }
+
+            exception.message shouldContain providerName
+        }
+    }
+
+    "readSecret" should {
+        "return an existing secret" {
+            createStorage().readSecret(PASSWORD_PATH) shouldBe PASSWORD_SECRET
+        }
+
+        "return null for a non-existing secret" {
+            createStorage().readSecret(Path("unknown-path")) should beNull()
+        }
+
+        "wrap an exception thrown by the SecretsProvider" {
+            val exception = shouldThrow<SecretStorageException> {
+                createStorage().readSecret(ERROR_PATH)
+            }
+
+            exception.cause should beInstanceOf<IllegalArgumentException>()
+        }
+    }
+
+    "getSecret" should {
+        "return an existing secret" {
+            createStorage().getSecret(PASSWORD_PATH) shouldBe PASSWORD_SECRET
+        }
+
+        "throw an exception for a non-existing secret" {
+            val path = Path("non-existing")
+
+            val exception = shouldThrow<SecretStorageException> {
+                createStorage().getSecret(path)
+            }
+
+            exception.message shouldContain path.path
+        }
+    }
+
+    "readSecretCatching" should {
+        "return a Result with an existing secret" {
+            val result = createStorage().readSecretCatching(PASSWORD_PATH)
+
+            result shouldBeSuccess PASSWORD_SECRET
+        }
+
+        "return a Result with null for a non-existing secret" {
+            val result = createStorage().readSecretCatching(Path("non-existing"))
+
+            result shouldBeSuccess null
+        }
+
+        "return a failed Result if the SecretsProvider throws an exception" {
+            val result = createStorage().readSecretCatching(ERROR_PATH)
+
+            result shouldBeFailure { exception ->
+                exception.cause should beInstanceOf<IllegalArgumentException>()
+            }
+        }
+    }
+
+    "getSecretCatching" should {
+        "return a Result with an existing secret" {
+            val result = createStorage().getSecretCatching(PASSWORD_PATH)
+
+            result shouldBeSuccess PASSWORD_SECRET
+        }
+
+        "return a failed Result for a non existing secret" {
+            val path = Path("unresolvable")
+
+            val result = createStorage().getSecretCatching(path)
+
+            result shouldBeFailure { exception ->
+                exception should beInstanceOf<SecretStorageException>()
+                exception.message shouldContain path.path
+            }
+        }
+    }
+
+    "writeSecret" should {
+        "write a secret successfully" {
+            val newPath = Path("new-secret")
+            val newSecret = Secret("BrandNewSecret")
+            val storage = createStorage()
+
+            storage.writeSecret(newPath, newSecret)
+
+            storage.getSecret(newPath) shouldBe newSecret
+        }
+
+        "throw an exception if writing fails" {
+            val exception = shouldThrow<SecretStorageException> {
+                createStorage().writeSecret(ERROR_PATH, Secret("will-fail"))
+            }
+
+            exception.cause should beInstanceOf<IllegalArgumentException>()
+        }
+    }
+
+    "writeSecretCatching" should {
+        "return a success result if the operation is successful" {
+            val newPath = Path("new-secret")
+            val newSecret = Secret("BrandNewSecret")
+            val storage = createStorage()
+
+            val result = storage.writeSecretCatching(newPath, newSecret)
+
+            result.isSuccess shouldBe true
+            storage.getSecret(newPath) shouldBe newSecret
+        }
+
+        "return a failure result for a failing operation" {
+            val result = createStorage().writeSecretCatching(ERROR_PATH, Secret("?"))
+
+            result shouldBeFailure { exception ->
+                exception should beInstanceOf<SecretStorageException>()
+                exception.cause should beInstanceOf<IllegalArgumentException>()
+            }
+        }
+    }
+
+    "removeSecret" should {
+        "successfully remove a secret" {
+            val storage = createStorage()
+
+            storage.removeSecret(PASSWORD_PATH)
+
+            storage.readSecret(PASSWORD_PATH) should beNull()
+        }
+
+        "throw an exception if removing fails" {
+            val exception = shouldThrow<SecretStorageException> {
+                createStorage().readSecret(ERROR_PATH)
+            }
+
+            exception.cause should beInstanceOf<IllegalArgumentException>()
+        }
+    }
+
+    "removeSecretCatching" should {
+        "return a success result if the operation is successful" {
+            val storage = createStorage()
+            val result = storage.removeSecretCatching(PASSWORD_PATH)
+
+            result.isSuccess shouldBe true
+            storage.readSecret(PASSWORD_PATH) should beNull()
+        }
+
+        "return a failure result for a failed operation" {
+            val result = createStorage().removeSecretCatching(ERROR_PATH)
+
+            result shouldBeFailure { exception ->
+                exception should beInstanceOf<SecretStorageException>()
+                exception.cause should beInstanceOf<IllegalArgumentException>()
+            }
+        }
+    }
+
+    "listPaths" should {
+        "return a list with sub paths" {
+            val paths = createStorage().listPaths(SecretsProviderFactoryForTesting.PARENT_PATH)
+
+            paths shouldContainExactlyInAnyOrder listOf(
+                PASSWORD_PATH,
+                SecretsProviderFactoryForTesting.SERVICE_PATH,
+                SecretsProviderFactoryForTesting.TOKEN_PATH
+            )
+        }
+
+        "throw an exception if listing fails" {
+            val exception = shouldThrow<SecretStorageException> {
+                createStorage().listPaths(ERROR_PATH)
+            }
+
+            exception.cause should beInstanceOf<IllegalArgumentException>()
+        }
+    }
+
+    "listPathsCatching" should {
+        "return a success result if the operation is successful" {
+            val result = createStorage().listPathsCatching(SecretsProviderFactoryForTesting.PARENT_PATH)
+
+            result shouldBeSuccess { paths ->
+                paths shouldContainExactlyInAnyOrder listOf(
+                    PASSWORD_PATH,
+                    SecretsProviderFactoryForTesting.SERVICE_PATH,
+                    SecretsProviderFactoryForTesting.TOKEN_PATH
+                )
+            }
+        }
+
+        "return a failure result for a failed operation" {
+            val result = createStorage().listPathsCatching(ERROR_PATH)
+
+            result shouldBeFailure { exception ->
+                exception should beInstanceOf<SecretStorageException>()
+                exception.cause should beInstanceOf<IllegalArgumentException>()
+            }
+        }
+    }
+})
+
+/** The path that causes errors. */
+private val ERROR_PATH = Path("error")
+
+/**
+ * Return a [SecretStorage] object wrapping the test provider.
+ */
+private fun createStorage(): SecretStorage {
+    val properties = mapOf(
+        "${SecretStorage.CONFIG_PREFIX}.${SecretStorage.NAME_PROPERTY}" to SecretsProviderFactoryForTesting.NAME,
+        "${SecretStorage.CONFIG_PREFIX}.${SecretsProviderFactoryForTesting.ERROR_PATH_PROPERTY}" to ERROR_PATH.path
+    )
+    val config = ConfigFactory.parseMap(properties)
+
+    return SecretStorage.createStorage(config)
+}
