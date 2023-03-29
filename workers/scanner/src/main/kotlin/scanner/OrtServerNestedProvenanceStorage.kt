ff67ef329371238f609b311a97aa48009dd158c4
Martin Nonnenmacher <martin.nonnenmacher@bosch.io>
2023-03-29 12:52:21 +0200

    feat(scanner): Add a nested provenance storage
    Add a `NestedProvenanceStorage` implementation to be used by the scanner
    worker.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.io>

---
+++ workers/scanner/src/main/kotlin/scanner/OrtServerNestedProvenanceStorage.kt
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.workers.scanner
+
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.inList
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.deleteWhere
+import org.jetbrains.exposed.sql.select
+
+import org.ossreviewtoolkit.model.RepositoryProvenance
+import org.ossreviewtoolkit.scanner.provenance.NestedProvenance
+import org.ossreviewtoolkit.scanner.provenance.NestedProvenanceResolutionResult
+import org.ossreviewtoolkit.scanner.provenance.NestedProvenanceStorage
+import org.ossreviewtoolkit.server.dao.blockingQuery
+import org.ossreviewtoolkit.server.dao.tables.provenance.NestedProvenanceDao
+import org.ossreviewtoolkit.server.dao.tables.provenance.NestedProvenanceSubRepositoriesTable
+import org.ossreviewtoolkit.server.dao.tables.provenance.NestedProvenanceSubRepositoryDao
+import org.ossreviewtoolkit.server.dao.tables.provenance.NestedProvenancesTable
+import org.ossreviewtoolkit.server.dao.tables.runs.shared.VcsInfoDao
+import org.ossreviewtoolkit.server.dao.tables.runs.shared.VcsInfoTable
+import org.ossreviewtoolkit.server.workers.common.mapToModel
+import org.ossreviewtoolkit.server.workers.common.mapToOrt
+
+class OrtServerNestedProvenanceStorage : NestedProvenanceStorage {
+    override fun putNestedProvenance(
+        root: RepositoryProvenance,
+        result: NestedProvenanceResolutionResult
+    ) = blockingQuery {
+        val resolvedVcs = root.getResolvedVcs()
+
+        // Remove any old entries for the provided root provenance.
+        val oldEntries = NestedProvenancesTable.innerJoin(VcsInfoTable)
+            .slice(NestedProvenancesTable.id)
+            .select {
+                VcsInfoTable.type eq resolvedVcs.type and
+                        (VcsInfoTable.url eq resolvedVcs.url) and
+                        (VcsInfoTable.revision eq resolvedVcs.revision)
+            }.withDistinct().map { it[NestedProvenancesTable.id].value }
+
+        NestedProvenanceSubRepositoriesTable.deleteWhere { nestedProvenanceId inList oldEntries }
+        NestedProvenancesTable.deleteWhere { id inList oldEntries }
+
+        // Store the nested provenance result.
+        val vcsDao = VcsInfoDao.getOrPut(resolvedVcs)
+
+        val nestedProvenanceDao = NestedProvenanceDao.new {
+            rootVcs = vcsDao
+            rootResolvedRevision = root.resolvedRevision
+            hasOnlyFixedRevisions = result.hasOnlyFixedRevisions
+        }
+
+        result.nestedProvenance.subRepositories.forEach { (path, repositoryProvenance) ->
+            val nestedVcsDao = VcsInfoDao.getOrPut(repositoryProvenance.vcsInfo.mapToModel())
+
+            NestedProvenanceSubRepositoryDao.new {
+                nestedProvenance = nestedProvenanceDao
+                vcs = nestedVcsDao
+                resolvedRevision = repositoryProvenance.resolvedRevision
+                this.path = path
+            }
+        }
+    }.getOrThrow()
+
+    override fun readNestedProvenance(root: RepositoryProvenance): NestedProvenanceResolutionResult? = blockingQuery {
+        val resolvedVcs = root.getResolvedVcs()
+
+        NestedProvenancesTable.innerJoin(VcsInfoTable)
+            .slice(NestedProvenancesTable.columns)
+            .select {
+                VcsInfoTable.type eq resolvedVcs.type and
+                        (VcsInfoTable.url eq resolvedVcs.url) and
+                        (VcsInfoTable.revision eq resolvedVcs.revision)
+            }.firstOrNull()?.let {
+                NestedProvenanceDao.wrapRow(it).mapToOrt()
+            }
+    }.getOrThrow()
+}
+
+private fun RepositoryProvenance.getResolvedVcs() = vcsInfo.copy(revision = resolvedRevision).mapToModel()
+
+private fun NestedProvenanceDao.mapToOrt(): NestedProvenanceResolutionResult {
+    val nestedProvenance = NestedProvenance(
+        root = RepositoryProvenance(
+            vcsInfo = rootVcs.mapToModel().mapToOrt(),
+            resolvedRevision = rootResolvedRevision
+        ),
+        subRepositories = subRepositories.associate {
+            it.path to RepositoryProvenance(
+                vcsInfo = it.vcs.mapToModel().mapToOrt(),
+                resolvedRevision = it.resolvedRevision
+            )
+        }
+    )
+
+    return NestedProvenanceResolutionResult(nestedProvenance, hasOnlyFixedRevisions)
+}
---
+++ workers/scanner/src/test/kotlin/OrtServerNestedProvenanceStorageTest.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.workers.scanner
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+
+import org.ossreviewtoolkit.model.KnownProvenance
+import org.ossreviewtoolkit.model.RepositoryProvenance
+import org.ossreviewtoolkit.model.VcsInfo
+import org.ossreviewtoolkit.model.VcsType
+import org.ossreviewtoolkit.scanner.provenance.NestedProvenance
+import org.ossreviewtoolkit.scanner.provenance.NestedProvenanceResolutionResult
+import org.ossreviewtoolkit.server.dao.test.DatabaseTestExtension
+
+class OrtServerNestedProvenanceStorageTest : WordSpec() {
+    private val storage = OrtServerNestedProvenanceStorage()
+
+    init {
+        extension(
+            DatabaseTestExtension()
+        )
+
+        "putNestedProvenance" should {
+            "store a nested provenance in the database" {
+                val root = createRepositoryProvenance()
+                val result = NestedProvenanceResolutionResult(
+                    nestedProvenance = createNestedProvenance(root),
+                    hasOnlyFixedRevisions = true
+                )
+
+                storage.putNestedProvenance(root, result)
+
+                storage.readNestedProvenance(root) shouldBe result
+            }
+
+            "overwrite a previously stored nested provenance" {
+                val root = createRepositoryProvenance()
+                val result1 = NestedProvenanceResolutionResult(
+                    nestedProvenance = createNestedProvenance(root, mapOf("path" to root)),
+                    hasOnlyFixedRevisions = true
+                )
+                val result2 = NestedProvenanceResolutionResult(createNestedProvenance(root), true)
+
+                storage.putNestedProvenance(root, result1)
+                storage.putNestedProvenance(root, result2)
+
+                storage.readNestedProvenance(root) shouldBe result2
+            }
+        }
+    }
+}
+
+private fun createVcsInfo(
+    type: VcsType = VcsType.GIT,
+    url: String = "https://github.com/apache/logging-log4j2.git",
+    revision: String = "be881e503e14b267fb8a8f94b6d15eddba7ed8c4"
+) = VcsInfo(type, url, revision)
+
+private fun createRepositoryProvenance(
+    vcsInfo: VcsInfo = createVcsInfo(),
+    resolvedRevision: String = vcsInfo.revision
+) = RepositoryProvenance(vcsInfo, resolvedRevision)
+
+private fun createNestedProvenance(
+    root: KnownProvenance,
+    subRepositories: Map<String, RepositoryProvenance> = emptyMap()
+) = NestedProvenance(root, subRepositories)
