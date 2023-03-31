bed5b18992f0e6874d4786c7da27b4cf00f7f987
Oliver Heger <oliver.heger@bosch.io>
2023-03-31 11:21:16 +0200

    refactor(vault): Extract code related to Vault test-container setup
    Move the code to a new VaultTestContainer class. This makes it easy to
    use this functionality in other tests, too. Upcoming commits will add
    more tests.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ secrets/vault/src/test/kotlin/VaultTestContainer.kt
@@ -0,0 +1,182 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.secrets.vault
+
+import io.kotest.assertions.fail
+import io.kotest.common.runBlocking
+import io.kotest.core.extensions.install
+import io.kotest.core.spec.Spec
+import io.kotest.extensions.testcontainers.TestContainerExtension
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.request.put
+import io.ktor.client.request.setBody
+
+import java.util.regex.Pattern
+
+import org.ossreviewtoolkit.server.secrets.vault.model.VaultCredentials
+
+import org.testcontainers.utility.DockerImageName
+import org.testcontainers.vault.VaultContainer
+
+class VaultTestContainer {
+    companion object {
+        /** The root path under which secrets are stored in the vault. */
+        const val PATH = "ort/server/secrets/"
+
+        /** The root token used by the vault service. */
+        private const val VAULT_TOKEN = "onetobindthemall"
+
+        /** The header in which vault expects the authorization token. */
+        private const val TOKEN_HEADER = "X-Vault-Token"
+
+        /** The name of the role used to authenticate the client. */
+        private const val ROLE_NAME = "test-server"
+
+        /** The name of the docker image for the vault test container. */
+        private val image = DockerImageName.parse("hashicorp/vault:1.13").asCompatibleSubstituteFor("vault")
+
+        /**
+         * Obtain the [VaultCredentials] to construct an authorized token for the test Vault container. Use [client]
+         * for requests against the API.
+         */
+        private suspend fun getCredentials(client: HttpClient): VaultCredentials {
+            // Query the roleId.
+            val roleId = client.get("/v1/auth/approle/role/$ROLE_NAME/role-id")
+                .body<String>().extractJsonField("role_id")
+
+            // Generate and obtain a secretId.
+            val secretId = client.post("/v1/auth/approle/role/$ROLE_NAME/secret-id")
+                .body<String>().extractJsonField("secret_id")
+
+            return VaultCredentials(roleId, secretId)
+        }
+
+        /**
+         * Prepare the Vault container to support AppRole authentication and set up the test role with an associated
+         * policy. Use [client] to interact with the container.
+         */
+        private suspend fun configureAppRoleAuthentication(client: HttpClient) {
+            // Enable AppRole authentication.
+            client.post("/v1/sys/auth/approle") {
+                setBody("""{"type": "approle"}""")
+            }
+
+            // Create a policy for the role used within tests that allows full access to the secrets below the test
+            // path.
+            val policyAccess = policy("secret/data/$PATH", "create", "read", "update", "delete")
+            val policyDelete = policy("secret/metadata/$PATH", "delete")
+            client.put("/v1/sys/policies/acl/$ROLE_NAME") {
+                setBody(
+                    """{
+                            "policy": "$policyAccess\n$policyDelete" 
+                       }
+                    """.trimIndent()
+                )
+            }
+
+            // Create a test role and associate it with the policy.
+            client.post("/v1/auth/approle/role/$ROLE_NAME") {
+                setBody(
+                    """
+                        {
+                            "token_policies": "$ROLE_NAME",
+                            "token_ttl": "2h"
+                        }
+                    """.trimIndent()
+                )
+            }
+        }
+
+        /**
+         * Generate the string to define a policy that grants the given [capabilities] to the given [path].
+         */
+        private fun policy(path: String, vararg capabilities: String): String =
+            """path \"$path*\" {\n capabilities = [ ${capabilities.joinToString(", ") { """\"$it\"""" }} ]\n}"""
+
+        /**
+         * Create an [HttpClient] that is configured for the interaction with the given [vault] container.
+         */
+        private fun createVaultClient(vault: VaultContainer<*>): HttpClient =
+            HttpClient(OkHttp) {
+                defaultRequest {
+                    url(vault.httpHostAddress)
+                    header(TOKEN_HEADER, VAULT_TOKEN)
+                }
+                expectSuccess = true
+            }
+
+        /**
+         * Extract the value of the string field with the given [name] from this JSON string or fail if this field
+         * cannot be resolved.
+         */
+        private fun String.extractJsonField(name: String): String {
+            val regExtract = Regex(""""${Pattern.quote(name)}"\s*:\s*"([^"]+)"""")
+            return regExtract.find(this)?.groupValues?.get(1) ?: fail("Cannot find field '$name' in '$this'.")
+        }
+    }
+
+    /** The managed [VaultContainer]. */
+    val vault: VaultContainer<*> = VaultContainer(image)
+        .withVaultToken(VAULT_TOKEN)
+        .withSecretInVault("secret/${PATH}user", "value=scott")
+        .withSecretInVault("secret/${PATH}password", "value=tiger")
+        .withSecretInVault("secret/${PATH}strange", "noValue=set")
+
+    /** The credentials for accessing the container. */
+    private val credentials: VaultCredentials by lazy { setUpCredentials() }
+
+    /**
+     * Create a [VaultSecretsProvider] for accessing the managed Vault service under the given [rootPath].
+     */
+    fun createProvider(rootPath: String = PATH): VaultSecretsProvider {
+        val config = VaultConfiguration(vault.httpHostAddress, credentials, rootPath)
+        return VaultSecretsProvider(config)
+    }
+
+    /**
+     * Trigger the configuration of the managed Vault service. Set up the AppRole authentication method and obtain
+     * corresponding credentials.
+     */
+    private fun setUpCredentials(): VaultCredentials {
+        val client = createVaultClient(vault)
+
+        return runBlocking {
+            configureAppRoleAuthentication(client)
+            getCredentials(client)
+        }
+    }
+}
+
+/**
+ * Create a [VaultTestContainer] instance and install it as extension in this [Spec].
+ */
+fun Spec.installVaultTestContainer(): VaultTestContainer {
+    val container = VaultTestContainer()
+    install(TestContainerExtension(container.vault))
+
+    return container
+}
