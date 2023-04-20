c54a5282e09a7caeb1999aae002279bb43de0a80
Oleksandr Yarosevych <fixed-term.oleksandr.yarosevych@bosch.io>
2023-04-20 11:03:49 +0200

    feat(scanner): Create Scan Result Storage implementation
    Signed-off-by: Oleksandr Yarosevych <fixed-term.oleksandr.yarosevych@bosch.io>

---
+++ workers/scanner/src/main/kotlin/scanner/OrtServerScanResultStorage.kt
@@ -0,0 +1,187 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.workers.scanner
+
+import kotlinx.datetime.toJavaInstant
+import kotlinx.datetime.toKotlinInstant
+
+import org.jetbrains.exposed.sql.SizedCollection
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.and
+
+import org.ossreviewtoolkit.model.ArtifactProvenance
+import org.ossreviewtoolkit.model.CopyrightFinding
+import org.ossreviewtoolkit.model.Issue
+import org.ossreviewtoolkit.model.KnownProvenance
+import org.ossreviewtoolkit.model.LicenseFinding
+import org.ossreviewtoolkit.model.RepositoryProvenance
+import org.ossreviewtoolkit.model.ScanResult
+import org.ossreviewtoolkit.model.ScanSummary
+import org.ossreviewtoolkit.model.ScannerDetails
+import org.ossreviewtoolkit.model.Severity
+import org.ossreviewtoolkit.model.TextLocation
+import org.ossreviewtoolkit.scanner.ProvenanceBasedScanStorage
+import org.ossreviewtoolkit.scanner.ScanStorageException
+import org.ossreviewtoolkit.server.dao.blockingQuery
+import org.ossreviewtoolkit.server.dao.tables.AdditionalScanResultData
+import org.ossreviewtoolkit.server.dao.tables.CopyrightFindingDao
+import org.ossreviewtoolkit.server.dao.tables.LicenseFindingDao
+import org.ossreviewtoolkit.server.dao.tables.ScanResultDao
+import org.ossreviewtoolkit.server.dao.tables.ScanResultsTable
+import org.ossreviewtoolkit.server.dao.tables.ScanSummaryDao
+import org.ossreviewtoolkit.server.dao.tables.runs.shared.OrtIssueDao
+import org.ossreviewtoolkit.server.model.runs.OrtIssue
+
+/**
+ * A class providing access to scan results.
+ *
+ * [ScanResult] are searched by details of a [KnownProvenance] which it is associated with. If the [KnownProvenance] is
+ * an [ArtifactProvenance], the URL and the hash value must match. If the [KnownProvenance] is a [RepositoryProvenance],
+ * the VCS type, URL and the resolved revision must match.
+ *
+ * Throws a [ScanStorageException] if an error occurs while reading from the storage.
+ */
+class OrtServerScanResultStorage : ProvenanceBasedScanStorage {
+    override fun read(provenance: KnownProvenance): List<ScanResult> = blockingQuery {
+            when (provenance) {
+                is ArtifactProvenance -> {
+                    ScanResultDao.find(
+                        ScanResultsTable.artifactUrl eq provenance.sourceArtifact.url and
+                                (ScanResultsTable.artifactHash eq provenance.sourceArtifact.hash.value)
+                    )
+                }
+
+                is RepositoryProvenance -> {
+                    ScanResultDao.find(
+                        ScanResultsTable.vcsType eq provenance.vcsInfo.type.toString() and
+                                (ScanResultsTable.vcsUrl eq provenance.vcsInfo.url) and
+                                (ScanResultsTable.vcsRevision eq provenance.resolvedRevision)
+                    )
+                }
+            }.map {
+                ScanResult(
+                    provenance = provenance,
+                    scanner = ScannerDetails(
+                        name = it.scannerName,
+                        version = it.scannerVersion,
+                        configuration = it.scannerConfiguration
+                    ),
+                    summary = it.scanSummary.mapToOrt(),
+                    additionalData = it.additionalScanResultData?.data ?: emptyMap()
+                )
+            }
+        }.getOrThrow()
+
+    override fun write(scanResult: ScanResult) {
+        val provenance = scanResult.provenance
+
+        if (provenance !is KnownProvenance) {
+            throw ScanStorageException("Scan result must have a known provenance, but it is $provenance.")
+        }
+
+        if (provenance is RepositoryProvenance && provenance.vcsInfo.path.isNotEmpty()) {
+            throw ScanStorageException("Repository provenances with a non-empty VCS path are not supported.")
+        }
+        blockingQuery {
+            ScanResultDao.new {
+                when (provenance) {
+                    is ArtifactProvenance -> {
+                        this.artifactUrl = provenance.sourceArtifact.url
+                        this.artifactHash = provenance.sourceArtifact.hash.value
+                    }
+
+                    is RepositoryProvenance -> {
+                        this.vcsType = provenance.vcsInfo.type.toString()
+                        this.vcsUrl = provenance.vcsInfo.url
+                        this.vcsRevision = provenance.resolvedRevision
+                    }
+                }
+
+                val issues = scanResult.summary.issues.map(Issue::mapToModel).map(OrtIssueDao::getOrPut)
+
+                this.scannerName = scanResult.scanner.name
+                this.scannerVersion = scanResult.scanner.version
+                this.scannerConfiguration = scanResult.scanner.configuration
+                this.additionalScanResultData = AdditionalScanResultData(scanResult.additionalData)
+                this.scanSummary = ScanSummaryDao.new {
+                    this.startTime = scanResult.summary.startTime.toKotlinInstant()
+                    this.endTime = scanResult.summary.endTime.toKotlinInstant()
+                    this.packageVerificationCode = scanResult.summary.packageVerificationCode
+                    this.issues = SizedCollection(issues)
+                }
+
+                val summary = this.scanSummary
+                scanResult.summary.licenseFindings.forEach {
+                    LicenseFindingDao.new {
+                        this.scanSummary = summary
+                        this.license = it.license.toString()
+                        this.path = it.location.path
+                        this.startLine = it.location.startLine
+                        this.endLine = it.location.endLine
+                        this.score = it.score
+                    }
+                }
+                scanResult.summary.copyrightFindings.forEach {
+                    CopyrightFindingDao.new {
+                        this.scanSummary = summary
+                        this.statement = it.statement
+                        this.path = it.location.path
+                        this.startLine = it.location.startLine
+                        this.endLine = it.location.endLine
+                    }
+                }
+            }
+        }.getOrThrow()
+    }
+}
+
+private fun ScanSummaryDao.mapToOrt() = ScanSummary(
+    this.startTime.toJavaInstant(),
+    this.endTime.toJavaInstant(),
+    this.packageVerificationCode,
+    this.licenseFindings.map { it.mapToOrt() }.toSortedSet(),
+    this.copyrightFindings.map { it.mapToOrt() }.toSortedSet(),
+    this.issues.map {
+        Issue(
+            it.timestamp.toJavaInstant(),
+            it.source,
+            it.message,
+            Severity.valueOf(it.severity)
+        )
+    }
+)
+
+private fun LicenseFindingDao.mapToOrt() = LicenseFinding(
+    this.license,
+    TextLocation(this.path, this.startLine, this.endLine),
+    this.score
+)
+
+private fun CopyrightFindingDao.mapToOrt() = CopyrightFinding(
+    this.statement,
+    TextLocation(this.path, this.startLine, this.endLine)
+)
+
+private fun Issue.mapToModel() = OrtIssue(
+    timestamp = this.timestamp.toKotlinInstant(),
+    source = this.source,
+    message = this.message,
+    severity = this.severity.name
+)
---
+++ workers/scanner/src/test/kotlin/OrtServerScanResultStorageTest.kt
@@ -0,0 +1,185 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.workers.scanner
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.collections.shouldNotContain
+import io.kotest.matchers.shouldBe
+
+import java.time.Instant
+
+import org.ossreviewtoolkit.model.ArtifactProvenance
+import org.ossreviewtoolkit.model.CopyrightFinding
+import org.ossreviewtoolkit.model.Hash
+import org.ossreviewtoolkit.model.HashAlgorithm
+import org.ossreviewtoolkit.model.Issue
+import org.ossreviewtoolkit.model.KnownProvenance
+import org.ossreviewtoolkit.model.LicenseFinding
+import org.ossreviewtoolkit.model.RemoteArtifact
+import org.ossreviewtoolkit.model.RepositoryProvenance
+import org.ossreviewtoolkit.model.ScanResult
+import org.ossreviewtoolkit.model.ScanSummary
+import org.ossreviewtoolkit.model.ScannerDetails
+import org.ossreviewtoolkit.model.Severity
+import org.ossreviewtoolkit.model.TextLocation
+import org.ossreviewtoolkit.model.VcsInfo
+import org.ossreviewtoolkit.model.VcsType
+import org.ossreviewtoolkit.server.dao.test.DatabaseTestExtension
+
+private const val TIME_STAMP_SECONDS = 1678119934L
+
+class OrtServerScanResultStorageTest : WordSpec() {
+    private val scanResultStorage = OrtServerScanResultStorage()
+
+    init {
+        extension(
+            DatabaseTestExtension()
+        )
+
+        "write" should {
+            "create a repository provenance scan result in the storage" {
+                val provenance = createRepositoryProvenance()
+                val scanResult = createScanResult("VulnerableCode", createIssue("source1"), provenance)
+                scanResultStorage.write(scanResult)
+
+                scanResultStorage.read(provenance) shouldBe listOf(scanResult)
+            }
+
+            "create an artifact provenance scan result in the storage" {
+                val provenance = createArtifactProvenance()
+                val scanResult = createScanResult("VulnerableCode", createIssue("source1"), provenance)
+                scanResultStorage.write(scanResult)
+
+                scanResultStorage.read(provenance) shouldBe listOf(scanResult)
+            }
+        }
+
+        "read" should {
+            "read scan results by repository provenance in the database" {
+                val repositoryProvenance = createRepositoryProvenance()
+                val artifactProvenance = createArtifactProvenance()
+
+                val scanResult1 = createScanResult("VulnerableCode", createIssue("source1"), repositoryProvenance)
+                val scanResult2 = createScanResult("FossID", createIssue("source2"), repositoryProvenance)
+                val scanResult3 = createScanResult("FossID", createIssue("source3"), artifactProvenance)
+
+                scanResultStorage.write(scanResult1)
+                scanResultStorage.write(scanResult2)
+                scanResultStorage.write(scanResult3)
+
+                val readResult = scanResultStorage.read(repositoryProvenance)
+                readResult shouldContainExactlyInAnyOrder listOf(scanResult1, scanResult2)
+                readResult shouldNotContain scanResult3
+            }
+
+            "read scan results by artifact provenance in the database" {
+                val artifactProvenance = createArtifactProvenance()
+                val repositoryProvenance = createRepositoryProvenance()
+
+                val scanResult1 = createScanResult("VulnerableCode", createIssue("source1"), artifactProvenance)
+                val scanResult2 = createScanResult("FossID", createIssue("source2"), artifactProvenance)
+                val scanResult3 = createScanResult("FossID", createIssue("source3"), repositoryProvenance)
+
+                scanResultStorage.write(scanResult1)
+                scanResultStorage.write(scanResult2)
+                scanResultStorage.write(scanResult3)
+
+                val readResult = scanResultStorage.read(artifactProvenance)
+                readResult shouldContainExactlyInAnyOrder listOf(scanResult1, scanResult2)
+                readResult shouldNotContain scanResult3
+            }
+
+            "return empty results list in case no matching repository provenance scan results are found the database" {
+                val artifactProvenance = createArtifactProvenance()
+                val repositoryProvenance = createRepositoryProvenance()
+
+                val scanResult = createScanResult("VulnerableCode", createIssue("source"), artifactProvenance)
+
+                scanResultStorage.write(scanResult)
+
+                val readResult = scanResultStorage.read(repositoryProvenance)
+                readResult shouldBe emptyList()
+            }
+
+            "return empty results list in case no matching artifact provenance scan results are found the database" {
+                val artifactProvenance = createArtifactProvenance()
+                val repositoryProvenance = createRepositoryProvenance()
+
+                val scanResult = createScanResult("FossID", createIssue("source"), repositoryProvenance)
+
+                scanResultStorage.write(scanResult)
+
+                val readResult = scanResultStorage.read(artifactProvenance)
+                readResult shouldBe emptyList()
+            }
+        }
+    }
+}
+
+private fun createVcsInfo() = VcsInfo(
+    VcsType.GIT,
+    "https://github.com/apache/logging-log4j2.git",
+    "be881e503e14b267fb8a8f94b6d15eddba7ed8c4"
+)
+
+private fun createRepositoryProvenance(
+    vcsInfo: VcsInfo = createVcsInfo(),
+    resolvedRevision: String = vcsInfo.revision
+) = RepositoryProvenance(vcsInfo, resolvedRevision)
+
+private fun createRemoteArtifact() =
+    RemoteArtifact(
+        url = "https://repo1.maven.org/maven2/org/apache/logging/" +
+                "log4j/log4j-api/2.14.1/log4j-api-2.14.1-sources.jar",
+        hash = Hash("b2327c47ca413c1ec183575b19598e281fcd74d8", HashAlgorithm.SHA1)
+    )
+
+private fun createArtifactProvenance() = ArtifactProvenance(createRemoteArtifact())
+
+private fun createIssue(source: String) =
+    Issue(Instant.ofEpochSecond(TIME_STAMP_SECONDS), source, "message", Severity.ERROR)
+
+private fun createScanResult(scannerName: String, issue: Issue, provenance: KnownProvenance): ScanResult {
+    return ScanResult(
+        provenance = provenance,
+        scanner = ScannerDetails(scannerName, "1.2", "config"),
+        summary = ScanSummary(
+            Instant.ofEpochSecond(TIME_STAMP_SECONDS),
+            Instant.ofEpochSecond(TIME_STAMP_SECONDS),
+            "fyi3g4i72g482",
+            sortedSetOf(
+                LicenseFinding(
+                    "LicenseRef-23",
+                    TextLocation("//example/path", 1, 50),
+                    Float.MIN_VALUE
+                )
+            ),
+            sortedSetOf(
+                CopyrightFinding(
+                    "Copyright Finding Statement",
+                    TextLocation("//example/path", 1, 50)
+                )
+            ),
+            listOf(issue)
+        ),
+        mapOf("additional" to "data", "additional" to "data")
+    )
+}
