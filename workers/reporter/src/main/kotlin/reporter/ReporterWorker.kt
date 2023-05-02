df48e21ddeefed8006e0aebcfba191aaf9d1c347
Onur Demirci <onur.demirci@bosch.io>
2023-05-02 12:53:30 +0200

    feat(reporter): Implement the `ReporterWorker`
    Signed-off-by: Onur Demirci <onur.demirci@bosch.io>

---
+++ workers/reporter/src/main/kotlin/reporter/ReporterWorker.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.workers.reporter
+
+import org.ossreviewtoolkit.server.dao.blockingQuery
+import org.ossreviewtoolkit.server.model.JobStatus
+import org.ossreviewtoolkit.server.model.ReporterJob
+import org.ossreviewtoolkit.server.workers.common.JobIgnoredException
+import org.ossreviewtoolkit.server.workers.common.RunResult
+import org.ossreviewtoolkit.server.workers.common.mapToOrt
+
+import org.slf4j.LoggerFactory
+
+private val logger = LoggerFactory.getLogger(ReporterWorker::class.java)
+
+private val invalidStates = setOf(JobStatus.FAILED, JobStatus.FINISHED)
+
+internal class ReporterWorker(private val runner: ReporterRunner, private val dao: ReporterWorkerDao) {
+    fun run(jobId: Long, traceId: String): RunResult = runCatching {
+        val (reporterJob, ortResult) = blockingQuery {
+            val reporterJob = getValidReporterJob(jobId)
+            val ortRun = dao.getOrtRun(reporterJob.ortRunId)
+            requireNotNull(ortRun) {
+                "ORT run '${reporterJob.ortRunId}' not found."
+            }
+
+            val repository = dao.getRepository(ortRun.repositoryId)
+            requireNotNull(repository) {
+                "Repository '${ortRun.repositoryId}' not found."
+            }
+
+            val analyzerRun = dao.getAnalyzerRunForReporterJob(reporterJob)
+            val advisorRun = dao.getAdvisorRunForReporterJob(reporterJob)
+            val evaluatorRun = dao.getEvaluatorRunForReporterJob(reporterJob)
+
+            // TODO: As soon as ScannerRun is implemented, it should be considered also in the mapping of an OrtResult.
+            val ortResult = ortRun.mapToOrt(
+                repository = repository.mapToOrt(ortRun.revision),
+                analyzerRun = analyzerRun?.mapToOrt(),
+                advisorRun = advisorRun?.mapToOrt(),
+                evaluatorRun = evaluatorRun?.mapToOrt()
+            )
+
+            Pair(reporterJob, ortResult)
+        }.getOrThrow()
+
+        runner.run(ortResult, reporterJob.configuration)
+
+        RunResult.Success
+    }.getOrElse {
+        when (it) {
+            is JobIgnoredException -> {
+                logger.warn("Message with traceId '$traceId' ignored: ${it.message}")
+                RunResult.Ignored
+            }
+
+            else -> {
+                logger.error("Error while processing message with traceId '$traceId': ${it.message}")
+                RunResult.Failed(it)
+            }
+        }
+    }
+
+    private fun getValidReporterJob(jobId: Long) =
+        dao.getReporterJob(jobId)?.validate()
+            ?: throw IllegalArgumentException("The reporter job '$jobId' does not exist.")
+
+    private fun ReporterJob.validate() = apply {
+        if (status in invalidStates) {
+            throw JobIgnoredException("Reporter job '$id' status is already set to '$status'.")
+        }
+    }
+}
---
+++ workers/reporter/src/test/kotlin/reporter/ReporterWorkerTest.kt
@@ -0,0 +1,129 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.workers.reporter
+
+import io.kotest.assertions.fail
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+
+import kotlinx.datetime.Clock
+
+import org.ossreviewtoolkit.model.OrtResult
+import org.ossreviewtoolkit.server.dao.test.mockkTransaction
+import org.ossreviewtoolkit.server.model.JobStatus
+import org.ossreviewtoolkit.server.model.OrtRun
+import org.ossreviewtoolkit.server.model.ReporterJob
+import org.ossreviewtoolkit.server.model.ReporterJobConfiguration
+import org.ossreviewtoolkit.server.model.Repository
+import org.ossreviewtoolkit.server.model.runs.AnalyzerRun
+import org.ossreviewtoolkit.server.model.runs.EvaluatorRun
+import org.ossreviewtoolkit.server.model.runs.advisor.AdvisorRun
+import org.ossreviewtoolkit.server.workers.common.RunResult
+import org.ossreviewtoolkit.server.workers.common.mapToOrt
+
+private const val ORT_SERVER_MAPPINGS_FILE = "org.ossreviewtoolkit.server.workers.common.OrtServerMappingsKt"
+
+private const val REPORTER_JOB_ID = 1L
+private const val REPOSITORY_ID = 1L
+private const val ORT_RUN_ID = 12L
+private const val TRACE_ID = "42"
+
+private val reporterJob = ReporterJob(
+    id = REPORTER_JOB_ID,
+    ortRunId = ORT_RUN_ID,
+    createdAt = Clock.System.now(),
+    startedAt = Clock.System.now(),
+    finishedAt = null,
+    configuration = ReporterJobConfiguration(listOf("WebApp")),
+    status = JobStatus.CREATED
+)
+
+class ReporterWorkerTest : StringSpec({
+    "Reports for a project should be created successfully" {
+        val analyzerRun = mockk<AnalyzerRun>()
+        val advisorRun = mockk<AdvisorRun>()
+        val evaluatorRun = mockk<EvaluatorRun>()
+        val repository = mockk<Repository>()
+        val ortRun = mockk<OrtRun> {
+            every { id } returns ORT_RUN_ID
+            every { repositoryId } returns REPOSITORY_ID
+            every { revision } returns "main"
+        }
+
+        mockkStatic(ORT_SERVER_MAPPINGS_FILE)
+        every { repository.mapToOrt(any(), any()) } returns mockk()
+        every { analyzerRun.mapToOrt() } returns mockk()
+        every { advisorRun.mapToOrt() } returns mockk()
+        every { evaluatorRun.mapToOrt() } returns mockk()
+        every { ortRun.mapToOrt(any(), any(), any(), any(), any()) } returns OrtResult.EMPTY
+
+        val dao = mockk<ReporterWorkerDao> {
+            every { getAnalyzerRunForReporterJob(any()) } returns analyzerRun
+            every { getAdvisorRunForReporterJob(any()) } returns advisorRun
+            every { getEvaluatorRunForReporterJob(any()) } returns evaluatorRun
+            every { getReporterJob(any()) } returns reporterJob
+            every { getOrtRun(any()) } returns ortRun
+            every { getRepository(any()) } returns repository
+        }
+
+        val worker = ReporterWorker(ReporterRunner(), dao)
+
+        mockkTransaction {
+            val result = worker.run(REPORTER_JOB_ID, TRACE_ID)
+
+            result shouldBe RunResult.Success
+        }
+    }
+
+    "A failure result should be returned in case of an error" {
+        val testException = IllegalStateException("Test exception")
+        val dao = mockk<ReporterWorkerDao> {
+            every { getReporterJob(any()) } throws testException
+        }
+
+        val worker = ReporterWorker(ReporterRunner(), dao)
+
+        mockkTransaction {
+            when (val result = worker.run(REPORTER_JOB_ID, TRACE_ID)) {
+                is RunResult.Failed -> result.error shouldBe testException
+                else -> fail("Unexpected result: $result")
+            }
+        }
+    }
+
+    "An ignored result should be returned for an invalid job" {
+        val invalidJob = reporterJob.copy(status = JobStatus.FINISHED)
+        val dao = mockk<ReporterWorkerDao> {
+            every { getReporterJob(any()) } returns invalidJob
+        }
+
+        val worker = ReporterWorker(ReporterRunner(), dao)
+
+        mockkTransaction {
+            val result = worker.run(REPORTER_JOB_ID, TRACE_ID)
+
+            result shouldBe RunResult.Ignored
+        }
+    }
+})
