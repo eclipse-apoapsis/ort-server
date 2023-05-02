e5f53f6afe439c745f5de725c4de5072c2dcf657
Onur Demirci <onur.demirci@bosch.io>
2023-05-02 12:53:25 +0200

    feat(reporter): Implement the `ReporterWorkerDao`
    This class provides functions to make database queries that are needed in
    the `ReporterWorker`.
    
    Signed-off-by: Onur Demirci <onur.demirci@bosch.io>

---
+++ workers/reporter/src/main/kotlin/reporter/ReporterWorkerDao.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.workers.reporter
+
+import org.ossreviewtoolkit.server.model.ReporterJob
+import org.ossreviewtoolkit.server.model.repositories.AdvisorJobRepository
+import org.ossreviewtoolkit.server.model.repositories.AdvisorRunRepository
+import org.ossreviewtoolkit.server.model.repositories.AnalyzerJobRepository
+import org.ossreviewtoolkit.server.model.repositories.AnalyzerRunRepository
+import org.ossreviewtoolkit.server.model.repositories.EvaluatorJobRepository
+import org.ossreviewtoolkit.server.model.repositories.EvaluatorRunRepository
+import org.ossreviewtoolkit.server.model.repositories.OrtRunRepository
+import org.ossreviewtoolkit.server.model.repositories.ReporterJobRepository
+import org.ossreviewtoolkit.server.model.repositories.RepositoryRepository
+import org.ossreviewtoolkit.server.model.runs.AnalyzerRun
+import org.ossreviewtoolkit.server.model.runs.EvaluatorRun
+import org.ossreviewtoolkit.server.model.runs.advisor.AdvisorRun
+
+class ReporterWorkerDao(
+    private val advisorJobRepository: AdvisorJobRepository,
+    private val advisorRunRepository: AdvisorRunRepository,
+    private val analyzerJobRepository: AnalyzerJobRepository,
+    private val analyzerRunRepository: AnalyzerRunRepository,
+    private val evaluatorJobRepository: EvaluatorJobRepository,
+    private val evaluatorRunRepository: EvaluatorRunRepository,
+    private val ortRunRepository: OrtRunRepository,
+    private val reporterJobRepository: ReporterJobRepository,
+    private val repositoryRepository: RepositoryRepository
+) {
+    fun getReporterJob(jobId: Long) = reporterJobRepository.get(jobId)
+
+    fun getOrtRun(ortRunId: Long) = ortRunRepository.get(ortRunId)
+
+    fun getRepository(repositoryId: Long) = repositoryRepository.get(repositoryId)
+
+    fun getAnalyzerRunForReporterJob(reporterJob: ReporterJob): AnalyzerRun? {
+        val ortRun = ortRunRepository.get(reporterJob.ortRunId) ?: return null
+        val analyzerJob = analyzerJobRepository.getForOrtRun(ortRun.id) ?: return null
+        return analyzerRunRepository.getByJobId(analyzerJob.id)
+    }
+
+    fun getAdvisorRunForReporterJob(reporterJob: ReporterJob): AdvisorRun? {
+        val ortRun = ortRunRepository.get(reporterJob.ortRunId) ?: return null
+        val advisorJob = advisorJobRepository.getForOrtRun(ortRun.id) ?: return null
+        return advisorRunRepository.getByJobId(advisorJob.id)
+    }
+
+    fun getEvaluatorRunForReporterJob(reporterJob: ReporterJob): EvaluatorRun? {
+        val ortRun = ortRunRepository.get(reporterJob.ortRunId) ?: return null
+        val evaluatorJob = evaluatorJobRepository.getForOrtRun(ortRun.id) ?: return null
+        return evaluatorRunRepository.getByJobId(evaluatorJob.id)
+    }
+}
---
+++ workers/reporter/src/test/kotlin/reporter/ReporterWorkerDaoTest.kt
@@ -0,0 +1,181 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.workers.reporter
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+
+import kotlinx.datetime.Clock
+
+import org.ossreviewtoolkit.server.dao.repositories.DaoAdvisorJobRepository
+import org.ossreviewtoolkit.server.dao.repositories.DaoAdvisorRunRepository
+import org.ossreviewtoolkit.server.dao.repositories.DaoAnalyzerJobRepository
+import org.ossreviewtoolkit.server.dao.repositories.DaoAnalyzerRunRepository
+import org.ossreviewtoolkit.server.dao.repositories.DaoEvaluatorJobRepository
+import org.ossreviewtoolkit.server.dao.repositories.DaoEvaluatorRunRepository
+import org.ossreviewtoolkit.server.dao.repositories.DaoOrtRunRepository
+import org.ossreviewtoolkit.server.dao.repositories.DaoReporterJobRepository
+import org.ossreviewtoolkit.server.dao.repositories.DaoRepositoryRepository
+import org.ossreviewtoolkit.server.dao.test.DatabaseTestExtension
+import org.ossreviewtoolkit.server.dao.test.Fixtures
+import org.ossreviewtoolkit.server.model.runs.AnalyzerConfiguration
+import org.ossreviewtoolkit.server.model.runs.Environment
+import org.ossreviewtoolkit.server.model.runs.advisor.AdvisorConfiguration
+import org.ossreviewtoolkit.utils.common.gibibytes
+
+class ReporterWorkerDaoTest : WordSpec({
+    val analyzerRunRepository = DaoAnalyzerRunRepository()
+    val advisorRunRepository = DaoAdvisorRunRepository()
+    val evaluatorRunRepository = DaoEvaluatorRunRepository()
+    val dao = ReporterWorkerDao(
+        advisorJobRepository = DaoAdvisorJobRepository(),
+        advisorRunRepository = advisorRunRepository,
+        analyzerJobRepository = DaoAnalyzerJobRepository(),
+        analyzerRunRepository = analyzerRunRepository,
+        evaluatorJobRepository = DaoEvaluatorJobRepository(),
+        evaluatorRunRepository = evaluatorRunRepository,
+        ortRunRepository = DaoOrtRunRepository(),
+        reporterJobRepository = DaoReporterJobRepository(),
+        repositoryRepository = DaoRepositoryRepository()
+    )
+    lateinit var fixtures: Fixtures
+
+    extension(
+        DatabaseTestExtension {
+            fixtures = Fixtures()
+        }
+    )
+
+    "getReporterJob" should {
+        "return job if job does exist" {
+            val job = dao.getReporterJob(fixtures.reporterJob.id)
+
+            job shouldBe fixtures.reporterJob
+        }
+
+        "return null if job does not exist" {
+            dao.getReporterJob(-1L) shouldBe null
+        }
+    }
+
+    "getOrtRun" should {
+        "return an ort run" {
+            val ortRun = dao.getOrtRun(fixtures.ortRun.id)
+
+            ortRun shouldBe fixtures.ortRun
+        }
+
+        "return null if run does not exist" {
+            dao.getOrtRun(-1L) shouldBe null
+        }
+    }
+
+    "getRepository" should {
+        "return a repository" {
+            val repository = dao.getRepository(fixtures.repository.id)
+
+            repository shouldBe fixtures.repository
+        }
+
+        "return null if repository does not exist" {
+            dao.getRepository(-1L) shouldBe null
+        }
+    }
+
+    "getAnalyzerRunForReporterJob" should {
+        "return an analyzer run" {
+            val createdAnalyzerRun = analyzerRunRepository.create(
+                analyzerJobId = fixtures.analyzerJob.id,
+                startTime = Clock.System.now(),
+                endTime = Clock.System.now(),
+                environment = Environment(
+                    ortVersion = "1.0.0",
+                    javaVersion = "17",
+                    os = "Linux",
+                    processors = 8,
+                    maxMemory = 16.gibibytes,
+                    variables = emptyMap(),
+                    toolVersions = emptyMap()
+                ),
+                config = AnalyzerConfiguration(),
+                projects = emptySet(),
+                packages = emptySet(),
+                issues = emptyMap(),
+                dependencyGraphs = emptyMap()
+            )
+
+            val requestedAnalyzerRun = dao.getAnalyzerRunForReporterJob(fixtures.reporterJob)
+
+            requestedAnalyzerRun shouldBe createdAnalyzerRun
+        }
+
+        "return null if run does not exist" {
+            dao.getAnalyzerRunForReporterJob(fixtures.reporterJob.copy(ortRunId = -1L)) shouldBe null
+        }
+    }
+
+    "getAdvisorRunForReporterJob" should {
+        "return an advisor run" {
+            val createdAdvisorRun = advisorRunRepository.create(
+                advisorJobId = fixtures.advisorJob.id,
+                startTime = Clock.System.now(),
+                endTime = Clock.System.now(),
+                environment = Environment(
+                    ortVersion = "1.0.0",
+                    javaVersion = "17",
+                    os = "Linux",
+                    processors = 8,
+                    maxMemory = 16.gibibytes,
+                    variables = emptyMap(),
+                    toolVersions = emptyMap()
+                ),
+                config = AdvisorConfiguration(null, null, null, null, emptyMap()),
+                advisorRecords = emptyMap()
+            )
+
+            val requestedAdvisorRun = dao.getAdvisorRunForReporterJob(fixtures.reporterJob)
+
+            requestedAdvisorRun shouldBe createdAdvisorRun
+        }
+
+        "return null if run does not exist" {
+            dao.getAdvisorRunForReporterJob(fixtures.reporterJob.copy(ortRunId = -1L)) shouldBe null
+        }
+    }
+
+    "getEvaluatorRunForReporterJob" should {
+        "return an evaluator run" {
+            val createdEvaluatorRun = evaluatorRunRepository.create(
+                evaluatorJobId = fixtures.evaluatorJob.id,
+                startTime = Clock.System.now(),
+                endTime = Clock.System.now(),
+                violations = emptyList()
+            )
+
+            val requestedEvaluatorRun = dao.getEvaluatorRunForReporterJob(fixtures.reporterJob)
+
+            requestedEvaluatorRun shouldBe createdEvaluatorRun
+        }
+
+        "return null if run does not exist" {
+            dao.getEvaluatorRunForReporterJob(fixtures.reporterJob.copy(ortRunId = -1L)) shouldBe null
+        }
+    }
+})
