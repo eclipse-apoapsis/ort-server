bf804532f9ab818d5a75ce403987803a520a5adf
Oliver Heger <oliver.heger@bosch.io>
2023-05-11 17:06:23 +0200

    feat(storage): Add StorageProviderFactory interface
    Via this interface concrete StorageProvider instances can be obtained.
    The factories available on the classpath are looked up via the Java
    ServiceLoader mechanism.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ storage/spi/src/main/kotlin/StorageProviderFactory.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.storage
+
+import com.typesafe.config.Config
+
+/**
+ * Definition of a factory interface for creating a concrete [StorageProvider] instance.
+ *
+ * The storage implementation to be used for a specific use case can be defined in the application configuration.
+ * Based on this, a factory (matched by its [name] property) is looked up from a service loader and asked to create the
+ * corresponding provider object.
+ */
+interface StorageProviderFactory {
+    /** Defines a name for this concrete storage implementation. */
+    val name: String
+
+    /**
+     * Return an initialized [StorageProvider] using the given [config].
+     */
+    fun createProvider(config: Config): StorageProvider
+}
