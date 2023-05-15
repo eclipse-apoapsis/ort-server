78cca73b89c0145c56cfddd46d2059b5673c6c96
Martin Nonnenmacher <martin.nonnenmacher@bosch.io>
2023-05-15 21:45:37 +0200

    feat(model): Add a model for permissions
    Add enums representing the permissions available for organizations,
    products, and repositories. The permissions will be used to control
    access to endpoints in the API and to manage roles in Keycloak.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.io>

---
+++ model/src/commonMain/kotlin/authorization/OrganizationPermission.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.model.authorization
+
+import org.ossreviewtoolkit.server.model.Organization
+import org.ossreviewtoolkit.server.model.Product
+
+enum class OrganizationPermission {
+
+    /** Permission to read the [Organization] details. */
+    READ,
+
+    /** Permission to write the [Organization] details. */
+    WRITE,
+
+    /** Permission to write the [Organization] secrets. */
+    WRITE_SECRETS,
+
+    /** Permission to read the list of [Product]s of the [Organization]. */
+    READ_PRODUCTS,
+
+    /** Permission to create a [Product] for the [Organization]. */
+    CREATE_PRODUCT,
+
+    /** Permission to delete the [Organization]. */
+    DELETE;
+
+    companion object {
+        /**
+         * Get all [role names][roleName] for the provided [organizationId].
+         */
+        fun getRolesForOrganization(organizationId: Long) =
+            enumValues<OrganizationPermission>().map { it.roleName(organizationId) }
+    }
+
+    /** A unique name for this permission to be used to represent the permission as a role in Keycloak. */
+    fun roleName(organizationId: Long): String = "${prefix(organizationId)}_${name.lowercase()}"
+
+    private fun prefix(organizationId: Long) = "permission_organization_$organizationId"
+}
---
+++ model/src/commonMain/kotlin/authorization/ProductPermission.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.model.authorization
+
+import org.ossreviewtoolkit.server.model.Product
+import org.ossreviewtoolkit.server.model.Repository
+
+enum class ProductPermission {
+    /** Permission to read the [Product] details. */
+    READ,
+
+    /** Permission to write the [Product] details. */
+    WRITE,
+
+    /** Permission to write the [Product] secrets. */
+    WRITE_SECRETS,
+
+    /** Permission to read the list of [repositories][Repository] of the [Product]. */
+    READ_REPOSITORIES,
+
+    /** Permission to create a [Repository] for the [Product]. */
+    CREATE_REPOSITORY,
+
+    /** Permission to delete the [Product]. */
+    DELETE;
+
+    companion object {
+        /**
+         * Get all [role names][roleName] for the provided [productId].
+         */
+        fun getRolesForProduct(productId: Long) =
+            enumValues<ProductPermission>().map { it.roleName(productId) }
+    }
+
+    /** A unique name for this permission to be used to represent the permission as a role in Keycloak. */
+    fun roleName(productId: Long): String = "${prefix(productId)}_${name.lowercase()}"
+
+    private fun prefix(productId: Long) = "permission_product_$productId"
+}
