1fc91d1537233b5bd234a8633f9fe5a0f4d80712
Oliver Heger <oliver.heger@bosch.io>
2023-05-19 15:32:37 +0200

    feat(storage): Add helper functions for dealing with large objects
    PostgreSQL uses a proprietary API for accessing and manipulating large
    objects [1]. Add functionality to wrap this API, so that the actual
    StorageProvider implementation is simplified.
    
    [1]: https://jdbc.postgresql.org/documentation/binary-data/
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ storage/database/src/main/kotlin/LargeObjects.kt
@@ -0,0 +1,141 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.storage.database
+
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.InputStream
+import java.nio.file.Files
+import java.sql.Connection
+
+import kotlin.io.path.outputStream
+
+import org.jetbrains.exposed.sql.Transaction
+
+import org.postgresql.PGConnection
+import org.postgresql.largeobject.LargeObjectManager
+
+import org.slf4j.LoggerFactory
+
+private val logger = LoggerFactory.getLogger("LargeObjects")
+
+/**
+ * Use proprietary API of PosgreSQL to create a large object and populate it with the given [data]. Return the ID of
+ * this object which needs to be stored in the storage table as a reference.
+ */
+internal fun Transaction.storeLargeObject(data: InputStream): Long {
+    val largeObjectManager = largeObjectManager(jdbcConnection())
+
+    val oid = largeObjectManager.createLO(LargeObjectManager.READWRITE)
+    largeObjectManager.open(oid, LargeObjectManager.WRITE).use { obj ->
+        data.copyTo(obj.outputStream)
+    }
+
+    return oid
+}
+
+/**
+ * Return an [InputStream] to read the data of the large object with the given [oid]. Check the given [size] against
+ * the configured [inMemoryLimit].
+ */
+internal fun Transaction.readLargeObject(oid: Long, size: Long, inMemoryLimit: Int): InputStream {
+    val largeObjectManager = largeObjectManager(jdbcConnection())
+    return getStreamForLargeObject(largeObjectManager, oid, size, inMemoryLimit)
+}
+
+/**
+ * Delete the large object with the given [oid]. This is not done automatically when the referencing entity is deleted.
+ */
+internal fun Transaction.deleteLargeObject(oid: Long) {
+    val largeObjectManager = largeObjectManager(jdbcConnection())
+    largeObjectManager.delete(oid)
+}
+
+/**
+ * Return an [InputStream] to read the data of the large object with the given [oid] using the given
+ * [largeObjectManager]. If the object fits into memory (according to the given [object size][size] and
+ * [limit][inMemoryLimit]), return a [ByteArrayInputStream]. Otherwise, copy its data into a temporary file and
+ * return a stream from this file.
+ */
+internal fun getStreamForLargeObject(
+    largeObjectManager: LargeObjectManager,
+    oid: Long,
+    size: Long,
+    inMemoryLimit: Int
+): InputStream {
+    return largeObjectManager.open(oid, LargeObjectManager.READ).use { largeObject ->
+        if (size <= inMemoryLimit) {
+            logger.debug("Loading data of size {} into memory.", size)
+
+            val buffer = ByteArrayOutputStream(size.toInt())
+            largeObject.inputStream.copyTo(buffer)
+            ByteArrayInputStream(buffer.toByteArray())
+        } else {
+            val tempFile = Files.createTempFile("dbstorage", "tmp")
+            logger.debug("Storing data of size {} in temporary file '{}'.", size, tempFile)
+
+            largeObject.inputStream.copyTo(tempFile.outputStream())
+            TempFileInputStream(tempFile.toFile())
+        }
+    }
+}
+
+/**
+ * A specialized [InputStream] implementation that operates on a temporary file. The read functions are implemented
+ * to access the input stream from this file. When the stream is closed the file is deleted.
+ *
+ * This class is used to work around the limitation that PostgreSQL large objects can only be accessed during a
+ * transaction. Most use cases, however, require reading the stream after the transaction. Therefore, to avoid that the
+ * whole data needs to be read in memory, the storage provider implementation creates a temporary file first and then
+ * exposes the stream from this file.
+ *
+ * Note: This would be good use case for delegation, but unfortunately, this only works for interfaces.
+ */
+internal class TempFileInputStream(
+    /** The temporary file to wrap. */
+    private val tempFile: File
+) : InputStream() {
+    /** The stream to the temporary file to which all read operations are delegated. */
+    private val tempStream = tempFile.inputStream()
+
+    override fun close() {
+        tempStream.close()
+        tempFile.delete()
+    }
+
+    override fun read(): Int = tempStream.read()
+
+    override fun read(b: ByteArray): Int = tempStream.read(b)
+
+    override fun read(b: ByteArray, off: Int, len: Int): Int = tempStream.read(b, off, len)
+}
+
+/**
+ * Return the JDBC [Connection] from this transaction. This is required for some low-level operations.
+ */
+private fun Transaction.jdbcConnection(): Connection =
+    requireNotNull(connection.connection as? Connection) { "Cannot obtain JDBC connection." }
+
+/**
+ * Obtain the PostgreSQL API for managing large objects from the given [connection].
+ */
+private fun largeObjectManager(connection: Connection): LargeObjectManager =
+    connection.unwrap(PGConnection::class.java).largeObjectAPI
---
+++ storage/database/src/test/kotlin/LargeObjectsTest.kt
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.storage.database
+
+import io.kotest.core.spec.Spec
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.engine.spec.tempdir
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
+
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.runs
+import io.mockk.verify
+
+import java.io.ByteArrayInputStream
+import java.io.File
+
+import org.postgresql.largeobject.LargeObject
+import org.postgresql.largeobject.LargeObjectManager
+
+class LargeObjectsTest : WordSpec({
+    "TempFileInputStream" should {
+        "read data byte-wise" {
+            val data = "The data to be read."
+            val file = createTempFile(data.toByteArray())
+
+            TempFileInputStream(file).use { stream ->
+                stream.read() shouldBe 'T'.code.toByte()
+                stream.read() shouldBe 'h'.code.toByte()
+            }
+        }
+
+        "read an array of data" {
+            val bufSize = 8
+            val data = "This is a block of data to be read"
+            val file = createTempFile(data.toByteArray())
+
+            TempFileInputStream(file).use { stream ->
+                val buf = ByteArray(bufSize)
+                stream.read(buf) shouldBe bufSize
+                buf shouldBe data.substring(0, bufSize).toByteArray()
+            }
+        }
+
+        "read a part of a byte array" {
+            val data = ByteArray(16) { idx -> idx.toByte() }
+            val file = createTempFile(data)
+
+            TempFileInputStream(file).use { stream ->
+                val buf = ByteArray(16)
+                stream.read(buf, 4, 8) shouldBe 8
+                buf shouldBe byteArrayOf(0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 0, 0, 0, 0)
+            }
+        }
+
+        "delete the temporary file when it is closed" {
+            val file = createTempFile(byteArrayOf(1, 2, 3))
+
+            val stream = TempFileInputStream(file)
+            stream.close()
+
+            file.exists() shouldBe false
+        }
+    }
+
+    "getStreamForLargeObject" should {
+        "read the data into memory if it fits" {
+            val oid = 42L
+            val data = "The data of the large object.".toByteArray()
+
+            val lom = mockk<LargeObjectManager>()
+            val obj = mockk<LargeObject>()
+            every { lom.open(oid, LargeObjectManager.READ) } returns obj
+            every { obj.inputStream } returns ByteArrayInputStream(data)
+            every { obj.close() } just runs
+
+            val stream = getStreamForLargeObject(lom, oid, data.size.toLong(), data.size)
+
+            stream.readAllBytes() shouldBe data
+            stream.shouldBeInstanceOf<ByteArrayInputStream>()
+
+            verify { obj.close() }
+        }
+
+        "create a temporary file if necessary" {
+            val oid = 43L
+            val data = "This is really huge data that does not fit into memory.".toByteArray()
+
+            val lom = mockk<LargeObjectManager>()
+            val obj = mockk<LargeObject>()
+            every { lom.open(oid, LargeObjectManager.READ) } returns obj
+            every { obj.inputStream } returns ByteArrayInputStream(data)
+            every { obj.close() } just runs
+
+            getStreamForLargeObject(lom, oid, data.size.toLong(), data.size - 1).use { stream ->
+                stream.readAllBytes() shouldBe data
+                stream.shouldBeInstanceOf<TempFileInputStream>()
+            }
+
+            verify { obj.close() }
+        }
+    }
+})
+
+/**
+ * Create a file in a temporary directory and populate it with the given [data].
+ */
+private fun Spec.createTempFile(data: ByteArray): File {
+    val dir = tempdir()
+    val file = dir.resolve("test.tmp")
+
+    file.writeBytes(data)
+    return file
+}
