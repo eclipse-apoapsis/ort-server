bbddccaedb546e2e56e4372d83e1328d3a254dbc
Oliver Heger <oliver.heger@bosch.io>
2023-06-05 15:58:30 +0200

    feat(services): Add InfrastructureServiceService class
    This new service is responsible for managing the infrastructure
    services of organizations and products. It is going to be called from
    the corresponding API endpoints.
    
    So far, only the services associated with an organization can be
    managed.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ services/src/main/kotlin/InfrastructureServiceService.kt
@@ -0,0 +1,150 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.services
+
+import org.jetbrains.exposed.sql.Database
+
+import org.ossreviewtoolkit.server.dao.dbQuery
+import org.ossreviewtoolkit.server.model.InfrastructureService
+import org.ossreviewtoolkit.server.model.Secret
+import org.ossreviewtoolkit.server.model.repositories.InfrastructureServiceRepository
+import org.ossreviewtoolkit.server.model.util.ListQueryParameters
+import org.ossreviewtoolkit.server.model.util.OptionalValue
+import org.ossreviewtoolkit.server.model.util.asPresent
+
+/**
+ * A service providing functionality for managing [infrastructure services][InfrastructureService].
+ *
+ * Infrastructure services can be manually assigned to organizations and products. On the repository level, they are
+ * defined using a configuration file that is part of the repository and read during the analysis phase.
+ */
+class InfrastructureServiceService(
+    /** Reference to the database. */
+    private val db: Database,
+
+    /** The repository for infrastructure services. */
+    private val infrastructureServiceRepository: InfrastructureServiceRepository,
+
+    /** The service to manage secrets. */
+    private val secretService: SecretService
+) {
+    /**
+     * Create an [InfrastructureService] with the given properties and assign it to the organization with the given
+     * [organizationId].
+     */
+    suspend fun createForOrganization(
+        organizationId: Long,
+        name: String,
+        url: String,
+        description: String?,
+        usernameSecretRef: String,
+        passwordSecretRef: String
+    ): InfrastructureService {
+        val usernameSecret = resolveOrganizationSecret(organizationId, usernameSecretRef)
+        val passwordSecret = resolveOrganizationSecret(organizationId, passwordSecretRef)
+
+        return db.dbQuery {
+            infrastructureServiceRepository.create(
+                name,
+                url,
+                description,
+                usernameSecret,
+                passwordSecret,
+                organizationId,
+                null
+            )
+        }
+    }
+
+    /**
+     * Update the [InfrastructureService] with the given [name] assigned to the organization with the given
+     * [organizationId] based on the provided properties.
+     */
+    suspend fun updateForOrganization(
+        organizationId: Long,
+        name: String,
+        url: OptionalValue<String>,
+        description: OptionalValue<String?>,
+        usernameSecretRef: OptionalValue<String>,
+        passwordSecretRef: OptionalValue<String>
+    ): InfrastructureService {
+        val usernameSecret = resolveOrganizationSecretOptional(organizationId, usernameSecretRef)
+        val passwordSecret = resolveOrganizationSecretOptional(organizationId, passwordSecretRef)
+
+        return db.dbQuery {
+            infrastructureServiceRepository.updateForOrganizationAndName(
+                organizationId,
+                name,
+                url,
+                description,
+                usernameSecret,
+                passwordSecret
+            )
+        }
+    }
+
+    /**
+     * Delete the [InfrastructureService] with the given [name] that is assigned to the organization with the given
+     * [organizationId].
+     */
+    suspend fun deleteForOrganization(organizationId: Long, name: String) {
+        db.dbQuery {
+            infrastructureServiceRepository.deleteForOrganizationAndName(organizationId, name)
+        }
+    }
+
+    /**
+     * Return a list with the [InfrastructureService]s assigned to the organization with the given [organizationId]
+     * according to the provided [parameters].
+     */
+    suspend fun listForOrganization(
+        organizationId: Long,
+        parameters: ListQueryParameters
+    ): List<InfrastructureService> = db.dbQuery {
+        infrastructureServiceRepository.listForOrganization(organizationId, parameters)
+    }
+
+    /**
+     * Resolve a secret reference for the given [organizationId] and [secretName]. Throw an exception if the
+     * reference cannot be resolved.
+     */
+    private suspend fun resolveOrganizationSecret(organizationId: Long, secretName: String): Secret =
+        secretService.getSecretByOrganizationIdAndName(organizationId, secretName)
+            ?: throw InvalidSecretReferenceException(secretName)
+
+    /**
+     * Resolve a secret reference in an [OptionalValue] for the given [organizationId]. Note that this cannot be
+     * done through the [OptionalValue.map] function, because the transformation function is not a suspend function.
+     */
+    private suspend fun resolveOrganizationSecretOptional(
+        organizationId: Long,
+        secretName: OptionalValue<String>
+    ): OptionalValue<Secret> =
+        when (secretName) {
+            is OptionalValue.Present -> resolveOrganizationSecret(organizationId, secretName.value).asPresent()
+            else -> OptionalValue.Absent
+        }
+}
+
+/**
+ * An exception class that is thrown if the reference to a secret cannot be resolved.
+ */
+class InvalidSecretReferenceException(reference: String) :
+    Exception("Could not resolve secret reference '$reference'.")
---
+++ services/src/test/kotlin/InfrastructureServiceServiceTest.kt
@@ -0,0 +1,295 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.services
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.runs
+import io.mockk.slot
+import io.mockk.unmockkAll
+import io.mockk.verify
+
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.transactionManager
+
+import org.ossreviewtoolkit.server.dao.dbQuery
+import org.ossreviewtoolkit.server.model.InfrastructureService
+import org.ossreviewtoolkit.server.model.Secret
+import org.ossreviewtoolkit.server.model.repositories.InfrastructureServiceRepository
+import org.ossreviewtoolkit.server.model.util.ListQueryParameters
+import org.ossreviewtoolkit.server.model.util.OptionalValue
+import org.ossreviewtoolkit.server.model.util.asPresent
+
+class InfrastructureServiceServiceTest : WordSpec({
+    beforeSpec {
+        mockkStatic(
+            "org.ossreviewtoolkit.server.dao.DatabaseKt",
+            "org.jetbrains.exposed.sql.transactions.TransactionApiKt"
+        )
+    }
+
+    afterSpec {
+        unmockkAll()
+    }
+
+    "createForOrganization" should {
+        "create an infrastructure service for an organization" {
+            testWithHelper {
+                val userSecret = mockOrganizationSecret(USERNAME_SECRET)
+                val passSecret = mockOrganizationSecret(PASSWORD_SECRET)
+
+                val infrastructureService = mockk<InfrastructureService>()
+                every {
+                    repository.create(
+                        SERVICE_NAME,
+                        SERVICE_URL,
+                        SERVICE_DESC,
+                        userSecret,
+                        passSecret,
+                        ORGANIZATION_ID,
+                        null
+                    )
+                } returns infrastructureService
+
+                val createResult = service.createForOrganization(
+                    ORGANIZATION_ID,
+                    SERVICE_NAME,
+                    SERVICE_URL,
+                    SERVICE_DESC,
+                    USERNAME_SECRET,
+                    PASSWORD_SECRET
+                )
+
+                createResult shouldBe infrastructureService
+            }
+        }
+
+        "throw an exception if a secret reference cannot be resolved" {
+            testWithHelper(verifyTx = false) {
+                mockOrganizationSecret(USERNAME_SECRET)
+                coEvery {
+                    secretService.getSecretByOrganizationIdAndName(ORGANIZATION_ID, PASSWORD_SECRET)
+                } returns null
+
+                val exception = shouldThrow<InvalidSecretReferenceException> {
+                    service.createForOrganization(
+                        ORGANIZATION_ID,
+                        SERVICE_NAME,
+                        SERVICE_URL,
+                        SERVICE_DESC,
+                        USERNAME_SECRET,
+                        PASSWORD_SECRET
+                    )
+                }
+
+                exception.message shouldContain PASSWORD_SECRET
+            }
+        }
+    }
+
+    "updateForOrganization" should {
+        "update the properties of a service" {
+            testWithHelper {
+                val userSecret = mockOrganizationSecret(USERNAME_SECRET)
+                val passSecret = mockOrganizationSecret(PASSWORD_SECRET)
+
+                val infrastructureService = mockk<InfrastructureService>()
+                every {
+                    repository.updateForOrganizationAndName(
+                        ORGANIZATION_ID,
+                        SERVICE_NAME,
+                        any(),
+                        any(),
+                        any(),
+                        any()
+                    )
+                } returns infrastructureService
+
+                val updateResult = service.updateForOrganization(
+                    ORGANIZATION_ID,
+                    SERVICE_NAME,
+                    SERVICE_URL.asPresent(),
+                    SERVICE_DESC.asPresent(),
+                    USERNAME_SECRET.asPresent(),
+                    PASSWORD_SECRET.asPresent()
+                )
+
+                updateResult shouldBe infrastructureService
+
+                val slotUrl = slot<OptionalValue<String>>()
+                val slotDescription = slot<OptionalValue<String?>>()
+                val slotUsernameSecret = slot<OptionalValue<Secret>>()
+                val slotPasswordSecret = slot<OptionalValue<Secret>>()
+                verify {
+                    repository.updateForOrganizationAndName(
+                        ORGANIZATION_ID,
+                        SERVICE_NAME,
+                        capture(slotUrl),
+                        capture(slotDescription),
+                        capture(slotUsernameSecret),
+                        capture(slotPasswordSecret)
+                    )
+                }
+
+                equalsOptionalValues(SERVICE_URL.asPresent(), slotUrl.captured) shouldBe true
+                equalsOptionalValues(SERVICE_DESC.asPresent(), slotDescription.captured) shouldBe true
+                equalsOptionalValues(userSecret.asPresent(), slotUsernameSecret.captured) shouldBe true
+                equalsOptionalValues(passSecret.asPresent(), slotPasswordSecret.captured) shouldBe true
+            }
+        }
+
+        "throw an exception if a secret reference cannot be resolved" {
+            testWithHelper(verifyTx = false) {
+                coEvery { secretService.getSecretByOrganizationIdAndName(any(), any()) } returns null
+
+                shouldThrow<InvalidSecretReferenceException> {
+                    service.updateForOrganization(
+                        ORGANIZATION_ID,
+                        SERVICE_NAME,
+                        OptionalValue.Absent,
+                        SERVICE_DESC.asPresent(),
+                        "someNonExistingSecret".asPresent(),
+                        OptionalValue.Absent
+                    )
+                }
+            }
+        }
+    }
+
+    "deleteForOrganization" should {
+        "delete an infrastructure service" {
+            testWithHelper {
+                every { repository.deleteForOrganizationAndName(ORGANIZATION_ID, SERVICE_NAME) } just runs
+
+                service.deleteForOrganization(ORGANIZATION_ID, SERVICE_NAME)
+
+                verify {
+                    repository.deleteForOrganizationAndName(ORGANIZATION_ID, SERVICE_NAME)
+                }
+            }
+        }
+    }
+
+    "listForOrganization" should {
+        "return a list with the infrastructure services of the organization" {
+            val services = listOf<InfrastructureService>(mockk(), mockk(), mockk())
+            val parameters = ListQueryParameters(limit = 7, offset = 11)
+
+            testWithHelper {
+                every { repository.listForOrganization(ORGANIZATION_ID, parameters) } returns services
+
+                val result = service.listForOrganization(ORGANIZATION_ID, parameters)
+
+                result shouldBe services
+            }
+        }
+    }
+})
+
+/**
+ * Run a test defined by the given [block] which makes use of a [TestHelper] instance. [Optionally][verifyTx] verify
+ * that a database transaction was used.
+ */
+private suspend fun testWithHelper(verifyTx: Boolean = true, block: suspend TestHelper.() -> Unit) {
+    val helper = TestHelper()
+    helper.block()
+    if (verifyTx) {
+        helper.verifyTransaction()
+    }
+}
+
+/**
+ * A test helper class managing an instance of the service under test and its dependencies.
+ */
+private class TestHelper(
+    /** Mock for the database. */
+    val db: Database = createDatabaseMock(),
+
+    /** Mock for the repository for infrastructure services. */
+    val repository: InfrastructureServiceRepository = mockk(),
+
+    /** Mock for the secret service. */
+    val secretService: SecretService = mockk(),
+
+    /** The service under test. */
+    val service: InfrastructureServiceService = InfrastructureServiceService(db, repository, secretService)
+) {
+    /**
+     * Create a mock [Secret] and prepare the mock [SecretService] to return it when asked for a secret for the
+     * test organization with the given [name].
+     */
+    fun mockOrganizationSecret(name: String): Secret {
+        val secret = mockk<Secret>()
+        coEvery {
+            secretService.getSecretByOrganizationIdAndName(ORGANIZATION_ID, name)
+        } returns secret
+
+        return secret
+    }
+
+    /**
+     * Verify that a database transaction was active.
+     */
+    fun verifyTransaction() {
+        coVerify {
+            db.dbQuery(any(), any(), any(), any())
+        }
+    }
+}
+
+/**
+ * Create a mock [Database]. The mock is prepared to expect invocations of the [Database.dbQuery] function. The
+ * blocks passed to this function are directly executed.
+ */
+private fun createDatabaseMock(): Database =
+    mockk {
+        coEvery { dbQuery(any(), any(), any(), any<() -> Any>()) } answers {
+            val block = arg<() -> Any>(4)
+            block()
+        }
+        every { transactionManager } returns mockk(relaxed = true)
+    }
+
+/**
+ * Check whether the given optional values [v1] and [v2] are equal. This function is needed, since [OptionalValue]
+ * does not implement equals().
+ */
+private fun <T> equalsOptionalValues(v1: OptionalValue<T>, v2: OptionalValue<T>): Boolean =
+    when {
+        v1 is OptionalValue.Present && v2 is OptionalValue.Present -> v1.value == v2.value
+        v1 == OptionalValue.Absent && v2 == OptionalValue.Absent -> true
+        else -> false
+    }
+
+private const val ORGANIZATION_ID = 1000L
+private const val SERVICE_NAME = "TestInfrastructureService"
+private const val SERVICE_URL = "https://repo.example.org/infra/test"
+private const val SERVICE_DESC = "This is a test infrastructure service"
+private const val USERNAME_SECRET = "myUserSecret"
+private const val PASSWORD_SECRET = "myPassSecret"
