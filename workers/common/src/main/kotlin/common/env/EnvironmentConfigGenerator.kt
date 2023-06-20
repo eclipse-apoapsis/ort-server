53d558e1f60814132df75ba11aaa96cbf512c8d6
Oliver Heger <oliver.heger@bosch.io>
2023-06-20 07:24:58 +0200

    feat(workers): Extract a common interface for config generators
    This interface allows handling the generation of package
    manager-specific configuration files in a generic way. This is going
    to simplify the implementation of EnvironmentService.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ workers/common/src/main/kotlin/common/env/EnvironmentConfigGenerator.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.workers.common.env
+
+/**
+ * A common interface for generators for package manager-specific configuration files.
+ *
+ * A generator can process [EnvironmentServiceDefinition]s of a specific type. Based on a collection of such
+ * definitions, it can create a concrete configuration file.
+ */
+interface EnvironmentConfigGenerator<T : EnvironmentServiceDefinition> {
+    /**
+     * The class for the environment definitions supported by this generator. Based on this type, a filtering for the
+     * relevant definitions can be done.
+     */
+    val environmentDefinitionType: Class<T>
+
+    /**
+     * Generate a configuration file for the given [definitions] with the help of the given [builder].
+     */
+    suspend fun generate(builder: ConfigFileBuilder, definitions: Collection<T>)
+
+    /**
+     * Generate a configuration file for the supported definition types in the provided [definitions] with the help of
+     * the given [builder].
+     */
+    suspend fun generateApplicable(builder: ConfigFileBuilder, definitions: Collection<EnvironmentServiceDefinition>) {
+        generate(builder, definitions.filterIsInstance(environmentDefinitionType))
+    }
+}
---
+++ workers/common/src/test/kotlin/common/env/EnvironmentConfigGeneratorTest.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.workers.common.env
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.shouldBe
+
+import io.mockk.mockk
+
+import org.ossreviewtoolkit.server.model.InfrastructureService
+
+class EnvironmentConfigGeneratorTest : StringSpec({
+    "Filtering for applicable definitions works" {
+        val def1 = EnvironmentServiceDefinitionA(mockk())
+        val def2 = EnvironmentServiceDefinitionB(mockk())
+        val def3 = EnvironmentServiceDefinitionB(mockk())
+        val def4 = EnvironmentServiceDefinitionA(mockk())
+        val def5 = EnvironmentServiceDefinitionA(mockk())
+        val def6 = EnvironmentServiceDefinitionB(mockk())
+        val builder = mockk<ConfigFileBuilder>()
+
+        val generator = GeneratorTestImpl()
+        generator.generateApplicable(builder, listOf(def1, def2, def3, def4, def5, def6))
+
+        generator.providedBuilder shouldBe builder
+        generator.providedDefinitions shouldContainExactlyInAnyOrder listOf(def1, def4, def5)
+    }
+})
+
+/**
+ * A test environment service definition class.
+ */
+private class EnvironmentServiceDefinitionA(service: InfrastructureService) : EnvironmentServiceDefinition(service)
+
+/**
+ * Another test environment service definition class.
+ */
+private class EnvironmentServiceDefinitionB(service: InfrastructureService) : EnvironmentServiceDefinition(service)
+
+/**
+ * A test generator implementation that allows inspecting the data passed to its [generate] function.
+ */
+private class GeneratorTestImpl : EnvironmentConfigGenerator<EnvironmentServiceDefinitionA> {
+    /** Stores the builder passed to the [generate] function. */
+    var providedBuilder: ConfigFileBuilder? = null
+
+    /** Stores the definitions passed to the [generate] function. */
+    val providedDefinitions = mutableListOf<EnvironmentServiceDefinition>()
+
+    override val environmentDefinitionType: Class<EnvironmentServiceDefinitionA>
+        get() = EnvironmentServiceDefinitionA::class.java
+
+    override suspend fun generate(builder: ConfigFileBuilder, definitions: Collection<EnvironmentServiceDefinitionA>) {
+        providedBuilder = builder
+        providedDefinitions += definitions
+    }
+}
