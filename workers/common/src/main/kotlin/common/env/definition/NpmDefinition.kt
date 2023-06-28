9c1a596e71e46fb90a80794bd6725c568cc59e46
Oliver Heger <oliver.heger@bosch.io>
2023-06-28 09:34:13 +0200

    feat(workers): Add a class to describe NPM environment definitions
    Using this class, a repository can declare that a specific
    InfrastructureService should be included in the NPM .npmrc
    configuration file.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ workers/common/src/main/kotlin/common/env/definition/NpmDefinition.kt
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.workers.common.env.definition
+
+import org.ossreviewtoolkit.server.model.InfrastructureService
+
+/**
+ * An enumeration class defining the supported ways to declare the authentication against an NPM registry.
+ */
+enum class NpmAuthMode {
+    /**
+     * Authentication is done via the properties `username` and `_password`; the values are obtained directly from the
+     * corresponding secrets defined for the infrastructure service.
+      */
+    PASSWORD,
+
+    /**
+     * Authentication is done via the properties `username` and `_password`; the values are obtained from the
+     * corresponding secrets defined for the infrastructure service, but the value of the password secret is base64
+     * encoded.
+     */
+    PASSWORD_BASE64,
+
+    /**
+     * Authentication is done via the `_auth` property using the password secret from the infrastructure service as
+     * value. The username secret is ignored.
+     */
+    PASSWORD_AUTH,
+
+    /**
+     * Authentication is done via the `_authToken` property using the password secret from the infrastructure service
+     * as value. The username secret is ignored.
+     */
+    PASSWORD_AUTH_TOKEN,
+
+    /**
+     * Authentication is done via the `_auth` property; the value is generated from the username and password secrets
+     * defined for the infrastructure service whose values are concatenated, separated by a colon, and base64 encoded.
+     */
+    USERNAME_PASSWORD_AUTH
+}
+
+/**
+ * A specific [EnvironmentServiceDefinition] class for generating the _.npmrc_ file for the NPM package manager.
+ *
+ * This class defines the NPM-specific configuration of a private registry. The most complex part here is the
+ * authentication. NPM allows multiple options here; to reflect those, the [NpmAuthMode] enumeration class has been
+ * introduced.
+ *
+ * See https://docs.npmjs.com/cli/v9/configuring-npm/npmrc?v=true
+ */
+class NpmDefinition(
+    service: InfrastructureService,
+
+    /**
+     * An optional scope of the registry. If defined, the generated _npmrc_ file will contain an entry that assigns
+     * this scope to the registry, and NPM definition files can reference this scope.
+     */
+    val scope: String? = null,
+
+    /**
+     * An optional email address that may be required for the authentication.
+     */
+    val email: String? = null,
+
+    /**
+     * Defines the way authentication should be handled for this private registry.
+     */
+    val authMode: NpmAuthMode = NpmAuthMode.PASSWORD,
+
+    /**
+     * A flag to control the generation of the `always-auth` property for this registry. Via this flag, NPM can be
+     * instructed to always send authentication information.
+     */
+    val alwaysAuth: Boolean = true
+) : EnvironmentServiceDefinition(service)
