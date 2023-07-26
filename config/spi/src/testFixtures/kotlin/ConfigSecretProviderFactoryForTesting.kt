3705eff812a6e32a03089b8b88a5838c95bbdcbc
Oliver Heger <oliver.heger@bosch.io>
2023-07-26 08:07:56 +0200

    test(config): Add a test implementation for querying secrets
    This implementation is going to be used for testing whether
    ConfigManager correctly wraps a ConfigSecretProvider. It is also
    useful for tests in other modules that need to access secrets from the
    configuration.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ config/spi/src/testFixtures/kotlin/ConfigSecretProviderFactoryForTesting.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.config
+
+import com.typesafe.config.Config
+
+/**
+ * A test implementation of the [ConfigSecretProvider] interface. This implementation reads the known secrets from
+ * the configuration passed to the factory. Thus, it can be customized for being used in tests quite easily.
+ */
+class ConfigSecretProviderFactoryForTesting : ConfigSecretProviderFactory {
+    companion object {
+        /** The name of this test implementation. */
+        const val NAME = "configSecretProviderForTesting"
+
+        /**
+         * The name of a configuration property under which the test secrets are located. When an instance is created,
+         * it reads this section and exposes all the properties contained as secrets.
+         */
+        const val SECRETS_PROPERTY = "testSecrets"
+    }
+
+    override val name: String
+        get() = NAME
+
+    override fun createProvider(config: Config): ConfigSecretProvider {
+        val secrets = config.getConfig(SECRETS_PROPERTY)
+            .entrySet().associate { it.key to it.value.unwrapped().toString() }
+
+        return object : ConfigSecretProvider {
+            override fun getSecret(path: Path): String {
+                return secrets.getValue(path.path)
+            }
+        }
+    }
+}
