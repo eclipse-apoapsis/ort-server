5aa1ba3f7d754779446612ebf45584269b96b01b
Oleksandr Yarosevych <fixed-term.oleksandr.yarosevych@bosch.io>
2023-08-02 16:41:02 +0200

    feat(workers): Add environment service definition for Yarn registries
    Signed-off-by: Oleksandr Yarosevych <fixed-term.oleksandr.yarosevych@bosch.io>

---
+++ workers/common/src/main/kotlin/common/env/definition/YarnDefinition.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.workers.common.env.definition
+
+import org.ossreviewtoolkit.server.model.InfrastructureService
+
+/**
+ * A specific [EnvironmentServiceDefinition] class for generating the Yarn _.yarnrc.yml_ file with configuration
+ * specific to registries.
+ *
+ * See https://yarnpkg.com/configuration/yarnrc#npmRegistries
+ */
+class YarnDefinition(
+    service: InfrastructureService,
+
+    /**
+     * Represents the registry URI.
+     */
+    val registryUri: String,
+
+    /**
+     * A flag to control the generation of the `mpmAlwaysAuth` property for this registry. Via this flag, Yarn can be
+     * instructed to always send authentication information.
+     */
+    val alwaysAuth: Boolean = true,
+
+    /**
+     * Defines the way authentication should be handled for this private registry.
+     */
+    val authMode: YarnAuthMode = YarnAuthMode.AUTH_TOKEN
+) : EnvironmentServiceDefinition(service)
+
+enum class YarnAuthMode {
+    /**
+     * Authentication is done via the `npmAuthIdent` property using the username and password secrets from the
+     * infrastructure service as values. This authentication is strongly discouraged in favor of [AUTH_TOKEN]
+     */
+    AUTH_IDENT,
+
+    /**
+     * Authentication is done via the `npmAuthToken` property using the password secret from the infrastructure service
+     * as a value. The username secret is ignored.
+     */
+    AUTH_TOKEN
+}
