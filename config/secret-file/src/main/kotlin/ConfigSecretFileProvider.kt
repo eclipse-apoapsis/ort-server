49e3a90cdf51bf2c69565b576fd7934a33d19252
Oliver Heger <oliver.heger@bosch.io>
2023-08-07 15:47:52 +0200

    feat(config): Add ConfigSecretFileProvider class
    This is an implementation of the ConfigSecretProvider interface that
    reads the values of secrets from one or multiple files. It is
    configured with the files it should read.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ config/secret-file/src/main/kotlin/ConfigSecretFileProvider.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.config.secret.file
+
+import java.io.File
+
+import org.ossreviewtoolkit.server.config.ConfigSecretProvider
+import org.ossreviewtoolkit.server.config.Path
+
+/**
+ * An implementation of the [ConfigSecretProvider] interface that reads secrets from files.
+ *
+ * An instance is initialized with a collection of [File]s that contain the available secrets. Each file is expected to
+ * be a text file that defines secrets in its lines. The syntax corresponds to properties file; so each line should be
+ * of the form `key=value`. Empty lines and lines starting with a '#' character are ignored.
+ *
+ * When the provider is queried for a secret, it reads the files in the provided order, line by line, until it finds
+ * a key that matches the requested path. The value defined on this line is returned as secret value. So if there are
+ * conflicting secret values, the first one that is encountered wins. This makes it possible to override selected
+ * values in files.
+ *
+ * No caching is applied, so the files are read on each request for a secret. This is less efficient, but allows
+ * picking up changes on secret values immediately.
+ */
+class ConfigSecretFileProvider(
+    /**
+     * The list of files with secrets to be read by this instance. The files are read in the provided order, so the
+     * order is relevant.
+     */
+    private val secretFiles: Collection<File>
+) : ConfigSecretProvider {
+    companion object {
+        /** The character separating a secret name from its value. */
+        private const val KEY_VALUE_SEPARATOR = '='
+
+        /** A character marking a line as comment line. */
+        private const val COMMENT_CHARACTER = '#'
+
+        /** A regular expression to split a line in the file into the secret name and its value. */
+        private val splitKeyValueRegex = Regex("""\s*$KEY_VALUE_SEPARATOR\s*""")
+    }
+
+    override fun getSecret(path: Path): String {
+        return getSecretValue(path.path, secretFiles.iterator())
+    }
+
+    private tailrec fun getSecretValue(name: String, filesIterator: Iterator<File>): String =
+        if (filesIterator.hasNext()) {
+            val matchingLine = filesIterator.next().bufferedReader().use { reader ->
+                reader.lineSequence().map { it.trim() }
+                    .filterNot { it.isEmpty() || it.startsWith(COMMENT_CHARACTER) || KEY_VALUE_SEPARATOR !in it }
+                    .map { it.split(splitKeyValueRegex, limit = 2) }
+                    .firstOrNull { it.first() == name }
+            }
+
+            if (matchingLine != null) {
+                matchingLine.takeIf { it.size == 2 }?.get(1).orEmpty()
+            } else {
+                getSecretValue(name, filesIterator)
+            }
+        } else {
+            throw NoSuchElementException("No value found for secret '$name'.")
+        }
+}
---
+++ config/secret-file/src/test/kotlin/ConfigSecretFileProviderTest.kt
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.config.secret.file
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.engine.spec.tempfile
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+
+import java.io.File
+import java.io.IOException
+
+import org.ossreviewtoolkit.server.config.Path
+
+class ConfigSecretFileProviderTest : StringSpec({
+    fun createSecretsFile(content: String): File =
+        tempfile().also { it.writeText(content) }
+
+    "A correct secret value should be returned" {
+        val secretFile = createSecretsFile(SECRETS_FILE_CONTENT)
+
+        val provider = ConfigSecretFileProvider(listOf(secretFile))
+        val secret = provider.getSecret(Path("secret1"))
+
+        secret shouldBe "value1"
+    }
+
+    "Keys and values should be trimmed correctly" {
+        val secretFile = createSecretsFile(SECRETS_FILE_CONTENT)
+
+        val provider = ConfigSecretFileProvider(listOf(secretFile))
+        val secret = provider.getSecret(Path("secret2"))
+
+        secret shouldBe "value2"
+    }
+
+    "An empty secret value should be returned" {
+        val secretFile = createSecretsFile(SECRETS_FILE_CONTENT)
+
+        val provider = ConfigSecretFileProvider(listOf(secretFile))
+        val secret = provider.getSecret(Path("secret3"))
+
+        secret shouldBe ""
+    }
+
+    "An exception is thrown for an unresolvable secret" {
+        val secretName = "secretInvalid"
+        val secretFile = createSecretsFile(SECRETS_FILE_CONTENT)
+
+        val provider = ConfigSecretFileProvider(listOf(secretFile))
+
+        val exception = shouldThrow<NoSuchElementException> {
+            provider.getSecret(Path(secretName))
+        }
+
+        exception.message shouldContain secretName
+    }
+
+    "Multiple files should be searched" {
+        val file1 = createSecretsFile("foo=bar")
+        val file2 = createSecretsFile(SECRETS_FILE_CONTENT)
+
+        val provider = ConfigSecretFileProvider(listOf(file1, file2))
+        val secret = provider.getSecret(Path("secret1"))
+
+        secret shouldBe "value1"
+    }
+
+    "Secret values from a file with a lower index in the list should take priority over values from later files" {
+        val secretName = "secret1"
+        val secretValue = "overridden value"
+        val file1 = createSecretsFile("$secretName=$secretValue")
+        val file2 = createSecretsFile(SECRETS_FILE_CONTENT)
+
+        val provider = ConfigSecretFileProvider(listOf(file1, file2))
+        val secret = provider.getSecret(Path("secret1"))
+
+        secret shouldBe secretValue
+    }
+
+    "Secret values containing the key-value separator should be handled correctly" {
+        val secretName = "secretWithComplexValue"
+        val secretValue = "top=secret"
+        val file = createSecretsFile("$secretName=$secretValue")
+
+        val provider = ConfigSecretFileProvider(listOf(file))
+        val secret = provider.getSecret(Path(secretName))
+
+        secret shouldBe secretValue
+    }
+
+    "A non-existing file should cause an IOException" {
+        val provider = ConfigSecretFileProvider(listOf(File("non-existing-file.sec")))
+
+        shouldThrow<IOException> {
+            provider.getSecret(Path("doesNotMatter"))
+        }
+    }
+})
+
+/** The content of a test file with some secret definitions. */
+private val SECRETS_FILE_CONTENT = """
+                # Test secrets
+                secret1=value1
+                secret2   =      value2  
+                secret3=
+                
+                secretInvalid
+            """.trimIndent()
