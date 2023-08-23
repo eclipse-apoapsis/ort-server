b27eb1ed6ca9cb45e82177c83b297641ca82afac
Oleksandr Yarosevych <fixed-term.oleksandr.yarosevych@bosch.io>
2023-08-23 13:16:20 +0200

    feat(workers): Implement Conan's remotes.json file generator
    Signed-off-by: Oleksandr Yarosevych <fixed-term.oleksandr.yarosevych@bosch.io>

---
+++ workers/common/src/main/kotlin/common/env/ConanGenerator.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.workers.common.env
+
+import org.ossreviewtoolkit.server.workers.common.env.definition.ConanDefinition
+
+/**
+ * A specialized generator class for Conan's _remotes.json_ configuration files.
+ *
+ * See: https://docs.conan.io/2.0/reference/config_files/remotes.html
+ */
+class ConanGenerator : EnvironmentConfigGenerator<ConanDefinition> {
+    companion object {
+        /** The name of the configuration file created by this generator. */
+        private const val TARGET = ".conan/remotes.json"
+    }
+
+    override val environmentDefinitionType: Class<ConanDefinition>
+        get() = ConanDefinition::class.java
+
+    override suspend fun generate(builder: ConfigFileBuilder, definitions: Collection<ConanDefinition>) {
+        builder.buildInUserHome(TARGET) {
+            println("{")
+            println("\"remotes\": [".prependIndent(INDENT_2_SPACES))
+            definitions.forEachIndexed { index, definition ->
+                if (index > 0) {
+                    // Print a comma and a line break before each definition for multi-definition remote.json files
+                    println(",")
+                }
+
+                println("{".prependIndent(INDENT_4_SPACES))
+
+                println("\"name\": \"${definition.name}\",".prependIndent(INDENT_6_SPACES))
+                println("\"url\": \"${definition.url}\",".prependIndent(INDENT_6_SPACES))
+                println("\"verify_ssl\": ${definition.verifySsl}".prependIndent(INDENT_6_SPACES))
+
+                print("}".prependIndent(INDENT_4_SPACES))
+            }
+            println()
+            println("]".prependIndent(INDENT_2_SPACES))
+            println("}")
+        }
+    }
+}
---
+++ workers/common/src/test/kotlin/common/env/ConanGeneratorTest.kt
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.workers.common.common.env
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.shouldBe
+
+import org.ossreviewtoolkit.server.workers.common.env.ConanGenerator
+import org.ossreviewtoolkit.server.workers.common.env.MockConfigFileBuilder
+import org.ossreviewtoolkit.server.workers.common.env.definition.ConanDefinition
+
+class ConanGeneratorTest : WordSpec({
+    "environmentDefinitionType" should {
+        "return the correct definition class" {
+            val definitionType = ConanGenerator().environmentDefinitionType
+
+            definitionType shouldBe ConanDefinition::class.java
+        }
+    }
+
+    "generate" should {
+        "generate the file at the correct location" {
+            val definition = ConanDefinition(
+                MockConfigFileBuilder.createInfrastructureService(REMOTE_URL),
+                REMOTE_NAME,
+                REMOTE_URL,
+                true
+            )
+
+            val mockBuilder = MockConfigFileBuilder()
+
+            ConanGenerator().generate(mockBuilder.builder, listOf(definition))
+
+            mockBuilder.homeFileName shouldBe ".conan/remotes.json"
+        }
+
+        "generate a file with a single remote" {
+            val definition = ConanDefinition(
+                MockConfigFileBuilder.createInfrastructureService(REMOTE_URL),
+                REMOTE_NAME,
+                REMOTE_URL,
+                true
+            )
+
+            val mockBuilder = MockConfigFileBuilder()
+
+            ConanGenerator().generate(mockBuilder.builder, listOf(definition))
+
+            val expectedLines = listOf(
+                "{",
+                "  \"remotes\": [",
+                "    {",
+                "      \"name\": \"$REMOTE_NAME\",",
+                "      \"url\": \"$REMOTE_URL\",",
+                "      \"verify_ssl\": true",
+                "    }",
+                "  ]",
+                "}"
+            )
+            val lines = mockBuilder.generatedLines()
+            lines shouldContainExactly expectedLines
+        }
+
+        "generate a file with several remotes" {
+            val definitions = listOf(
+                ConanDefinition(
+                    MockConfigFileBuilder.createInfrastructureService(REMOTE_URL),
+                    REMOTE_NAME,
+                    REMOTE_URL,
+                    true
+                ),
+                ConanDefinition(
+                    MockConfigFileBuilder.createInfrastructureService(REMOTE_URL + 1),
+                    REMOTE_NAME + 1,
+                    REMOTE_URL + 1,
+                    false
+                )
+            )
+
+            val mockBuilder = MockConfigFileBuilder()
+
+            ConanGenerator().generate(mockBuilder.builder, definitions)
+
+            val expectedLines = listOf(
+                "{",
+                "  \"remotes\": [",
+                "    {",
+                "      \"name\": \"$REMOTE_NAME\",",
+                "      \"url\": \"$REMOTE_URL\",",
+                "      \"verify_ssl\": true",
+                "    },",
+                "    {",
+                "      \"name\": \"${REMOTE_NAME + 1}\",",
+                "      \"url\": \"${REMOTE_URL + 1}\",",
+                "      \"verify_ssl\": false",
+                "    }",
+                "  ]",
+                "}"
+            )
+            val lines = mockBuilder.generatedLines()
+            lines shouldContainExactly expectedLines
+        }
+    }
+})
+
+const val REMOTE_NAME = "conancenter"
+const val REMOTE_URL = "https://center.conan.io"
