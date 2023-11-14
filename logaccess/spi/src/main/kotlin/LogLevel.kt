9e793fd2c2934e3f927ed3b12ea92a152ae98c5c
Oliver Heger <oliver.heger@bosch.io>
2023-11-14 13:11:14 +0100

    feat(logaccess): Add a LogLevel enumeration
    This class defines constants for the supported log levels, which can
    be used to specify the desired content of log files to be downloaded.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ logaccess/spi/src/main/kotlin/LogLevel.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.logaccess
+
+import java.util.EnumSet
+
+/**
+ * An enumeration class defining the supported log levels in ORT Server log files.
+ *
+ * Using the constants provided by this class, users can filter for specific log information when downloading log
+ * files.
+ */
+enum class LogLevel {
+    /** The log level DEBUG. */
+    DEBUG,
+
+    /** The log level INFO. */
+    INFO,
+
+    /** The log level WARN. */
+    WARN,
+
+    /** The log level ERROR. */
+    ERROR;
+
+    companion object {
+        /**
+         * Return a [Set] containing the given [level] and all [LogLevel]s that are higher than this one. When
+         * filtering log files for a specific level the semantics is typically that not only log statements with the
+         * given level are selected but also statements with levels of increased severity. For instance a log file of
+         * level [INFO] contains logs with the levels [WARN] and [ERROR] as well. This function can be used to obtain
+         * all the levels to be taken into account when applying such a filter.
+         */
+        fun levelOrHigher(level: LogLevel): Set<LogLevel> = EnumSet.range(level, ERROR)
+    }
+}
---
+++ logaccess/spi/src/test/kotlin/LogLevelTest.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.logaccess
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+
+class LogLevelTest : WordSpec({
+    "levelOrHigher" should {
+        "return a correct set for the ERROR level" {
+            LogLevel.levelOrHigher(LogLevel.ERROR) shouldContainExactlyInAnyOrder setOf(LogLevel.ERROR)
+        }
+
+        "return a correct set for the WARN level" {
+            LogLevel.levelOrHigher(LogLevel.WARN) shouldContainExactlyInAnyOrder setOf(
+                LogLevel.ERROR,
+                LogLevel.WARN
+            )
+        }
+
+        "return a correct set for the INFO level" {
+            LogLevel.levelOrHigher(LogLevel.INFO) shouldContainExactlyInAnyOrder setOf(
+                LogLevel.ERROR,
+                LogLevel.WARN,
+                LogLevel.INFO
+            )
+        }
+
+        "return a correct set for the DEBUG level" {
+            LogLevel.levelOrHigher(LogLevel.DEBUG) shouldContainExactlyInAnyOrder setOf(
+                LogLevel.ERROR,
+                LogLevel.WARN,
+                LogLevel.INFO,
+                LogLevel.DEBUG
+            )
+        }
+    }
+})
