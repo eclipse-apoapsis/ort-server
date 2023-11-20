61d338f207250fbd3b6b1dbf24835747f7ccc7a5
Oliver Heger <oliver.heger@bosch.io>
2023-11-20 08:35:06 +0100

    feat(logaccess): Add LokiConfig class
    This class collects the configuration properties supported by the
    Grafana Loki LogFileProvider implementation.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ logaccess/loki/src/main/kotlin/LokiConfig.kt
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.logaccess.loki
+
+import org.ossreviewtoolkit.server.config.ConfigManager
+import org.ossreviewtoolkit.server.config.Path
+import org.ossreviewtoolkit.server.utils.config.getIntOrDefault
+import org.ossreviewtoolkit.server.utils.config.getStringOrNull
+
+/**
+ * A data class storing configuration options for accessing a Grafana Loki instance.
+ *
+ * Note that according to [1] Loki does not support an authentication by itself, but requires a reverse proxy for this
+ * purpose. This means that in theory multiple different options for authentication could be supported. This
+ * implementation is currently limited to an optional basic authentication: If both a username and password are
+ * configured, the corresponding authorization header is set; otherwise, it is skipped.
+ *
+ * [1] https://grafana.com/docs/loki/latest/operations/authentication/
+ */
+data class LokiConfig(
+    /**
+     * The URL of the Grafana Loki server to interact with. Note that this is the root URL; the path to the endpoint
+     * ("/loki/api/v1/...") is appended automatically.
+     */
+    val serverUrl: String,
+
+    /**
+     * The namespace for which logs are to be retrieved. This has to be added to query strings sent to Loki.
+     */
+    val namespace: String,
+
+    /**
+     * A limit to be passed as parameter when calling the query endpoint. The Loki API expects such a limit parameter
+     * or uses a (rather small) limit as default.
+     */
+    val limit: Int,
+
+    /** An optional username for basic auth. */
+    val username: String?,
+
+    /** An optional password for basic auth. */
+    val password: String?,
+) {
+    companion object {
+        /** The configuration property that defines the root URL of the Loki server. */
+        private const val SERVER_URL_PROPERTY = "lokiServerUrl"
+
+        /** The configuration property that defines the namespace from which logs are to be gathered. */
+        private const val NAMESPACE_PROPERTY = "lokiNamespace"
+
+        /** The configuration property that defines the username for basic authentication. */
+        private const val USERNAME_PROPERTY = "lokiUsername"
+
+        /** The configuration property that defines the password for basic authentication. */
+        private const val PASSWORD_PROPERTY = "lokiPassword"
+
+        /** The configuration property that defines a limit for query results. */
+        private const val LIMIT_PROPERTY = "lokiQueryLimit"
+
+        /** The default limit to be passed to the query endpoint. */
+        private const val DEFAULT_LIMIT = 1000
+
+        /**
+         * Return a new instance of [LokiConfig] that has been initialized from the passed in [configManager].
+         */
+        fun create(configManager: ConfigManager): LokiConfig {
+            val username = configManager.getStringOrNull(USERNAME_PROPERTY)
+            val password = if (username == null) {
+                null
+            } else {
+                configManager.getSecret(Path(PASSWORD_PROPERTY))
+            }
+
+            return LokiConfig(
+                configManager.getString(SERVER_URL_PROPERTY),
+                configManager.getString(NAMESPACE_PROPERTY),
+                configManager.getIntOrDefault(LIMIT_PROPERTY, DEFAULT_LIMIT),
+                username,
+                password
+            )
+        }
+    }
+}
---
+++ logaccess/loki/src/test/kotlin/LokiConfigTest.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.logaccess.loki
+
+import com.typesafe.config.ConfigFactory
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+import io.mockk.mockk
+
+import org.ossreviewtoolkit.server.config.ConfigManager
+
+class LokiConfigTest : StringSpec({
+    "An instance should be created from a ConfigManager" {
+        val limit = 4321
+        val username = "lokiUser"
+        val password = "lokiPass"
+        val configMap = mapOf(
+            "lokiServerUrl" to SERVER_URL,
+            "lokiNamespace" to NAMESPACE,
+            "lokiQueryLimit" to limit,
+            "lokiUsername" to username,
+            "lokiPassword" to password
+        )
+        val configManager = createConfigManager(configMap)
+        val expectedConfig = LokiConfig(SERVER_URL, NAMESPACE, limit, username, password)
+
+        val lokiConfig = LokiConfig.create(configManager)
+
+        lokiConfig shouldBe expectedConfig
+    }
+
+    "An instance should be created with default properties" {
+        val configMap = mapOf(
+            "lokiServerUrl" to SERVER_URL,
+            "lokiNamespace" to NAMESPACE
+        )
+        val configManager = createConfigManager(configMap)
+        val expectedConfig = LokiConfig(SERVER_URL, NAMESPACE, 1000, null, null)
+
+        val lokiConfig = LokiConfig.create(configManager)
+
+        lokiConfig shouldBe expectedConfig
+    }
+})
+
+/**
+ * Return a [ConfigManager] object with a configuration that contains the properties of the provided [configMap].
+ */
+private fun createConfigManager(configMap: Map<String, Any>): ConfigManager =
+    ConfigManager(
+        ConfigFactory.parseMap(configMap),
+        { mockk() },
+        { mockk() },
+        allowSecretsFromConfig = true
+    )
+
+private const val SERVER_URL = "https://loki.example.org"
+private const val NAMESPACE = "test_namespace"
