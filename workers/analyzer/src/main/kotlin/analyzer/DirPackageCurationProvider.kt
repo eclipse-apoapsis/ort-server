a8f5846374f8992c4786b47e10fbb320149e4895
Oliver Heger <oliver.heger@bosch.io>
2023-12-06 15:43:40 +0100

    feat(analyzer): Add a custom PackageCurationProvider implementation
    This implementation expects curation files in a folder structure
    following certain naming conventions. It differs from ORT's
    `FilePackageCurationProvider` in that it does not load all curation
    files initially, but resolves the files for packages on demand. For
    the intended usage scenario - the provider is used for a single
    Analyzer run -, this skips the overhead of initialization, which can
    be significant for a large number of curations or if files are loaded
    from a slow network share.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ workers/analyzer/src/main/kotlin/analyzer/DirPackageCurationProvider.kt
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.workers.analyzer
+
+import java.io.File
+
+import org.ossreviewtoolkit.model.Identifier
+import org.ossreviewtoolkit.model.Package
+import org.ossreviewtoolkit.model.PackageCuration
+import org.ossreviewtoolkit.model.readValue
+import org.ossreviewtoolkit.model.utils.PackageCurationProvider
+import org.ossreviewtoolkit.plugins.packagecurationproviders.api.PackageCurationProviderFactory
+import org.ossreviewtoolkit.utils.common.Options
+import org.ossreviewtoolkit.utils.common.encodeOr
+
+import org.slf4j.LoggerFactory
+
+private val logger = LoggerFactory.getLogger(DirPackageCurationProvider::class.java)
+
+/**
+ * A specialized [PackageCurationProvider] implementation that reads curation data from a folder structure that
+ * adheres to specific conventions.
+ *
+ * The expected folder structure is based on the components of an identifier. On top level, a directory corresponding
+ * to the package type must be present. On the next level, the provider searches for a subfolder whose name is equal to
+ * the namespace (or an underscore character if the namespace is undefined). In this subfolder, a file is expected
+ * whose name matches the identifier's _name_ component with the extension `.yml`. This file can contain package
+ * curations for the different versions of this package. So, for instance, curations for the package
+ * `Maven:org.apache.commons:commons-lang3:3.12` would be looked up in a file named `commons-lang3.yml` under the
+ * relative path `Maven/org.apache.commons`.
+ *
+ * There are some corner cases with identifiers that cannot be mapped directly to paths on a file system. These are
+ * handled as follows:
+ * - An empty component of the identifier (e.g. a missing namespace) is mapped to the underscore character ("_").
+ * - Some special characters that are problematic in file names are mapped to their numeric hexadecimal representation
+ *   (%XX).
+ */
+class DirPackageCurationProvider(
+    /** The root path of the folder structure with curation files. */
+    private val root: File
+) : PackageCurationProvider {
+    override fun getCurationsFor(packages: Collection<Package>): Set<PackageCuration> {
+        return packages.flatMapTo(mutableSetOf()) { pkg ->
+            val curationFile = root.resolve(pkg.id.toCurationPath())
+            logger.debug("Looking up curation file '{}'.", curationFile.absolutePath)
+
+            curationFile.takeIf { it.isFile }?.readValue<List<PackageCuration>>().orEmpty().filter {
+                it.isApplicable(pkg.id)
+            }
+        }
+    }
+}
+
+/**
+ * A data class defining the configuration options supported by [DirPackageCurationProvider].
+ */
+data class DirPackageCurationProviderConfig(
+    /** The root path of the folder structure to be searched by the provider. */
+    val path: String
+)
+
+/**
+ * A factory implementation for creating [DirPackageCurationProvider] instances.
+ */
+class DirPackageCurationProviderFactory : PackageCurationProviderFactory<DirPackageCurationProviderConfig> {
+    override val type: String
+        get() = "Dir"
+
+    override fun create(config: DirPackageCurationProviderConfig): PackageCurationProvider {
+        logger.info("Creating DirPackageCurationProvider using root path '{}'.", config.path)
+
+        return DirPackageCurationProvider(File(config.path))
+    }
+
+    override fun parseConfig(options: Options, secrets: Options): DirPackageCurationProviderConfig =
+        DirPackageCurationProviderConfig(options.getValue("path"))
+}
+
+/**
+ * The path must be aligned with the
+ * [conventions for the ort-config repository](https://github.com/oss-review-toolkit/ort-config#curations).
+ */
+private fun Identifier.toCurationPath() = "${type.encodeOr("_")}/${namespace.encodeOr("_")}/${name.encodeOr("_")}.yml"
---
+++ workers/analyzer/src/test/kotlin/analyzer/DirPackageCurationProviderTest.kt
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2023 The ORT Project Authors (See <https://github.com/oss-review-toolkit/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.ossreviewtoolkit.server.workers.analyzer
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.engine.spec.tempdir
+import io.kotest.matchers.collections.beEmpty
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.should
+
+import java.io.File
+
+import org.ossreviewtoolkit.model.Identifier
+import org.ossreviewtoolkit.model.Package
+import org.ossreviewtoolkit.model.PackageCuration
+import org.ossreviewtoolkit.model.PackageCurationData
+import org.ossreviewtoolkit.model.config.ProviderPluginConfiguration
+import org.ossreviewtoolkit.model.utils.PackageCurationProvider
+import org.ossreviewtoolkit.model.utils.toPurl
+import org.ossreviewtoolkit.model.writeValue
+import org.ossreviewtoolkit.plugins.packagecurationproviders.api.PackageCurationProviderFactory
+
+class DirPackageCurationProviderTest : StringSpec({
+    "An empty set should be returned if no package curations are found" {
+        val root = tempdir()
+        val provider = createProvider(root)
+
+        val curations = provider.getCurationsFor(listOf(packageLang, packageText))
+
+        curations should beEmpty()
+    }
+
+    "The correct package curations should be returned" {
+        val root = tempdir()
+        val curationFile = root.resolve(PACKAGE_TYPE).resolve(NAMESPACE).resolve("$NAME.yml")
+        val curation = createCuration(packageLang.id)
+        curationFile.writeValue(listOf(curation, createCuration(packageLang.id.copy(version = "1.0"))))
+
+        val provider = createProvider(root)
+
+        val curations = provider.getCurationsFor(listOf(packageText, packageLang))
+
+        curations shouldContainExactlyInAnyOrder listOf(curation)
+    }
+
+    "Special characters in identifiers should be encoded" {
+        val root = tempdir()
+        val packageAngular = Package.EMPTY.copy(id = Identifier("NPM", "@angular", "animations", VERSION))
+        val angularCurationsFile = root.resolve("NPM").resolve("%40angular").resolve("animations.yml")
+        val angularCuration = createCuration(packageAngular.id)
+        angularCurationsFile.writeValue(listOf(angularCuration))
+
+        val packageCloud = Package.EMPTY.copy(id = Identifier("Go", "", "cloud.google.com/go", VERSION))
+        val cloudCurationsFile = root.resolve("Go").resolve("_").resolve("cloud.google.com%2Fgo.yml")
+        val cloudCuration = createCuration(packageCloud.id)
+        cloudCurationsFile.writeValue(listOf(cloudCuration))
+
+        val provider = createProvider(root)
+
+        val curations = provider.getCurationsFor(listOf(packageAngular, packageCloud))
+
+        curations shouldContainExactlyInAnyOrder listOf(angularCuration, cloudCuration)
+    }
+})
+
+private const val PACKAGE_TYPE = "Maven"
+private const val NAMESPACE = "org.apache.commons"
+private const val NAME = "commons-lang3"
+private const val VERSION = "3.12.0"
+
+private val packageLang = Package.EMPTY.copy(id = Identifier(PACKAGE_TYPE, NAMESPACE, NAME, VERSION))
+private val packageText = Package.EMPTY.copy(id = Identifier(PACKAGE_TYPE, NAMESPACE, "commons-text", "1.10.0"))
+
+/**
+ * Create a [DirPackageCurationProvider] test instance using the official plugin mechanism that looks up curations in
+ * the specified [root] folder.
+ */
+private fun createProvider(root: File): PackageCurationProvider {
+    val config = ProviderPluginConfiguration(
+        type = "Dir",
+        options = mapOf("path" to root.absolutePath)
+    )
+
+    return PackageCurationProviderFactory.create(listOf(config)).single().second
+}
+
+/**
+ * Return a test [PackageCuration] for a package with the given [id].
+ */
+private fun createCuration(id: Identifier): PackageCuration =
+    PackageCuration(
+        id = id,
+        data = PackageCurationData(
+            comment = "This is a curation for package ${id.toCoordinates()}.",
+            purl = id.toPurl()
+        )
+    )
