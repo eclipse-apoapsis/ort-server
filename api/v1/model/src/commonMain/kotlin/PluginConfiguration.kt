c1b624986352b223ce3fb8ce43ee43b68dcf7cf4
Martin Nonnenmacher <martin.nonnenmacher@bosch.com>
2024-02-29 15:15:19 +0100

    fix(api): Use the `Options` typealias from the API model
    Use the `Options` typealias from the API model instead of the business
    model in `PluginConfiguration`.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.com>

--- api/v1/model/src/commonMain/kotlin/PluginConfiguration.kt
+++ api/v1/model/src/commonMain/kotlin/PluginConfiguration.kt
@@ -21,8 +21,6 @@ package org.eclipse.apoapsis.ortserver.api.v1.model
 
 import kotlinx.serialization.Serializable
 
-import org.eclipse.apoapsis.ortserver.model.Options
-
 @Serializable
 data class PluginConfiguration(
     /**
