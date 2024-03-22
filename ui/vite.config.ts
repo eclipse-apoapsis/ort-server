1474616ea5d4b2a2578e83ad6e87ed905ee6463c
Mikko Murto <mikko.murto@doubleopen.org>
2024-03-22 20:50:49 +0200

    feat(ui): Add TanStack Router
    TanStack Router provides the routing functionality for the UI. TanStack
    Router is well suited for dashboard applications and provides a more
    type-safe development experience compared to other routing libraries, such
    as React Router or Next.js router.
    
    Signed-off-by: Mikko Murto <mikko.murto@doubleopen.org>

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -19,8 +19,9 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), TanStackRouterVite()],
+});
