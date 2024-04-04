56cb36bb32515a6db84c429beddeead19259d35a
Sebastian Schuberth <sebastian@doubleopen.org>
2024-04-04 12:32:59 +0200

    build(Gradle): Mark Jib tasks as incompatible with configuration cache
    See [1].
    
    [1]: https://docs.gradle.org/current/kotlin-dsl/gradle/org.gradle.api/-task/not-compatible-with-configuration-cache.html
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -17,6 +17,8 @@
  * License-Filename: LICENSE
  */
 
+import com.google.cloud.tools.jib.gradle.JibTask
+
 val dockerImagePrefix: String by project
 val dockerImageTag: String by project
 
@@ -35,6 +37,10 @@ application {
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+tasks.withType<JibTask> {
+    notCompatibleWithConfigurationCache("https://github.com/GoogleContainerTools/jib/issues/3132")
 }
 
 tasks.withType<Test>().configureEach {
