64e1f58e7d7a97c432de14b9670e03fd9a7eadf4
Martin Nonnenmacher <martin.nonnenmacher@bosch.com>
2024-04-17 22:56:20 +0200

    fix(compose): Configure the config-worker
    Add configuration for the config-worker to `docker-compose.yml`. Also
    add the required `RabbitMQ` queue and a simple example for a parameter
    validation script.
    
    The parameter validation script currently disables the notifier job as
    the notifier-worker is not configured in Docker Compose.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.com>

---
+++ scripts/compose/config/ort-server.params.kts
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2024 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import org.ossreviewtoolkit.utils.ort.Environment
+
+val defaultReportFormats = listOf(
+    "cyclonedx",
+    "ortresult",
+    "runstatistics",
+    "spdxdocument",
+    "webapp"
+)
+
+// Add default report formats in case no formats are specified.
+val resolvedReporterJobConfig = context.ortRun.jobConfigs.reporter?.let {
+    when {
+        it.formats.isEmpty() -> it.copy(formats = defaultReportFormats)
+        else -> it
+    }
+}
+
+// Disable the notifier job as the notifier-worker is currently not configured in Docker Compose.
+val resolvedJobConfigs = context.ortRun.jobConfigs.copy(reporter = resolvedReporterJobConfig, notifier = null)
+
+validationResult = ConfigValidationResultSuccess(
+    resolvedConfigurations = resolvedJobConfigs,
+    labels = mapOf(
+        "createdBy" to "ORT Server $ORT_SERVER_VERSION running ORT ${Environment.ORT_VERSION} in Docker Compose."
+    )
+)
