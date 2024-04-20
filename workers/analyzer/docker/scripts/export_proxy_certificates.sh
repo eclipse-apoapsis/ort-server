81d01917c7998add92067665c7b3ea3d523656ce
Martin Nonnenmacher <martin.nonnenmacher@bosch.com>
2024-04-20 19:18:11 +0200

    chore(analyzer): Add ORT Project Authors as copyright holders
    The `Analyzer.Dockerfile` and related scripts are based on the
    respective files of the OSS Review Toolkit [1]. Add the required
    copyright statements for correct attribution.
    
    [1]: https://github.com/oss-review-toolkit/ort

--- workers/analyzer/docker/scripts/export_proxy_certificates.sh
+++ workers/analyzer/docker/scripts/export_proxy_certificates.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+# Copyright (C) 2020 The ORT Project Authors (see <https://github.com/oss-review-toolkit/ort/blob/main/NOTICE>)
 # Copyright (C) 2023 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
