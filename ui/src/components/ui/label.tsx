0c8e594a48af6a4035e1c51ab2c8c94ecb416426
Mikko Murto <mikko.murto@doubleopen.org>
2024-04-25 10:01:59 +0300

    feat(ui): Create, list Organizations
    Implement the current "main view" of the application. List all of the
    user's current organizations and provide a way to create new
    organizations. (This implements the "CR" part of CRUD.)
    
    Signed-off-by: Mikko Murto <mikko.murto@doubleopen.org>

---
+++ ui/src/components/ui/label.tsx
@@ -0,0 +1,33 @@
+/*
+ * The component is originally a part of the shadcn/ui library at https://github.com/shadcn-ui/ui.
+ * The library is licensed as set out below:
+ *
+ * Copyright (c) 2023 shadcn
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+import * as React from "react"
+import * as LabelPrimitive from "@radix-ui/react-label"
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+const labelVariants = cva(
+  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+)
+
+const Label = React.forwardRef<
+  React.ElementRef<typeof LabelPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
+    VariantProps<typeof labelVariants>
+>(({ className, ...props }, ref) => (
+  <LabelPrimitive.Root
+    ref={ref}
+    className={cn(labelVariants(), className)}
+    {...props}
+  />
+))
+Label.displayName = LabelPrimitive.Root.displayName
+
+export { Label }
---
+++ ui/src/routes/_layout/create-organization.tsx
@@ -0,0 +1,138 @@
+/*
+ * Copyright (C) 2024 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import { useOrganizationsServicePostOrganizations } from '@/api/queries';
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from '@/components/ui/card';
+import { useToast } from '@/components/ui/use-toast';
+import { ApiError } from '@/api/requests';
+
+const formSchema = z.object({
+  name: z.string(),
+  description: z.string().optional(),
+});
+
+const CreateOrganizationPage = () => {
+  const navigate = useNavigate();
+  const { toast } = useToast();
+
+  const { mutateAsync } = useOrganizationsServicePostOrganizations({
+    onSuccess() {
+      toast({
+        title: 'Create Organization',
+        description: 'New organization created successfully.',
+      });
+      navigate({
+        to: '/',
+      });
+    },
+    onError(error: ApiError) {
+      toast({
+        title: error.message,
+        description: `${error.body.message}\n\n${error.body.cause}`,
+        variant: 'destructive',
+      });
+    },
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    await mutateAsync({
+      requestBody: {
+        name: values.name,
+        // There's a bug somewhere in the OpenAPI generation. Swagger UI hints that the bug may be
+        // in the API, as description in CreateOrganization is an empty object.
+        description: values.description as Record<string, unknown> | undefined,
+      },
+    });
+  }
+
+  return (
+    <Card className="w-full max-w-4xl mx-auto">
+      <CardHeader>Create Organization</CardHeader>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <CardContent>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Enter the name of your organization
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Optional description of the organization
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+          <CardFooter>
+            <Button type="submit">Create</Button>
+          </CardFooter>
+        </form>
+      </Form>
+    </Card>
+  );
+};
+
+export const Route = createFileRoute('/_layout/create-organization')({
+  component: CreateOrganizationPage,
+});
