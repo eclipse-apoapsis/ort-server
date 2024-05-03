bb4a94cb7f3a977fc33c97f1e80c6d370cbad368
Martin Nonnenmacher <martin.nonnenmacher@bosch.com>
2024-05-03 18:15:21 +0200

    fix(core): Fix OpenAPI representation of nullable properties
    When using kotlinx.serialization with ktor-swagger-ui, the
    json-schema-serialization [1] library is used for generating JSON
    schemas [2].
    
    For nullable values, this library generates invalid schemas which causes
    issues with consumer of the generated OpenAPI specification. Introduce a
    post-processing step to fix the schema.
    
    Both the original json-schema-serialization library [3] and the used
    fork [1] are inactive, so we should not expect a fix for this issue.
    
    A similar issue was fixed in ktor-swagger-ui [4], but the fix does not
    apply when using json-schema-serialization.
    
    Fixes #165.
    
    [1]: https://github.com/tillersystems/json-schema-serialization
    [2]: https://github.com/SMILEY4/ktor-swagger-ui/wiki/Kotlinx-Multiplatform-Encoder-Example
    [3]: https://github.com/Ricky12Awesome/json-schema-serialization
    [4]: https://github.com/SMILEY4/ktor-swagger-ui/issues/53
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.com>

--- core/src/main/kotlin/plugins/OpenApi.kt
+++ core/src/main/kotlin/plugins/OpenApi.kt
@@ -19,7 +19,7 @@
 
 package org.eclipse.apoapsis.ortserver.core.plugins
 
-import com.github.ricky12awesome.jss.encodeToSchema
+import com.github.ricky12awesome.jss.buildJsonSchema
 
 import io.github.smiley4.ktorswaggerui.SwaggerUI
 import io.github.smiley4.ktorswaggerui.data.AuthType
@@ -28,7 +28,13 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.config.ApplicationConfig
 
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.jsonPrimitive
 import kotlinx.serialization.serializer
 
 import org.eclipse.apoapsis.ortserver.model.ORT_SERVER_VERSION
@@ -108,7 +114,8 @@ fun Application.configureOpenApi() {
 
         encoding {
             schemaEncoder { type ->
-                json.encodeToSchema(serializer(type), generateDefinitions = false)
+                val schema = buildJsonSchema(serializer(type), generateDefinitions = false).replaceIfWithType()
+                json.encodeToString(schema)
             }
 
             schemaDefinitionsField = "definitions"
@@ -118,4 +125,59 @@ fun Application.configureOpenApi() {
             }
         }
     }
+}
+
+/**
+ * The json-schema-serialization library does not properly support nullable types. For example, for a nullable string
+ * property, it creates the following schema:
+ *
+ * ```json
+ * "property": {
+ *   "if": {
+ *     "type": "string"
+ *   },
+ *   "else": {
+ *     "type": "null"
+ *   }
+ * }
+ * ```
+ *
+ * This function replaces this with the correct schema:
+ *
+ * ```json
+ * "property": {
+ *   "type": ["string", "null"]
+ * }
+ * ```
+ */
+private fun JsonElement.replaceIfWithType(): JsonElement {
+    if (this !is JsonObject) {
+        return this
+    }
+
+    val ifElseTypes = getIfElseTypes()
+    val properties = toMutableMap()
+
+    if (ifElseTypes != null) {
+        properties["type"] = JsonArray(ifElseTypes.toList().map { JsonPrimitive(it) })
+        properties -= "if"
+        properties -= "else"
+    }
+
+    val result = properties.mapValues { (_, value) ->
+        value.replaceIfWithType()
+    }
+
+    return JsonObject(result)
+}
+
+private fun JsonObject.getIfElseTypes(): Pair<String, String>? {
+    val ifType = (get("if") as? JsonObject)?.get("type")?.jsonPrimitive?.content
+    val elseType = (get("else") as? JsonObject)?.get("type")?.jsonPrimitive?.content
+
+    if (ifType == null || elseType == null) {
+        return null
+    }
+
+    return ifType to elseType
 }
