4ffb412dda9fd0e7cf2642e5679e75b72b1e181a
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2024-05-06 11:50:19 +0300

    fix(ui): Raise the header to the top of the page
    Implementing some longer scrollable UI views revealed that the page
    content scrolled on top of the header. This commit fixes the issue by
    changing the header to be the topmost element on the page, so it's
    always visible.
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

--- ui/src/components/header.tsx
+++ ui/src/components/header.tsx
@@ -72,7 +72,7 @@ export const Header = () => {
   );
   
   return (
-    <header className="sticky top-0 flex justify-between h-16 gap-4 px-4 border-b bg-background md:px-6">
+    <header className="sticky top-0 z-50 flex justify-between h-16 gap-4 px-4 border-b bg-background md:px-6">
       <div className="flex flex-row items-center gap-4">
         <nav className="flex-col hidden gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
           <Link
