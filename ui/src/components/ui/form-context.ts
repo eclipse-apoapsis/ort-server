162005c587b29c90141f774664058102b733bdcc
Mikko Murto <mikko.murto@doubleopen.org>
2024-05-16 16:03:59 +0300

    refactor(ui): Fix fast refresh by only exporting components
    If a file exports something else in addition to the component created in
    it, fast refresh doesn't work[1]. Move exports from the button and form
    components to separate files to allow fast refresh.
    
    [1]: https://github.com/ArnaudBarre/eslint-plugin-react-refresh
    
    Signed-off-by: Mikko Murto <mikko.murto@doubleopen.org>

---
+++ ui/src/components/ui/form-context.ts
@@ -0,0 +1,53 @@
+/*
+ * The component is originally a part of the shadcn/ui library at https://github.com/shadcn-ui/ui.
+ * The library is licensed as set out below:
+ *
+ * Copyright (c) 2023 shadcn
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+import React from 'react';
+import { FieldPath, FieldValues, useFormContext } from 'react-hook-form';
+
+type FormFieldContextValue<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> = {
+  name: TName;
+};
+
+export const FormFieldContext = React.createContext<FormFieldContextValue>(
+  {} as FormFieldContextValue
+);
+
+export const useFormField = () => {
+  const fieldContext = React.useContext(FormFieldContext);
+  const itemContext = React.useContext(FormItemContext);
+  const { getFieldState, formState } = useFormContext();
+
+  const fieldState = getFieldState(fieldContext.name, formState);
+
+  if (!fieldContext) {
+    throw new Error('useFormField should be used within <FormField>');
+  }
+
+  const { id } = itemContext;
+
+  return {
+    id,
+    name: fieldContext.name,
+    formItemId: `${id}-form-item`,
+    formDescriptionId: `${id}-form-item-description`,
+    formMessageId: `${id}-form-item-message`,
+    ...fieldState,
+  };
+};
+
+type FormItemContextValue = {
+  id: string;
+};
+
+export const FormItemContext = React.createContext<FormItemContextValue>(
+  {} as FormItemContextValue
+);
