ee0b9adeed9d690915dd2856d51fd95e80e1439c
Mikko Murto <mikko.murto@doubleopen.org>
2024-05-16 16:03:59 +0300

    refactor(ui): Fix fast refresh by only exporting components
    If a file exports something else in addition to the component created in
    it, fast refresh doesn't work[1]. Move exports from the button and form
    components to separate files to allow fast refresh.
    
    [1]: https://github.com/ArnaudBarre/eslint-plugin-react-refresh
    
    Signed-off-by: Mikko Murto <mikko.murto@doubleopen.org>

--- ui/src/components/ui/alert-dialog.tsx
+++ ui/src/components/ui/alert-dialog.tsx
@@ -11,7 +11,7 @@ import * as React from 'react';
 import * as AlertDialogPrimitive from '@radix-ui/react-alert-dialog';
 
 import { cn } from '@/lib/utils';
-import { buttonVariants } from '@/components/ui/button';
+import { buttonVariants } from '@/components/ui/button-variants';
 
 const AlertDialog = AlertDialogPrimitive.Root;
 
