f9c8d4291eb92a624543e63b35395d3fe8a32856
Sebastian Schuberth <sebastian@doubleopen.org>
2024-05-20 16:47:58 +0200

    chore: Avoid explicit type declarations in assignments
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -35,7 +35,7 @@ group = "org.eclipse.apoapsis.ortserver"
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
 
-    val isDevelopment: Boolean = project.ext.has("development")
+    val isDevelopment = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
