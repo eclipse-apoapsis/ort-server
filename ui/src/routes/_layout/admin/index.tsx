0fe0407f1064bcad542c39384f78ce4b6baf6c1e
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2024-05-29 16:58:12 +0300

    refactor(ui): Add organizations table for admin dashboard
    Add initial simple organizations table and organizations count to the
    overview section in the admin panel. Include organization ids in the
    table, as this information is needed when assigning a user to a group
    for the organization in Keycloak.
    
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

--- ui/src/routes/_layout/admin/index.tsx
+++ ui/src/routes/_layout/admin/index.tsx
@@ -17,14 +17,226 @@
  * License-Filename: LICENSE
  */
 
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { createFileRoute, useNavigate } from '@tanstack/react-router';
-import { useEffect } from 'react';
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+} from '@tanstack/react-table';
+import {
+  ChevronDownIcon,
+  ChevronsUpDownIcon,
+  ChevronUpIcon,
+} from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { z } from 'zod';
 
+import { useOrganizationsServiceGetOrganizationsKey } from '@/api/queries';
+import { Organization, OrganizationsService } from '@/api/requests';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 import { useUser } from '@/hooks/useUser';
+import { cn } from '@/lib/utils';
+
+const columns: ColumnDef<Organization>[] = [
+  {
+    accessorKey: 'id',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          className='px-0'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          <Label className='cursor-pointer font-bold'>Id</Label>
+          {column.getIsSorted() === 'desc' ? (
+            <ChevronDownIcon className='ml-2 h-4 w-4' />
+          ) : column.getIsSorted() === 'asc' ? (
+            <ChevronUpIcon className='ml-2 h-4 w-4' />
+          ) : (
+            <ChevronsUpDownIcon className='ml-2 h-4 w-4' />
+          )}
+        </Button>
+      );
+    },
+    cell: ({ row }) => row.original.id,
+  },
+  {
+    accessorKey: 'name',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          className='px-0'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          <Label className='cursor-pointer font-bold'>Name</Label>
+          {column.getIsSorted() === 'desc' ? (
+            <ChevronDownIcon className='ml-2 h-4 w-4' />
+          ) : column.getIsSorted() === 'asc' ? (
+            <ChevronUpIcon className='ml-2 h-4 w-4' />
+          ) : (
+            <ChevronsUpDownIcon className='ml-2 h-4 w-4' />
+          )}
+        </Button>
+      );
+    },
+    cell: ({ row }) => row.original.name,
+  },
+];
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+const DataTable = <TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) => {
+  const [sorting, setSorting] = useState<SortingState>([
+    { id: 'id', desc: false },
+  ]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
+  });
+
+  return (
+    <div>
+      <div className='rounded-md border'>
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className='h-24 text-center'
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className='flex items-center justify-end space-x-2 py-4'>
+        <Button
+          variant='outline'
+          size='sm'
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant='outline'
+          size='sm'
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+const OverviewContent = () => {
+  const { data } = useSuspenseQuery({
+    queryKey: [useOrganizationsServiceGetOrganizationsKey],
+    queryFn: () => OrganizationsService.getOrganizations(),
+  });
+
+  return (
+    <>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4'>
+        <Card>
+          <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+            <CardTitle className='text-sm'>Organizations count</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className='text-2xl font-bold'>{data.data.length}</div>
+            <p className='hidden text-xs text-muted-foreground'>total</p>
+          </CardContent>
+        </Card>
+      </div>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-7'>
+        <Card className='col-span-4'>
+          <CardHeader>
+            <CardTitle>Organizations</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className='w-full'>
+              <DataTable columns={columns} data={data.data} />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
+};
 
 const AdminDashboard = () => {
   const user = useUser();
   const navigate = useNavigate();
+  const search = Route.useSearch();
 
   useEffect(() => {
     if (!user.hasRole('superuser')) {
@@ -41,12 +253,50 @@ const AdminDashboard = () => {
           <div className='flex items-center justify-between space-y-2'>
             <h2 className='text-3xl font-bold tracking-tight'>Dashboard</h2>
           </div>
+          <div className='space-y-4'>
+            <div className='inline-flex h-9 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground'>
+              <Button
+                variant='ghost'
+                className={cn(
+                  search.tab === undefined || search.tab === 'overview'
+                    ? 'bg-background text-foreground shadow hover:bg-background'
+                    : undefined,
+                  'h-7 px-3 font-semibold'
+                )}
+                onClick={() => navigate({ search: { tab: 'overview' } })}
+              >
+                Overview
+              </Button>
+              <Button
+                variant='ghost'
+                className={cn(
+                  search.tab === 'user_mgmt'
+                    ? 'bg-background text-foreground shadow hover:bg-background'
+                    : undefined,
+                  'h-7 px-3 font-semibold'
+                )}
+                onClick={() => navigate({ search: { tab: 'user_mgmt' } })}
+              >
+                User management
+              </Button>
+            </div>
+            <div className='mt-2 space-y-4 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2'>
+              {(search.tab === 'overview' || !search.tab) && (
+                <OverviewContent />
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </>
   );
 };
 
+const adminSearchSchema = z.object({
+  tab: z.string().optional(),
+});
+
 export const Route = createFileRoute('/_layout/admin/')({
+  validateSearch: adminSearchSchema,
   component: AdminDashboard,
 });
