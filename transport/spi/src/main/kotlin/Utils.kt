5a07d7b1b23fa9c9f86da2a80a125c3d7f2e41a5
Nicolas Nobelis <nicolas.nobelis@bosch.com>
2024-06-17 08:40:12 +0200

    feat(workers): Log the ORT Server version when a worker starts
    Signed-off-by: Nicolas Nobelis <nicolas.nobelis@bosch.com>

---
+++ transport/spi/src/main/kotlin/Utils.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2024 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.eclipse.apoapsis.ortserver.transport
+
+import org.eclipse.apoapsis.ortserver.model.ORT_SERVER_VERSION
+
+import org.slf4j.Logger
+
+private val ORT_SERVER_BANNER = """
+  ___    ____    _____           ____                                       
+ / _ \  |  _ \  |_   _|         / ___|    ___   _ __  __   __   ___   _ __  
+| | | | | |_) |   | |    _____  \___ \   / _ \ | '__| \ \ / /  / _ \ | '__| 
+| |_| | |  _ <    | |   |_____|  ___) | |  __/ | |     \ V /  |  __/ | |    
+ \___/  |_| \_\   |_|           |____/   \___| |_|      \_/    \___| |_|    
+ --------------------------------------------------------------------------
+ ORT Server version: $ORT_SERVER_VERSION
+"""
+
+/**
+ * Log the ORT Server version including the commit hash. Also display a pretty Ascii art banner.
+ */
+fun Logger.logVersion() {
+    info(ORT_SERVER_BANNER)
+}
