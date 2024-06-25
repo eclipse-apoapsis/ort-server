a27577d1b57b26901adcbc973ff7710c0bfc9d5f
Oliver Heger <oliver.heger@bosch.io>
2024-06-25 11:35:33 +0200

    feat(config): Add a GitHubConfigNoCache class
    This is an implementation of `GitHubConfigCache` which switches off
    caching. It can be used when caching is not required or no local file
    system for caching is available.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ config/github/src/test/kotlin/GitHubConfigNoCacheTest.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2024 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.eclipse.apoapsis.ortserver.config.github
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+
+import io.ktor.utils.io.ByteReadChannel
+
+class GitHubConfigNoCacheTest : WordSpec({
+    "getOrPutFile" should {
+        "return the data obtained from the load function" {
+            val data = "test configuration data".toByteArray()
+            val cache = GitHubConfigNoCache()
+
+            val stream = cache.getOrPutFile("foo", "bar") {
+                ByteReadChannel(data)
+            }
+
+            stream.readAllBytes() shouldBe data
+        }
+    }
+})
