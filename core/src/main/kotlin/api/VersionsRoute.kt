5669e4b0ee0dffdfb70bd08711e10267486f983a
Martin Nonnenmacher <martin.nonnenmacher@bosch.com>
2024-06-25 16:31:19 +0200

    feat(api): Add an endpoint to return the ORT-Server version
    Add an endpoint `/versions` to return the ORT-Server version. The
    endpoint returns a map as later on more tool versions will be returned,
    for example, the version of ORT that is in use.
    
    The endpoint requires authentication to not expose the version to
    arbitrary clients for security reasons.
    
    Fixes #95.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.com>

---
+++ core/src/main/kotlin/api/VersionsRoute.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2024 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.eclipse.apoapsis.ortserver.core.api
+
+import io.github.smiley4.ktorswaggerui.dsl.get
+
+import io.ktor.server.application.call
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+
+import org.eclipse.apoapsis.ortserver.core.apiDocs.getVersions
+import org.eclipse.apoapsis.ortserver.model.ORT_SERVER_VERSION
+
+fun Route.versions() = get("versions", getVersions) {
+    call.respond(
+        mapOf(
+            "ORT Server" to ORT_SERVER_VERSION
+        )
+    )
+}
---
+++ core/src/main/kotlin/apiDocs/VersionsDocs.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2024 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.eclipse.apoapsis.ortserver.core.apiDocs
+
+import io.github.smiley4.ktorswaggerui.dsl.OpenApiRoute
+
+import io.ktor.http.HttpStatusCode
+
+val getVersions: OpenApiRoute.() -> Unit = {
+    operationId = "getVersions"
+    summary = "Get the versions of the ORT server and other components."
+    tags = listOf("Versions")
+
+    response {
+        HttpStatusCode.OK to {
+            description = "Success"
+            jsonBody<Map<String, String>> {
+                example(
+                    name = "Versions",
+                    value = mapOf(
+                        "ORT Server" to "1.0.0"
+                    )
+                )
+            }
+        }
+    }
+}
