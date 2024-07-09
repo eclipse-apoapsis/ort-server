0d866b6da9faea7a699ee003bd4f9112024801dd
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2024-07-09 12:58:00 +0300

    refactor(ui): Extract job configurations to their own components
    To make the ORT run creation form mode modular and maintainable, extract
    all job configurations to their own components, and change the form to
    use the extracted components.
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

---
+++ ui/src/routes/_layout/organizations/$orgId/products/$productId/repositories/$repoId/-components/scanner-fields.tsx
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2024 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import { UseFormReturn } from 'react-hook-form';
+
+import {
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form';
+import { Switch } from '@/components/ui/switch';
+import { CreateRunFormValues } from '@/routes/_layout/organizations/$orgId/products/$productId/repositories/$repoId/create-run';
+
+type ScannerFieldsProps = {
+  form: UseFormReturn<CreateRunFormValues>;
+};
+
+export const ScannerFields = ({ form }: ScannerFieldsProps) => {
+  return (
+    <div className='flex flex-row align-middle'>
+      <FormField
+        control={form.control}
+        name='jobConfigs.scanner.enabled'
+        render={({ field }) => (
+          <FormControl>
+            <Switch
+              className='my-4 mr-4 data-[state=checked]:bg-green-500'
+              checked={field.value}
+              onCheckedChange={field.onChange}
+            />
+          </FormControl>
+        )}
+      />
+      <AccordionItem value='scanner' className='flex-1'>
+        <AccordionTrigger>Scanner</AccordionTrigger>
+        <AccordionContent>
+          <FormField
+            control={form.control}
+            name='jobConfigs.scanner.skipConcluded'
+            render={({ field }) => (
+              <FormItem className='mb-4 flex flex-row items-center justify-between rounded-lg border p-4'>
+                <div className='space-y-0.5'>
+                  <FormLabel>Skip concluded</FormLabel>
+                  <FormDescription>
+                    A flag to indicate whether packages that have a concluded
+                    license and authors set (to derive copyrights from) should
+                    be skipped in the scan in favor of only using the declared
+                    information.
+                  </FormDescription>
+                </div>
+                <FormControl>
+                  <Switch
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='jobConfigs.scanner.skipExcluded'
+            render={({ field }) => (
+              <FormItem className='mb-4 flex flex-row items-center justify-between rounded-lg border p-4'>
+                <div className='space-y-0.5'>
+                  <FormLabel>Skip excluded</FormLabel>
+                  <FormDescription>
+                    Do not scan excluded projects or packages.
+                  </FormDescription>
+                </div>
+                <FormControl>
+                  <Switch
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </AccordionContent>
+      </AccordionItem>
+    </div>
+  );
+};
