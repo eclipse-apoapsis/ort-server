4d547a7714cd2e1374b17012f41b36ca1cbea4b0
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2024-07-19 10:49:16 +0300

    feat(ui): Add product secrets table
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

--- ui/src/routes/_layout/organizations/$orgId/products/$productId/secrets.tsx
+++ ui/src/routes/_layout/organizations/$orgId/products/$productId/secrets.tsx
@@ -17,8 +17,21 @@
  * License-Filename: LICENSE
  */
 
+import { useSuspenseQueries } from '@tanstack/react-query';
 import { createFileRoute, redirect } from '@tanstack/react-router';
+import {
+  ColumnDef,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
 
+import {
+  useProductsServiceGetProductByIdKey,
+  useSecretsServiceGetSecretsByProductId,
+} from '@/api/queries';
+import { ProductsService, Secret, SecretsService } from '@/api/requests';
+import { DataTable } from '@/components/data-table/data-table';
+import { LoadingIndicator } from '@/components/loading-indicator';
 import {
   Card,
   CardContent,
@@ -26,16 +39,75 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { paginationSchema } from '@/schemas';
+
+const defaultPageSize = 10;
+
+const columns: ColumnDef<Secret>[] = [
+  {
+    accessorKey: 'name',
+    header: 'Name',
+  },
+  {
+    accessorKey: 'description',
+    header: 'Description',
+  },
+];
 
 const ProductSecrets = () => {
+  const params = Route.useParams();
+  const search = Route.useSearch();
+  const pageIndex = search.page ? search.page - 1 : 0;
+  const pageSize = search.pageSize ? search.pageSize : defaultPageSize;
+
+  const [{ data: product }, { data: secrets }] = useSuspenseQueries({
+    queries: [
+      {
+        queryKey: [useProductsServiceGetProductByIdKey, params.productId],
+        queryFn: async () =>
+          await ProductsService.getProductById({
+            productId: Number.parseInt(params.productId),
+          }),
+      },
+      {
+        queryKey: [
+          useSecretsServiceGetSecretsByProductId,
+          params.productId,
+          pageIndex,
+          pageSize,
+        ],
+        queryFn: async () =>
+          await SecretsService.getSecretsByProductId({
+            productId: Number.parseInt(params.productId),
+            limit: pageSize,
+            offset: pageIndex * pageSize,
+          }),
+      },
+    ],
+  });
+
+  const table = useReactTable({
+    data: secrets?.data || [],
+    columns,
+    pageCount: Math.ceil(secrets.pagination.totalCount / pageSize),
+    state: {
+      pagination: {
+        pageIndex,
+        pageSize,
+      },
+    },
+    getCoreRowModel: getCoreRowModel(),
+    manualPagination: true,
+  });
+
   return (
     <Card className='mx-auto w-full max-w-4xl'>
       <CardHeader>
         <CardTitle>Secrets</CardTitle>
-        <CardDescription>Manage secrets for this product.</CardDescription>
+        <CardDescription>Manage secrets for {product.name}.</CardDescription>
       </CardHeader>
       <CardContent>
-        <div>Content here.</div>
+        <DataTable table={table} />
       </CardContent>
     </Card>
   );
@@ -44,6 +116,33 @@ const ProductSecrets = () => {
 export const Route = createFileRoute(
   '/_layout/organizations/$orgId/products/$productId/secrets'
 )({
+  validateSearch: paginationSchema,
+  loaderDeps: ({ search: { page, pageSize } }) => ({ page, pageSize }),
+  loader: async ({ context, params, deps: { page, pageSize } }) => {
+    await Promise.allSettled([
+      context.queryClient.ensureQueryData({
+        queryKey: [useProductsServiceGetProductByIdKey, params.orgId],
+        queryFn: () =>
+          ProductsService.getProductById({
+            productId: Number(params.productId),
+          }),
+      }),
+      context.queryClient.ensureQueryData({
+        queryKey: [
+          useSecretsServiceGetSecretsByProductId,
+          params.orgId,
+          page,
+          pageSize,
+        ],
+        queryFn: () =>
+          SecretsService.getSecretsByProductId({
+            productId: Number(params.productId),
+            limit: pageSize || defaultPageSize,
+            offset: page ? (page - 1) * (pageSize || defaultPageSize) : 0,
+          }),
+      }),
+    ]);
+  },
   component: ProductSecrets,
   beforeLoad: ({ context, params }) => {
     if (
@@ -57,4 +156,5 @@ export const Route = createFileRoute(
       });
     }
   },
+  pendingComponent: LoadingIndicator,
 });
