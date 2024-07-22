235e8f9646c72ba7bde959760edb650787de59cf
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2024-07-22 13:49:58 +0300

    fix(ui): Fix loader indicator position
    Give the outer div of the LoadingIndicator component a full width to
    ensure the spinner is rendered in the center of its parent container.
    
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

--- ui/src/components/loading-indicator.tsx
+++ ui/src/components/loading-indicator.tsx
@@ -21,7 +21,7 @@ import { Loader2 } from 'lucide-react';
 
 export const LoadingIndicator = () => {
   return (
-    <div className='flex h-full flex-col items-center justify-center'>
+    <div className='flex h-full w-full flex-col items-center justify-center'>
       <div className='sr-only'>Loading data...</div>
       <Loader2 className='h-12 w-12 animate-spin' />
     </div>
