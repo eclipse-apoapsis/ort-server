d1cdd8a51128cdcacd76afecd8e2a1af989395cc
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2024-07-22 15:57:35 +0300

    feat(ui): Add generic DeleteDialog component
    The delete buttons and the dialogs that are opened by clicking them
    in the current UI all follow a similar pattern, so a generic
    component is created here, which can be used to reduce some code
    clutter.
    
    The description part of the dialog could be altered to take in a JSX
    element if needed, but as all current dialogs use the same pattern,
    only take in the changing parts as props for now.
    
    The open state of the dialog is handled with a React state variable,
    to better be able to handle the open state based on API call states
    and responses for example.
    
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

---
+++ ui/src/components/delete-dialog.tsx
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2024 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import { Loader2, OctagonAlert, TrashIcon } from 'lucide-react';
+
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+interface DeleteDialogProps {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  item: {
+    descriptor: string;
+    name: string;
+  };
+  onDelete: () => void;
+  isPending: boolean;
+  className?: string;
+}
+
+export const DeleteDialog = ({
+  open,
+  setOpen,
+  item,
+  onDelete,
+  isPending,
+  className,
+}: DeleteDialogProps) => {
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger asChild>
+        <Button
+          size='sm'
+          variant='outline'
+          className={cn('h-9 px-2', className)}
+        >
+          <span className='sr-only'>Delete</span>
+          <TrashIcon size={16} />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <div className='flex items-center'>
+            <OctagonAlert className='h-8 w-8 pr-2 text-red-500' />
+            <AlertDialogTitle>Delete {item.descriptor}</AlertDialogTitle>
+          </div>
+        </AlertDialogHeader>
+        <AlertDialogDescription>
+          Are you sure you want to delete this {item.descriptor}:{' '}
+          <span className='font-bold'>{item.name}</span>?
+        </AlertDialogDescription>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <Button onClick={onDelete} className='bg-red-500'>
+            {isPending ? (
+              <>
+                <span className='sr-only'>Deleting {item.descriptor}...</span>
+                <Loader2 size={16} className='mx-3 animate-spin' />
+              </>
+            ) : (
+              'Delete'
+            )}
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
