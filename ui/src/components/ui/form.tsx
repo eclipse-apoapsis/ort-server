338006bf77b3ec8fef96cacc6ee63e03c1d5eaf4
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2024-07-31 13:54:07 +0300

    refactor(ui): Use different HTML tag in FormDescription component
    Use a <div> tag in the FormDescription component to be able to use
    divisions and paragraphs in its descendants without getting an
    error in the console, and to not have to use any ugly workarounds.
    
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

--- ui/src/components/ui/form.tsx
+++ ui/src/components/ui/form.tsx
@@ -102,7 +102,7 @@ const FormDescription = React.forwardRef<
   const { formDescriptionId } = useFormField();
 
   return (
-    <p
+    <div
       ref={ref}
       id={formDescriptionId}
       className={cn('text-[0.8rem] text-muted-foreground', className)}
