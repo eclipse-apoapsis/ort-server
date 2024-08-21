fe033d4ee2cf744d1a66797a66d25352802f24b0
Martin Nonnenmacher <martin.nonnenmacher@bosch.com>
2024-08-22 01:10:36 +0200

    feat(core): Add initial MDC entries
    Add initial MDC entries for the component, and for the organization,
    product, and repository IDs taken from request parameters.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.com>

--- core/src/main/kotlin/plugins/Lifecycle.kt
+++ core/src/main/kotlin/plugins/Lifecycle.kt
@@ -21,11 +21,12 @@ package org.eclipse.apoapsis.ortserver.core.plugins
 
 import io.ktor.server.application.Application
 
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 import org.eclipse.apoapsis.ortserver.services.AuthorizationService
+import org.eclipse.apoapsis.ortserver.utils.logging.withMdcContext
 
 import org.koin.ktor.ext.inject
 
@@ -40,28 +41,31 @@ private val logger = LoggerFactory.getLogger(Application::class.java)
 fun Application.configureLifecycle() {
     environment.monitor.subscribe(DatabaseReady) {
         val authorizationService by inject<AuthorizationService>()
-        val scope = CoroutineScope(Dispatchers.IO)
 
-        scope.syncRoles(authorizationService)
+        runBlocking(Dispatchers.IO) {
+            syncRoles(authorizationService)
+        }
     }
 }
 
 /**
  * Trigger the synchronization of permissions and roles in Keycloak. The synchronization then runs in background.
  */
-private fun CoroutineScope.syncRoles(authorizationService: AuthorizationService) {
-    launch {
-        runCatching {
-            logger.info("Ensuring superuser role and group.")
-            authorizationService.ensureSuperuser()
-            logger.info("Synchronizing Keycloak permissions.")
-            authorizationService.synchronizePermissions()
-            logger.info("Synchronizing Keycloak roles.")
-            authorizationService.synchronizeRoles()
-        }.onSuccess {
-            logger.info("Synchronized Keycloak permissions and roles.")
-        }.onFailure {
-            logger.error("Error while synchronizing Keycloak permissions and roles.", it)
+private suspend fun syncRoles(authorizationService: AuthorizationService) {
+    withMdcContext("component" to "core") {
+        launch {
+            runCatching {
+                logger.info("Ensuring superuser role and group.")
+                authorizationService.ensureSuperuser()
+                logger.info("Synchronizing Keycloak permissions.")
+                authorizationService.synchronizePermissions()
+                logger.info("Synchronizing Keycloak roles.")
+                authorizationService.synchronizeRoles()
+            }.onSuccess {
+                logger.info("Synchronized Keycloak permissions and roles.")
+            }.onFailure {
+                logger.error("Error while synchronizing Keycloak permissions and roles.", it)
+            }
         }
     }
 }
