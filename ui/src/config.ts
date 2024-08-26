7e0b4d0f9c1adb8afbd0fa860e97bed9e912e314
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2024-08-26 08:28:37 +0300

    refactor(ui): Initialize UI configuration in separate file
    Gather all UI configuration in one file, which makes it easier to
    maintain and understand the configuration. All environment variable
    loading happens in this file.
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

---
+++ ui/src/config.ts
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2024 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import { AuthProviderProps } from 'react-oidc-context';
+
+interface Config {
+  BASEPATH: string;
+  API_URL: string;
+  oidcConfig: AuthProviderProps;
+  serverClientId: string;
+  authBaseUrl: string;
+  realm: string;
+  pollInterval: number;
+}
+
+// Read the environment variables needed to create the configuration object
+const BASEPATH = import.meta.env.VITE_BASEPATH || '/';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+const AUTHORITY =
+  import.meta.env.VITE_AUTHORITY || 'http://localhost:8081/realms/master';
+const UI_URL = import.meta.env.VITE_UI_URL || 'http://localhost:5173/';
+const CLIENT_ID = import.meta.env.VITE_CLIENT_ID || 'ort-server-ui-dev';
+const CLIENT_ID_SERVER = import.meta.env.VITE_CLIENT_ID_SERVER || 'ort-server';
+const RUN_POLL_INTERVAL =
+  Number.parseInt(import.meta.env.VITE_RUN_POLL_INTERVAL) || 10000;
+
+// Initialize the OpenID Connect configuration
+const oidcConfig = {
+  authority: AUTHORITY,
+  redirect_uri: UI_URL,
+  client_id: CLIENT_ID,
+  automaticSilentRenew: true,
+} satisfies AuthProviderProps;
+
+const serverClientId = CLIENT_ID_SERVER;
+
+// Configure Keycloak
+const authBaseUrl = AUTHORITY
+  ? AUTHORITY.split('/realms/')[0]
+  : 'http://localhost:8081';
+const realm = AUTHORITY ? AUTHORITY.split('/realms/')[1] : 'master';
+
+// Polling interval for run status updates
+const pollInterval = RUN_POLL_INTERVAL;
+
+export const config: Config = {
+  BASEPATH,
+  API_URL,
+  oidcConfig,
+  serverClientId,
+  authBaseUrl,
+  realm,
+  pollInterval,
+};
