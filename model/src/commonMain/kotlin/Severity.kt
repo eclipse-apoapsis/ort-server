3258f14a91a90cfd53e6ecf66160e8fed5d054fa
Martin Nonnenmacher <martin.nonnenmacher@bosch.com>
2024-08-28 17:14:56 +0200

    feat: Add severity enums
    Add severity enums to the `api` and `model` modules. The enum will be
    taken into use in the following commits.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.com>

---
+++ model/src/commonMain/kotlin/Severity.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2024 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.eclipse.apoapsis.ortserver.model
+
+import org.eclipse.apoapsis.ortserver.model.runs.Issue
+
+/**
+ * An enum for severities of [Issue]s and other similar objects.
+ */
+enum class Severity {
+    /** Used for issues that provide information only. */
+    HINT,
+
+    /** Used for issues that could lead to problems. */
+    WARNING,
+
+    /** Used for issues that must be addressed. */
+    ERROR
+}
