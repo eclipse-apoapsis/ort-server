bc81f5d0f6878c1cd2add3c990e3c1ce7f3702cb
Nicolas Nobelis <nicolas.nobelis@bosch.com>
2024-09-02 09:19:07 +0200

    test(github): Increase the delay between retries
    With the current delay of 1 second, the test code exhausts all its
    retries in 9 seconds, and it leads to some occasional flakiness in the
    CI.
    
    Signed-off-by: Nicolas Nobelis <nicolas.nobelis@bosch.com>

--- config/github/src/test/kotlin/GitHubConfigFileCacheTest.kt
+++ config/github/src/test/kotlin/GitHubConfigFileCacheTest.kt
@@ -209,7 +209,7 @@ class GitHubConfigFileCacheTest : WordSpec({
 
             val cache = GitHubConfigFileCache(cacheDir, lockCheckInterval, 5, 1.days)
 
-            retry(10, 30.seconds) {
+            retry(10, 30.seconds, 2.seconds) {
                 cache.cleanup("current")
                 revisionDir.exists() shouldBe false
             }
@@ -219,7 +219,7 @@ class GitHubConfigFileCacheTest : WordSpec({
             val cacheDir = tempdir()
             val cache = GitHubConfigFileCache(cacheDir, lockCheckInterval, 10, 1.days)
 
-            retry(10, 30.seconds) {
+            retry(10, 30.seconds, 2.seconds) {
                 val now = Clock.System.now()
                 val revisionDir = cacheDir.createChildDirAt(revision(1), now - 2.days)
 
