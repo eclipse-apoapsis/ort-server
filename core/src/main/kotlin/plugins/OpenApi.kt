b89872362f7c61274c6e807934e7a2676cf534be
Martin Nonnenmacher <martin.nonnenmacher@bosch.com>
2024-09-12 17:37:02 +0200

    chore(core): Remove the workaround for valid OpenAPI references
    Remove the workaround to fix the references in the OpenAPI spec
    introduced in 8a1a967. It is not required anymore because
    schema-kenerator 1.2.0 [1] introduced presets for the same formatting.
    
    [1]: https://github.com/SMILEY4/schema-kenerator/releases/tag/1.2.0
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.com>

--- core/src/main/kotlin/plugins/OpenApi.kt
+++ core/src/main/kotlin/plugins/OpenApi.kt
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2022 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
- * Copyright 2024 SMILEY4
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,31 +24,22 @@ import io.github.smiley4.ktorswaggerui.data.AuthType
 import io.github.smiley4.ktorswaggerui.routing.openApiSpec
 import io.github.smiley4.ktorswaggerui.routing.swaggerUI
 import io.github.smiley4.schemakenerator.core.connectSubTypes
-import io.github.smiley4.schemakenerator.core.data.BaseTypeData
-import io.github.smiley4.schemakenerator.core.data.Bundle
-import io.github.smiley4.schemakenerator.core.data.TypeId
-import io.github.smiley4.schemakenerator.core.data.flatten
 import io.github.smiley4.schemakenerator.core.handleNameAnnotation
 import io.github.smiley4.schemakenerator.reflection.collectSubTypes
 import io.github.smiley4.schemakenerator.reflection.processReflection
 import io.github.smiley4.schemakenerator.swagger.OptionalHandling
-import io.github.smiley4.schemakenerator.swagger.data.CompiledSwaggerSchema
-import io.github.smiley4.schemakenerator.swagger.data.SwaggerSchema
+import io.github.smiley4.schemakenerator.swagger.compileReferencingRoot
+import io.github.smiley4.schemakenerator.swagger.data.RefType
+import io.github.smiley4.schemakenerator.swagger.data.TitleType
 import io.github.smiley4.schemakenerator.swagger.generateSwaggerSchema
 import io.github.smiley4.schemakenerator.swagger.handleCoreAnnotations
-import io.github.smiley4.schemakenerator.swagger.steps.SwaggerSchemaCompileUtils.merge
-import io.github.smiley4.schemakenerator.swagger.steps.SwaggerSchemaCompileUtils.resolveReferences
-import io.github.smiley4.schemakenerator.swagger.steps.SwaggerSchemaCompileUtils.shouldReference
-import io.github.smiley4.schemakenerator.swagger.steps.SwaggerSchemaUtils
-import io.github.smiley4.schemakenerator.swagger.steps.buildTypeDataMap
+import io.github.smiley4.schemakenerator.swagger.withTitle
 
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.config.ApplicationConfig
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
-
-import io.swagger.v3.oas.models.media.Schema
 
 import kotlinx.datetime.Instant
 
@@ -144,8 +134,8 @@ fun Application.configureOpenApi() {
                         optionalHandling = OptionalHandling.NON_REQUIRED
                     }
                     .handleCoreAnnotations()
-                    .withCustomTitle()
-                    .customCompileReferencingRoot()
+                    .withTitle(TitleType.OPENAPI_SIMPLE)
+                    .compileReferencingRoot(RefType.OPENAPI_SIMPLE)
             }
         }
     }
@@ -158,165 +148,5 @@ fun Application.configureOpenApi() {
                 openApiSpec()
             }
         }
-    }
-}
-
-private fun Bundle<SwaggerSchema>.withCustomTitle(): Bundle<SwaggerSchema> {
-    return CustomTitleStep().process(this)
-}
-
-/**
- * An adapted version of [io.github.smiley4.schemakenerator.swagger.steps.SwaggerSchemaAutoTitleStep] that generates
- * valid titles for the generated schemas. For example, the type `GenericType<Type1, Type2>` is transformed to
- * `GenericType_Type1-Type2` instead of `GenericType<Type1,Type2>`.
- *
- * According to the [specification](https://swagger.io/specification/#components-object), schema keys must match the
- * regular expression `^[a-zA-Z0-9\.\-_]+$`.
- */
-private class CustomTitleStep {
-    fun process(bundle: Bundle<SwaggerSchema>): Bundle<SwaggerSchema> {
-        val typeDataMap = bundle.buildTypeDataMap()
-        return bundle.also { schema ->
-            process(schema.data, typeDataMap)
-            schema.supporting.forEach { process(it, typeDataMap) }
-        }
-    }
-
-    private fun process(schema: SwaggerSchema, typeDataMap: Map<TypeId, BaseTypeData>) {
-        if (schema.swagger.title == null) {
-            schema.swagger.title = determineTitle(schema.typeData, typeDataMap)
-        }
-    }
-
-    private fun determineTitle(typeData: BaseTypeData, typeDataMap: Map<TypeId, BaseTypeData>): String {
-        return typeData.simpleName.let {
-            if (typeData.typeParameters.isNotEmpty()) {
-                val paramString = typeData.typeParameters
-                    .map { (_, param) -> determineTitle(typeDataMap.getValue(param.type), typeDataMap) }
-                    .joinToString("-")
-                "${it}_$paramString"
-            } else {
-                it
-            }
-        }.let {
-            it + (typeData.id.additionalId?.let { a -> "#$a" }.orEmpty())
-        }
-    }
-}
-
-private fun Bundle<SwaggerSchema>.customCompileReferencingRoot(): CompiledSwaggerSchema {
-    return CustomCompileReferenceRootStep().compile(this)
-}
-
-/**
- * An adapted version of [io.github.smiley4.schemakenerator.swagger.steps.SwaggerSchemaCompileReferenceRootStep] that
- * generates valid references for the generated schemas (see [getRefPath]).
- */
-private class CustomCompileReferenceRootStep {
-    private val schemaUtils = SwaggerSchemaUtils()
-
-    /**
-     * Put referenced schemas into definitions and reference them
-     */
-    fun compile(bundle: Bundle<SwaggerSchema>): CompiledSwaggerSchema {
-        val result = CustomCompileReferenceStep().compile(bundle)
-        if (shouldReference(result.swagger)) {
-            val refPath = getRefPath(result.typeData, bundle.buildTypeDataMap())
-            return CompiledSwaggerSchema(
-                typeData = result.typeData,
-                swagger = schemaUtils.referenceSchema(refPath, true),
-                componentSchemas = buildMap {
-                    this.putAll(result.componentSchemas)
-                    this[refPath] = result.swagger
-                }
-            )
-        } else {
-            return CompiledSwaggerSchema(
-                typeData = result.typeData,
-                swagger = result.swagger,
-                componentSchemas = result.componentSchemas
-            )
-        }
-    }
-}
-
-/**
- * An adapted version of [io.github.smiley4.schemakenerator.swagger.steps.SwaggerSchemaCompileReferenceStep] that
- * generates valid references for the generated schemas (see [getRefPath]).
- */
-private class CustomCompileReferenceStep {
-    private val schemaUtils = SwaggerSchemaUtils()
-
-    /**
-     * Put referenced schemas into definitions and reference them
-     */
-    fun compile(bundle: Bundle<SwaggerSchema>): CompiledSwaggerSchema {
-        val schemaList = bundle.flatten()
-        val typeDataMap = bundle.buildTypeDataMap()
-        val components = mutableMapOf<String, Schema<*>>()
-
-        val root = resolveReferences(bundle.data.swagger) { refObj ->
-            resolve(refObj, schemaList, typeDataMap, components)
-        }
-
-        return CompiledSwaggerSchema(
-            typeData = bundle.data.typeData,
-            swagger = root,
-            componentSchemas = components
-        )
-    }
-
-    private fun resolve(
-        refObj: Schema<*>,
-        schemaList: List<SwaggerSchema>,
-        typeDataMap: Map<TypeId, BaseTypeData>,
-        components: MutableMap<String, Schema<*>>
-    ): Schema<*> {
-        val referencedId = TypeId.parse(refObj.`$ref`)
-        val referencedSchema = schemaList.find(referencedId)
-        return if (referencedSchema != null) {
-            if (shouldReference(referencedSchema.swagger)) {
-                val refPath = getRefPath(referencedSchema.typeData, typeDataMap)
-                if (!components.containsKey(refPath)) {
-                    components[refPath] = placeholder() // break out of infinite loops
-                    components[refPath] =
-                        resolveReferences(referencedSchema.swagger) { resolve(it, schemaList, typeDataMap, components) }
-                }
-                schemaUtils.referenceSchema(refPath, true)
-            } else {
-                merge(refObj, referencedSchema.swagger)
-            }
-        } else {
-            refObj
-        }
-    }
-
-    private fun placeholder() = Schema<Any>()
-
-    private fun Collection<SwaggerSchema>.find(id: TypeId): SwaggerSchema? {
-        return this.find { it.typeData.id == id }
-    }
-}
-
-/**
- * An adapted version of [io.github.smiley4.schemakenerator.swagger.steps.SwaggerSchemaCompileUtils.getRefPath] that
- * generates valid references for the generated schemas. For example, the type `GenericType<Type1, Type2>` is
- * transformed to `GenericType_Type1-Type2` instead of `GenericType<Type1,Type2>`.
- *
- * According to the [specification](https://swagger.io/specification/#components-object), schema keys must match the
- * regular expression `^[a-zA-Z0-9\.\-_]+$`.
- */
-private fun getRefPath(typeData: BaseTypeData, typeDataMap: Map<TypeId, BaseTypeData>): String {
-    return typeData.simpleName.let {
-        if (typeData.typeParameters.isNotEmpty()) {
-            val paramString = typeData.typeParameters
-                .map { (_, param) -> getRefPath(typeDataMap.getValue(param.type), typeDataMap) }
-                .joinToString("-")
-            "${it}_$paramString"
-        } else {
-            it
-        }
-    }.let {
-        it + (typeData.id.additionalId?.let { a -> "#$a" }.orEmpty())
     }
 }
