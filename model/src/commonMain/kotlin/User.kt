a43448bf7ff6d9c839e183097ff8df826d02bcd5
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2024-09-27 08:32:21 +0300

    feat(services): Add UserService
    Add a service to handle user related operations.
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

---
+++ model/src/commonMain/kotlin/User.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2022 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.eclipse.apoapsis.ortserver.model
+
+/**
+ * A data class representing a user.
+ */
+data class User(
+    /** The username of the user. */
+    val username: String,
+
+    /** The password of the user. */
+    val password: String?,
+
+    /** Whether the password is temporary. */
+    val temporary: Boolean,
+
+    /** The first name of the user. */
+    val firstName: String? = null,
+
+    /** The last name of the user. */
+    val lastName: String? = null,
+
+    /** The mail address of the user. */
+    val email: String? = null,
+
+    /** Specifies, whether the user can log in or not. */
+    val enabled: Boolean = true
+)
