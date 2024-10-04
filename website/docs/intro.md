e92bf32c87e022a4cc31153ee6f18746886d0091
Martin Nonnenmacher <martin.nonnenmacher@bosch.com>
2024-10-04 13:59:41 +0200

    docs(website): Format source code with prettier
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.com>

--- website/docs/intro.md
+++ website/docs/intro.md
@@ -8,18 +8,18 @@ The [Eclipse Apoapsis](https://projects.eclipse.org/projects/technology.apoapsis
 
 It is based on the [OSS Review Toolkit (ORT)](https://oss-review-toolkit.org) which provides:
 
-* Recursive dependency analysis for more than 20 package managers.
-* Integration of several vulnerability databases.
-* Integration of several license, copyright, and snippet scanners.
-* Customizable compliance rules.
-* Lots of report formats, including SPDX and CycloneDX SBOMs.
-* Flexible configuration.
+- Recursive dependency analysis for more than 20 package managers.
+- Integration of several vulnerability databases.
+- Integration of several license, copyright, and snippet scanners.
+- Customizable compliance rules.
+- Lots of report formats, including SPDX and CycloneDX SBOMs.
+- Flexible configuration.
 
 The ORT core functionality is extended with:
 
-* A scalable architecture with Kubernetes integration.
-* A REST API to trigger scans and manage data.
-* Keycloak integration for authentication and role management.
-* A central database to enable data analysis across projects.
+- A scalable architecture with Kubernetes integration.
+- A REST API to trigger scans and manage data.
+- Keycloak integration for authentication and role management.
+- A central database to enable data analysis across projects.
 
 The ORT Server developers are also contributors to ORT and both projects work in close collaboration.
--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -1,7 +1,8 @@
-import clsx from 'clsx';
+import { Icon } from '@iconify/react';
 import Heading from '@theme/Heading';
+import clsx from 'clsx';
+
 import styles from './styles.module.css';
-import {Icon} from '@iconify/react';
 
 type FeatureItem = {
   title: string;
@@ -16,9 +17,11 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/ort-logo.svg').default,
     description: (
       <>
-          The server integrates is baed on the OSS Review Toolkit, leveraging its APIs for component analysis, license
-          scanning, vulnerability databases, compliance rules, and report generation. This allows users to manage and
-          analyze dependencies and licenses with ease, offering detailed insights and compliance reporting.
+        The server integrates is baed on the OSS Review Toolkit, leveraging its
+        APIs for component analysis, license scanning, vulnerability databases,
+        compliance rules, and report generation. This allows users to manage and
+        analyze dependencies and licenses with ease, offering detailed insights
+        and compliance reporting.
       </>
     ),
   },
@@ -27,10 +30,12 @@ const FeatureList: FeatureItem[] = [
     icon: 'carbon:network-4',
     description: (
       <>
-          The ORT Server provides a comprehensive solution for organizations to perform Software Composition Analysis
-          (SCA) at scale. It supports a wide range of project setups, from mobile apps to cloud services, enabling
-          automatic generation of Software Bill of Materials (SBOMs), dependency analysis, and identification of
-          vulnerabilities, ensuring efficient management of software components across projects.
+        The ORT Server provides a comprehensive solution for organizations to
+        perform Software Composition Analysis (SCA) at scale. It supports a wide
+        range of project setups, from mobile apps to cloud services, enabling
+        automatic generation of Software Bill of Materials (SBOMs), dependency
+        analysis, and identification of vulnerabilities, ensuring efficient
+        management of software components across projects.
       </>
     ),
   },
@@ -39,9 +44,11 @@ const FeatureList: FeatureItem[] = [
     icon: 'devicon:react',
     description: (
       <>
-          The ORT Server includes a react-based web UI designed to streamline access to critical functions and data.
-          This interface allows users to manage the compliance and security of their projects with ease, providing
-          detailed reports of software components, licenses, and vulnerabilities.
+        The ORT Server includes a react-based web UI designed to streamline
+        access to critical functions and data. This interface allows users to
+        manage the compliance and security of their projects with ease,
+        providing detailed reports of software components, licenses, and
+        vulnerabilities.
       </>
     ),
   },
@@ -50,10 +57,11 @@ const FeatureList: FeatureItem[] = [
     icon: 'devicon:openapi',
     description: (
       <>
-          The ORT Server provides a REST API that allows for seamless integration with other tools and automation
-          workflows. This API offers endpoints for performing tasks like managing repositories, secrets, or users, and
-          getting the results of ORT runs, making it easy to incorporate ORT functionality into existing DevOps
-          pipelines or CI/CD environments.
+        The ORT Server provides a REST API that allows for seamless integration
+        with other tools and automation workflows. This API offers endpoints for
+        performing tasks like managing repositories, secrets, or users, and
+        getting the results of ORT runs, making it easy to incorporate ORT
+        functionality into existing DevOps pipelines or CI/CD environments.
       </>
     ),
   },
@@ -62,11 +70,14 @@ const FeatureList: FeatureItem[] = [
     icon: 'devicon:kubernetes',
     description: (
       <>
-          The ORT Server is designed with scalability in mind, leveraging container orchestration platforms like
-          Kubernetes for efficient resource management and deployment. It allows organizations to scale their software
-          composition analysis workflows horizontally by running the actual ORT components in separate containers.
-          Kubernetes integration also provides enhanced automation, fault tolerance, and dynamic load balancing,
-          ensuring high availability and reliability in production environments.
+        The ORT Server is designed with scalability in mind, leveraging
+        container orchestration platforms like Kubernetes for efficient resource
+        management and deployment. It allows organizations to scale their
+        software composition analysis workflows horizontally by running the
+        actual ORT components in separate containers. Kubernetes integration
+        also provides enhanced automation, fault tolerance, and dynamic load
+        balancing, ensuring high availability and reliability in production
+        environments.
       </>
     ),
   },
@@ -75,30 +86,32 @@ const FeatureList: FeatureItem[] = [
     icon: 'carbon:two-factor-authentication',
     description: (
       <>
-          The ORT Server incorporates robust user access and role management capabilities based on Keycloak, an open
-          source identity and access management solution. It supports user authentication, authorization, and
-          multi-factor authentication, allowing organizations to define roles and permissions for different users. This
-          ensures that different users can access only the relevant parts of the system.
+        The ORT Server incorporates robust user access and role management
+        capabilities based on Keycloak, an open source identity and access
+        management solution. It supports user authentication, authorization, and
+        multi-factor authentication, allowing organizations to define roles and
+        permissions for different users. This ensures that different users can
+        access only the relevant parts of the system.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, icon, description}: FeatureItem) {
+function Feature({ title, Svg, icon, description }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       {icon && (
-        <div className="text--center">
+        <div className='text--center'>
           <Icon className={styles.featureSvg} icon={icon} />
         </div>
       )}
       {Svg && (
-        <div className="text--center">
-          <Svg className={styles.featureSvg} role="img" />
+        <div className='text--center'>
+          <Svg className={styles.featureSvg} role='img' />
         </div>
       )}
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
+      <div className='text--center padding-horiz--md'>
+        <Heading as='h3'>{title}</Heading>
         <p>{description}</p>
       </div>
     </div>
@@ -108,8 +121,8 @@ function Feature({title, Svg, icon, description}: FeatureItem) {
 export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
-      <div className="container">
-        <div className="row">
+      <div className='container'>
+        <div className='row'>
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
