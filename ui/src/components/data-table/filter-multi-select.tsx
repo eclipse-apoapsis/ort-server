846bf6dfd6e8b0159933d9448ad2d99d07ffa544
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2024-10-15 16:16:42 +0300

    feat(ui): Add generic multivalue filter
    Add multivalue filter that can be used in the table
    toolbar to allow filtering the data with provided
    options.
    
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

---
+++ ui/src/components/data-table/filter-multi-select.tsx
@@ -0,0 +1,157 @@
+/*
+ * Copyright (C) 2024 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import { CheckIcon, PlusCircledIcon } from '@radix-ui/react-icons';
+import * as React from 'react';
+
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from '@/components/ui/command';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { Separator } from '@/components/ui/separator';
+import { cn } from '@/lib/utils';
+
+interface FilterMultiSelectProps<T> {
+  title?: string;
+  options: {
+    label: string;
+    value: T;
+    icon?: React.ComponentType<{ className?: string }>;
+  }[];
+  selected: T[];
+  setSelected: (selected: T[]) => void;
+}
+
+export function FilterMultiSelect<T extends string | number>({
+  title,
+  options,
+  selected,
+  setSelected,
+}: FilterMultiSelectProps<T>) {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant='outline' size='sm' className='h-8 border-dashed'>
+          <PlusCircledIcon className='mr-2 h-4 w-4' />
+          {title}
+          {selected.length > 0 && (
+            <>
+              <Separator orientation='vertical' className='mx-2 h-4' />
+              <Badge
+                variant='secondary'
+                className='rounded-sm px-1 font-normal lg:hidden'
+              >
+                {selected.length}
+              </Badge>
+              <div className='hidden space-x-1 lg:flex'>
+                {selected.length > 2 ? (
+                  <Badge
+                    variant='secondary'
+                    className='rounded-sm px-1 font-normal'
+                  >
+                    {selected.length} selected
+                  </Badge>
+                ) : (
+                  options
+                    .filter((option) => selected.includes(option.value))
+                    .map((option) => (
+                      <Badge
+                        variant='secondary'
+                        key={option.value.toString()}
+                        className='rounded-sm px-1 font-normal'
+                      >
+                        {option.label}
+                      </Badge>
+                    ))
+                )}
+              </div>
+            </>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className='w-[200px] p-0' align='start'>
+        <Command>
+          <CommandInput placeholder={title} />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup>
+              {options.map((option) => {
+                const isSelected = selected.includes(option.value);
+                return (
+                  <CommandItem
+                    key={option.value.toString()}
+                    onSelect={() => {
+                      if (isSelected) {
+                        setSelected(
+                          selected.filter((value) => value !== option.value)
+                        );
+                      } else {
+                        setSelected([...selected, option.value]);
+                      }
+                    }}
+                  >
+                    <div
+                      className={cn(
+                        'mr-2 flex h-4 w-4 items-center justify-center rounded-sm border border-primary',
+                        isSelected
+                          ? 'bg-primary text-primary-foreground'
+                          : 'opacity-50 [&_svg]:invisible'
+                      )}
+                    >
+                      <CheckIcon className={cn('h-4 w-4')} />
+                    </div>
+                    {option.icon && (
+                      <option.icon className='mr-2 h-4 w-4 text-muted-foreground' />
+                    )}
+                    <span>{option.label}</span>
+                  </CommandItem>
+                );
+              })}
+            </CommandGroup>
+            {selected.length > 0 && (
+              <>
+                <CommandSeparator />
+                <CommandGroup>
+                  <CommandItem
+                    onSelect={() => setSelected([])}
+                    className='justify-center text-center'
+                  >
+                    Clear filters
+                  </CommandItem>
+                </CommandGroup>
+              </>
+            )}
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
