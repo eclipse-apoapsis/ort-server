e16194323e25c9628dd0e9924eb1fb740e27694e
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2024-10-21 11:34:52 +0300

    fix(ui): Add missing date string options
    Seems the toLocaleString function doesn't use defaults for
    remaining format options when choosing options for some of
    them, so add the missing ones to fix the date not showing
    correctly.
    
    This is a fixup for c48f061e75287e258617a6bb73459db04d74cad3.
    
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

--- ui/src/lib/utils.ts
+++ ui/src/lib/utils.ts
@@ -28,5 +28,9 @@ export function getOrtDateTimeString(timestamp: string) {
   return new Date(timestamp).toLocaleString(navigator.language, {
     day: '2-digit',
     month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
   });
 }
