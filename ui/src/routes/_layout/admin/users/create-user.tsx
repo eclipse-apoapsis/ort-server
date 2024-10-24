ab63c5755e492986b8c8f1e7d0eeb538497f9249
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2024-10-24 09:33:30 +0300

    feat(ui): Extend the user creation form
    PR #1278 extends user creation by adding optional first name, last name
    and email to the user information.
    
    Add these fields to the user creation form and mark them as optional.
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

--- ui/src/routes/_layout/admin/users/create-user.tsx
+++ ui/src/routes/_layout/admin/users/create-user.tsx
@@ -48,6 +48,9 @@ import { toast } from '@/lib/toast';
 
 const formSchema = z.object({
   username: z.string(),
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+  email: z.string().email().optional(),
   password: z.string().optional(),
   temporary: z.boolean(),
 });
@@ -82,6 +85,9 @@ const CreateUser = () => {
     await mutateAsync({
       requestBody: {
         username: values.username,
+        firstName: values.firstName,
+        lastName: values.lastName,
+        email: values.email,
         password: values.password,
         temporary: values.temporary,
       },
@@ -104,6 +110,45 @@ const CreateUser = () => {
                   <FormLabel>Username</FormLabel>
                   <FormControl>
                     <Input {...field} autoFocus />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='firstName'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>First name</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder='(optional)' />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='lastName'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Last name</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder='(optional)' />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='email'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email address</FormLabel>
+                  <FormControl>
+                    <Input type='email' {...field} placeholder='(optional)' />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
