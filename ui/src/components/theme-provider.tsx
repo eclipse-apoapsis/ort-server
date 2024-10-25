52a7d1491575a00110ab628a7dfb7ca2fa6b0c0d
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2024-10-25 14:01:02 +0300

    refactor(ui): Fix illegal exports
    Exporting context alongside a component in code files breaks the hot
    reload functionality of `eslint-plugin-react-refresh`, so in release
    v0.4.14 this produces a warning [1], and since no warnings are allowed,
    the eslint check fails.
    
    Add a `ThemeProviderState` which works as a middleman between
    `ThemeProvider` and `ThemeProviderContext`, to ensure there are no illegal
    exports from any files.
    
    [1]: https://tinyurl.com/4whfwy5x
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

--- ui/src/components/theme-provider.tsx
+++ ui/src/components/theme-provider.tsx
@@ -17,28 +17,17 @@
  * License-Filename: LICENSE
  */
 
-import { createContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-type Theme = 'dark' | 'light' | 'system';
+import { ThemeProviderContext } from '@/components/theme-provider-context';
+
+export type Theme = 'dark' | 'light' | 'system';
 
 type ThemeProviderProps = {
   children: React.ReactNode;
   defaultTheme?: Theme;
   storageKey?: string;
 };
-
-type ThemeProviderState = {
-  theme: Theme;
-  setTheme: (theme: Theme) => void;
-};
-
-const initialState: ThemeProviderState = {
-  theme: 'light',
-  setTheme: () => null,
-};
-
-export const ThemeProviderContext =
-  createContext<ThemeProviderState>(initialState);
 
 export function ThemeProvider({
   children,
