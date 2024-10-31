edaa0c50184bd104e1ad15d57582de1f21d7bdb9
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2024-10-31 13:41:27 +0200

    feat(ui): Create a wrapper component for markdown renderer
    Create a minimal wrapper for the markdown renderer. Using a wrapper 
    component around the renderer makes it easier to tweak the formatting of 
    markdown elements when needed.
    
    Because vulnerabilities are at the moment shown wrapped in table cells and 
    it's illegal to have header elements as `<h1>` inside `<th>` elements [1], 
    headings are instead transformed into `<p>` elements with suitable 
    header-like formatting.
    
    Also minimal formatting is added to `<code>` elements, to stand out from
    ordinary text.
    
    [1]: https://html.spec.whatwg.org/#the-th-element
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

---
+++ ui/src/components/markdown-renderer.tsx
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2024 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import Markdown from 'react-markdown';
+
+type MarkdownRendererProps = {
+  markdown?: string;
+};
+
+export const MarkdownRenderer = ({ markdown }: MarkdownRendererProps) => {
+  return (
+    <Markdown
+      components={{
+        h1: ({ children }) => (
+          <p
+            role='heading'
+            aria-level={1}
+            className='mb-6 mt-6 text-xl font-semibold'
+          >
+            {children}
+          </p>
+        ),
+        h2: ({ children }) => (
+          <p
+            role='heading'
+            aria-level={2}
+            className='mb-4 mt-4 text-lg font-semibold'
+          >
+            {children}
+          </p>
+        ),
+        h3: ({ children }) => (
+          <p role='heading' aria-level={3} className='mb-2 mt-2 font-semibold'>
+            {children}
+          </p>
+        ),
+        h4: ({ children }) => (
+          <p role='heading' aria-level={4} className='mt-2 font-semibold'>
+            {children}
+          </p>
+        ),
+        h5: ({ children }) => (
+          <p role='heading' aria-level={5} className='mt-2 font-semibold'>
+            {children}
+          </p>
+        ),
+        h6: ({ children }) => (
+          <p role='heading' aria-level={6} className='mt-2 font-semibold'>
+            {children}
+          </p>
+        ),
+        code: ({ children }) => (
+          <code className='rounded bg-muted/100 p-1'>{children}</code>
+        ),
+      }}
+    >
+      {markdown}
+    </Markdown>
+  );
+};
