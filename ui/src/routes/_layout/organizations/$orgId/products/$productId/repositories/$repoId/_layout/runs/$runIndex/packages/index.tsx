76e2f8d361f44f75f2e1a9a58c1f77aeb574d315
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2024-11-07 11:44:11 +0200

    feat(ui): Show first crude version of packages table
    This is the absolute basic implementation of the "Packages" view of ORT
    run results, showing only a minimal amount of information about the found
    packages. The table will be improved with more data and properties in
    upcoming PRs.
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

--- ui/src/routes/_layout/organizations/$orgId/products/$productId/repositories/$repoId/_layout/runs/$runIndex/packages/index.tsx
+++ ui/src/routes/_layout/organizations/$orgId/products/$productId/repositories/$repoId/_layout/runs/$runIndex/packages/index.tsx
@@ -18,8 +18,19 @@
  */
 
 import { createFileRoute } from '@tanstack/react-router';
+import {
+  createColumnHelper,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
 
+import { usePackagesServiceGetPackagesByRunId } from '@/api/queries';
+import { prefetchUseRepositoriesServiceGetOrtRunByIndex } from '@/api/queries/prefetch';
+import { useRepositoriesServiceGetOrtRunByIndexSuspense } from '@/api/queries/suspense';
+import { Package } from '@/api/requests';
+import { DataTable } from '@/components/data-table/data-table';
 import { LoadingIndicator } from '@/components/loading-indicator';
+import { ToastError } from '@/components/toast-error';
 import {
   Card,
   CardContent,
@@ -27,19 +38,130 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import { Label } from '@/components/ui/label';
+import { toast } from '@/lib/toast';
+import { paginationSchema } from '@/schemas';
+
+const defaultPageSize = 10;
+
+const columnHelper = createColumnHelper<Package>();
+
+const columns = [
+  columnHelper.accessor(
+    (pkg) => {
+      const { type, namespace, name, version } = pkg.identifier;
+      return `${type ? type.concat(':') : ''}${namespace ? namespace.concat('/') : ''}${name ? name : ''}${version ? '@'.concat(version) : ''}`;
+    },
+    {
+      id: 'package',
+      header: 'Package ID',
+      cell: ({ row }) => {
+        return <div className='font-semibold'>{row.getValue('package')}</div>;
+      },
+    }
+  ),
+  columnHelper.accessor(
+    (row) => {
+      return row.processedDeclaredLicense.spdxExpression;
+    },
+    {
+      id: 'declaredLicense',
+      header: 'Declared License',
+      cell: ({ row }) => (
+        <div className='break-all'>{row.getValue('declaredLicense')}</div>
+      ),
+    }
+  ),
+  columnHelper.accessor('homepageUrl', {
+    header: 'Homepage',
+    cell: ({ row }) => (
+      <a
+        href={row.getValue('homepageUrl')}
+        target='_blank'
+        rel='noopener noreferrer'
+        className='font-semibold text-blue-400 hover:underline'
+      >
+        {row.getValue('homepageUrl')}
+      </a>
+    ),
+  }),
+];
 
 const PackagesComponent = () => {
+  const params = Route.useParams();
+  const search = Route.useSearch();
+  const pageIndex = search.page ? search.page - 1 : 0;
+  const pageSize = search.pageSize ? search.pageSize : defaultPageSize;
+
+  const { data: ortRun } = useRepositoriesServiceGetOrtRunByIndexSuspense({
+    repositoryId: Number.parseInt(params.repoId),
+    ortRunIndex: Number.parseInt(params.runIndex),
+  });
+
+  const {
+    data: packages,
+    isPending,
+    isError,
+    error,
+  } = usePackagesServiceGetPackagesByRunId({
+    runId: ortRun.id,
+    limit: pageSize,
+    offset: pageIndex * pageSize,
+  });
+
+  const table = useReactTable({
+    data: packages?.data || [],
+    columns,
+    pageCount: Math.ceil((packages?.pagination.totalCount ?? 0) / pageSize),
+    state: {
+      pagination: {
+        pageIndex,
+        pageSize,
+      },
+    },
+    getCoreRowModel: getCoreRowModel(),
+    manualPagination: true,
+  });
+
+  if (isPending) {
+    return <LoadingIndicator />;
+  }
+
+  if (isError) {
+    toast.error('Unable to load data', {
+      description: <ToastError error={error} />,
+      duration: Infinity,
+      cancel: {
+        label: 'Dismiss',
+        onClick: () => {},
+      },
+    });
+    return;
+  }
+
   return (
     <Card className='h-fit'>
       <CardHeader>
-        <CardTitle>Packages</CardTitle>
+        <CardTitle>Packages (ORT run global ID: {ortRun.id})</CardTitle>
         <CardDescription>
           Table of deduplicated packages found as dependencies of the project.
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <Label className='font-semibold'>Not implemented yet.</Label>
+        <DataTable
+          table={table}
+          setCurrentPageOptions={(currentPage) => {
+            return {
+              to: Route.to,
+              search: { ...search, page: currentPage },
+            };
+          }}
+          setPageSizeOptions={(size) => {
+            return {
+              to: Route.to,
+              search: { ...search, page: 1, pageSize: size },
+            };
+          }}
+        />
       </CardContent>
     </Card>
   );
@@ -48,6 +170,13 @@ const PackagesComponent = () => {
 export const Route = createFileRoute(
   '/_layout/organizations/$orgId/products/$productId/repositories/$repoId/_layout/runs/$runIndex/packages/'
 )({
+  validateSearch: paginationSchema,
+  loader: async ({ context, params }) => {
+    await prefetchUseRepositoriesServiceGetOrtRunByIndex(context.queryClient, {
+      repositoryId: Number.parseInt(params.repoId),
+      ortRunIndex: Number.parseInt(params.runIndex),
+    });
+  },
   component: PackagesComponent,
   pendingComponent: LoadingIndicator,
 });
