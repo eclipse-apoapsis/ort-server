aa31b4be539457ccaae33fd2c2e18a1ff26d72e3
Mikko Murto <mikko.murto@doubleopen.org>
2024-11-21 13:58:29 +0200

    feat(ui): Redirect to latest run from repository page
    To make the workflow more fluid, don't list all runs for a repository on a
    repository overview page, but redirect either to the latest run's page if a
    run exists for the repository, or to the create run page if no runs have
    yet been created for the repository. To still have access to all runs of a
    repository, add a separate runs page and a link to it to the run sidebar
    and the run number column.
    
    Also add links to repository secrets and repository users to the run
    sidebar, so accessing them doesn't require going through the runs list
    first.
    
    Signed-off-by: Mikko Murto <mikko.murto@doubleopen.org>

---
+++ ui/src/routes/_layout/organizations/$orgId/products/$productId/repositories/$repoId/index.tsx
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2024 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import { createFileRoute, redirect } from '@tanstack/react-router';
+
+import { RepositoriesService } from '@/api/requests';
+import { LoadingIndicator } from '@/components/loading-indicator';
+
+export const Route = createFileRoute(
+  '/_layout/organizations/$orgId/products/$productId/repositories/$repoId/'
+)({
+  loader: async ({ params }) => {
+    const runs = await RepositoriesService.getOrtRunsByRepositoryId({
+      repositoryId: Number.parseInt(params.repoId),
+      limit: 1,
+      sort: '-index',
+    });
+
+    const firstRun = runs.data[0];
+
+    if (firstRun) {
+      throw redirect({
+        to: '/organizations/$orgId/products/$productId/repositories/$repoId/runs/$runIndex',
+        params: {
+          orgId: params.orgId,
+          productId: params.productId,
+          repoId: params.repoId,
+          runIndex: firstRun.index.toString(),
+        },
+      });
+    } else {
+      throw redirect({
+        to: '/organizations/$orgId/products/$productId/repositories/$repoId/create-run',
+        params: {
+          orgId: params.orgId,
+          productId: params.productId,
+          repoId: params.repoId,
+        },
+      });
+    }
+  },
+  pendingComponent: LoadingIndicator,
+});
