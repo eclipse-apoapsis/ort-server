f90259ef9e223d1a1c4030ab3bc8920f953de24a
Onur Demirci <onur.demirci@bosch.io>
2024-11-27 13:26:27 +0100

    feat(cli): Add command to create an `OrtRun`
    This commit introduces the `StartCommand` as part of the `RunsCommand`.
    It creates an `OrtRun` for the provided repository and optionally  waits
    until it completes, if configured to do so. Missing configurations to
    create an `OrtRun` will be added in future updates.
    
    Signed-off-by: Onur Demirci <onur.demirci@bosch.io>

--- cli/src/main/kotlin/OrtServerMain.kt
+++ cli/src/main/kotlin/OrtServerMain.kt
@@ -21,8 +21,18 @@ package org.eclipse.apoapsis.ortserver.cli
 
 import com.github.ajalt.clikt.command.SuspendingNoOpCliktCommand
 import com.github.ajalt.clikt.command.main
+import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.groups.OptionGroup
+import com.github.ajalt.clikt.parameters.groups.provideDelegate
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
 
 import kotlin.system.exitProcess
+
+import kotlinx.serialization.json.Json
+
+import org.eclipse.apoapsis.ortserver.client.OrtServerClient
+import org.eclipse.apoapsis.ortserver.client.OrtServerClientConfig
 
 const val COMMAND_NAME = "ort-server"
 
@@ -31,4 +41,57 @@ suspend fun main(args: Array<String>) {
     exitProcess(0)
 }
 
-class OrtServerMain : SuspendingNoOpCliktCommand(COMMAND_NAME)
+class OrtServerMain : SuspendingNoOpCliktCommand(COMMAND_NAME) {
+    private val ortServerConfig by OrtServerOptions()
+
+    init {
+        subcommands(RunsCommand(ortServerConfig))
+    }
+}
+
+class OrtServerOptions : OptionGroup(
+    name = "ORT Server Options",
+    help = "Configuration options for the ORT Server instance."
+) {
+    val baseUrl by option(
+        "--base-url",
+        envvar = "ORT_SERVER_BASE_URL",
+        help = "The base URL of the ORT Server instance."
+    ).required()
+
+    val tokenUrl by option(
+        "--token-url",
+        envvar = "ORT_SERVER_TOKEN_URL",
+        help = "The URL to request a token for the ORT Server instance."
+    ).required()
+
+    val clientId by option(
+        "--client-id",
+        envvar = "ORT_SERVER_CLIENT_ID",
+        help = "The client ID to authenticate with the ORT Server instance."
+    ).required()
+
+    val username by option(
+        "--username",
+        envvar = "ORT_SERVER_USERNAME",
+        help = "The username to authenticate with the ORT Server instance."
+    ).required()
+
+    val password by option(
+        "--password",
+        envvar = "ORT_SERVER_PASSWORD",
+        help = "The password to authenticate with the ORT Server instance."
+    ).required()
+
+    fun toOrtServerClientConfig(): OrtServerClientConfig {
+        return OrtServerClientConfig(
+            baseUrl = baseUrl,
+            tokenUrl = tokenUrl,
+            clientId = clientId,
+            username = username,
+            password = password
+        )
+    }
+}
+
+internal val json = Json(OrtServerClient.JSON) { prettyPrint = true }
