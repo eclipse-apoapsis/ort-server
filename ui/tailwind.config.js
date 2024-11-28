13147660782e3f09c80b66cab7b01aad8e2bcf2a
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2024-11-28 07:41:16 +0200

    fix(ui): Remove backticks from code elements
    `tailwind-typography` plugin needs a bit of tweaking [1] for `::before`
    and `::after` elements to prevent rendering of backticks around code 
    elements.
    
    [1]: https://tinyurl.com/4dvmdm5u
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -84,6 +84,18 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            'code::before': {
+              content: '""',
+            },
+            'code::after': {
+              content: '""',
+            },
+          },
+        },
+      },
     },
   },
   plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
