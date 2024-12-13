91388d723ee7f4ff6d44172480f4dbecdc4efa1e
Martin Nonnenmacher <martin.nonnenmacher@bosch.com>
2024-12-13 20:40:40 +0100

    chore(compose): Update the health check script
    Since Keycloak 25.0.0 Java 21 is used [1] where the `URL(String)`
    constructor is deprecated, therefore use `URI(String).toURL()` instead.
    
    Also, the health checks now run at port 9000 [2].
    
    [1]: https://www.keycloak.org/docs/latest/release_notes/index.html#java-21-support
    [2]: https://www.keycloak.org/docs/latest/release_notes/index.html#management-port-for-metrics-and-health-endpoints
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.com>

--- scripts/docker/keycloak/HealthCheck.java
+++ scripts/docker/keycloak/HealthCheck.java
@@ -1,8 +1,8 @@
 // Health check script for Keycloak Docker container, to be used in Docker Compose.
 public class HealthCheck {
     public static void main(String[] args) throws java.lang.Throwable {
-        String url = "http://localhost:8080/health/live";
-        int responseCode = ((java.net.HttpURLConnection) new java.net.URL(url).openConnection()).getResponseCode();
+        String url = "http://localhost:9000/health/live";
+        int responseCode = ((java.net.HttpURLConnection) new java.net.URI(url).toURL().openConnection()).getResponseCode();
         System.exit(java.net.HttpURLConnection.HTTP_OK == responseCode ? 0 : 1);
     }
 }
