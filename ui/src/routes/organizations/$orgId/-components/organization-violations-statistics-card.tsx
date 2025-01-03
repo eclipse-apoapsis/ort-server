fe9da04dcf72015dbe9f0fab36a5debadddb8433
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2025-01-03 12:34:27 +0200

    feat(ui): Statistics cards for organization overview page
    With PR #1721, a statistics endpoint was added for organizations. Do
    further alignment between the organization and product overview pages
    by using the endpoint to show the same statistics cards (for issues,
    vulnerabilities, rule violations and packages) for organization
    that have already been implemented for product.
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

---
+++ ui/src/routes/organizations/$orgId/-components/organization-violations-statistics-card.tsx
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2024 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import { Scale } from 'lucide-react';
+
+import { useOrganizationsServiceGetOrtRunStatisticsByOrganizationIdSuspense } from '@/api/queries/suspense';
+import { Severity } from '@/api/requests';
+import { StatisticsCard } from '@/components/statistics-card';
+import { getRuleViolationSeverityBackgroundColor } from '@/helpers/get-status-class';
+import { cn } from '@/lib/utils';
+
+type OrganizationViolationsStatisticsCardProps = {
+  organizationId: number;
+  className?: string;
+};
+
+export const OrganizationViolationsStatisticsCard = ({
+  organizationId,
+  className,
+}: OrganizationViolationsStatisticsCardProps) => {
+  const data =
+    useOrganizationsServiceGetOrtRunStatisticsByOrganizationIdSuspense({
+      organizationId: organizationId,
+    });
+
+  const total = data.data.ruleViolationsCount;
+  const counts = data.data.ruleViolationsCountBySeverity;
+
+  return (
+    <StatisticsCard
+      title='Rule Violations'
+      icon={() => <Scale className='h-4 w-4 text-green-500' />}
+      value={total || '-'}
+      counts={
+        counts
+          ? Object.entries(counts).map(([severity, count]) => ({
+              key: severity,
+              count: count,
+              color: getRuleViolationSeverityBackgroundColor(
+                severity as Severity
+              ),
+            }))
+          : []
+      }
+      className={cn('h-full hover:bg-muted/50', className)}
+    />
+  );
+};
