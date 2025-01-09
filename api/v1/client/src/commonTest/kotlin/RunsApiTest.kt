bdceda51835e3ac11d56d6bafd86bfadf2b84713
Marcel Bochtler <marcel.bochtler@bosch.com>
2025-01-09 10:27:08 +0100

    style: Fix missing empty lines in imports
    This fixes multiple `detekt` issues.
    
    Signed-off-by: Marcel Bochtler <marcel.bochtler@bosch.com>

--- api/v1/client/src/commonTest/kotlin/RunsApiTest.kt
+++ api/v1/client/src/commonTest/kotlin/RunsApiTest.kt
@@ -21,6 +21,7 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
+
 import io.ktor.client.engine.mock.MockEngine
 import io.ktor.client.engine.mock.respondError
 import io.ktor.http.HttpStatusCode
