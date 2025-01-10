6c0d25d6a355bb2e7a6cfb578b7facf4f6fbcd93
Oliver Heger <oliver.heger@bosch.io>
2025-01-10 12:38:13 +0100

    feat(tasks): Introduce a new module for tasks
    The new module is going to host the implementations of maintenance tasks
    that should be scheduled periodically.
    
    Initially, add a basic interface for such tasks.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.io>

---
+++ tasks/build.gradle.kts
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2025 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+plugins {
+    // Apply precompiled plugins.
+    id("ort-server-kotlin-jvm-conventions")
+    id("ort-server-publication-conventions")
+}
+
+group = "org.eclipse.apoapsis.ortserver.tasks"
+
+dependencies {
+    implementation(projects.config.configSpi)
+    implementation(projects.dao)
+
+    implementation(libs.koinCore)
+    implementation(libs.kotlinxCoroutines)
+    implementation(libs.logback)
+    implementation(libs.typesafeConfig)
+
+    testImplementation(libs.kotestRunnerJunit5)
+    testImplementation(libs.mockk)
+}
