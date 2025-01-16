603bb6ea633e40e608550a616753e815f7411ccf
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2025-01-16 14:18:27 +0200

    refactor(ui): Edit showing search input for select filter
    Only show the search input for a specific value if there are
    more than five options to choose from.
    
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

--- ui/src/components/data-table/filter-multi-select.tsx
+++ ui/src/components/data-table/filter-multi-select.tsx
@@ -69,7 +69,7 @@ export function FilterMultiSelect<TValue>({
       </PopoverTrigger>
       <PopoverContent className='w-[200px] p-0' align='start'>
         <Command>
-          <CommandInput placeholder={title} />
+          {options.length > 5 && <CommandInput placeholder={title} />}
           <CommandList>
             <CommandEmpty>No results found.</CommandEmpty>
             <CommandGroup>
