a8b77c4fa6bc2b50c9ae6cee2094a9a0340320e0
Sebastian Schuberth <sebastian@doubleopen.org>
2025-01-29 15:48:37 +0100

    feat(ui): Introduce a component for inline code formatting
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

---
+++ ui/src/components/typography.tsx
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2025 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+export function InlineCode({ children }: { children: string }) {
+  return (
+    <code className='relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold'>
+      {children}
+    </code>
+  );
+}
