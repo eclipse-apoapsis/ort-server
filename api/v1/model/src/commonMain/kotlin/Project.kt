518c05e4f11e10917c379cff5cf5bde5dba4678e
Julian Olderdissen <julian.olderdissen@bosch.com>
2025-02-18 15:41:03 +0100

    feat(api): Add projects endpoint for ORT runs
    Introduce a new API endpoint to expose project data for an ORT run. This
    change defines the API model, mapping, route, and documentation
    necessary to support clients retrieving projects.
    
    Resolves #1613.
    
    Signed-off-by: Julian Olderdissen <julian.olderdissen@bosch.com>

---
+++ api/v1/model/src/commonMain/kotlin/Project.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2025 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.eclipse.apoapsis.ortserver.api.v1.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Project(
+    val identifier: Identifier,
+    val cpe: String? = null,
+    val definitionFilePath: String,
+    val authors: Set<String>,
+    val declaredLicenses: Set<String>,
+    val processedDeclaredLicense: ProcessedDeclaredLicense,
+    val vcs: VcsInfo,
+    val vcsProcessed: VcsInfo,
+    val description: String,
+    val homepageUrl: String,
+    val scopeNames: Set<String>
+)
