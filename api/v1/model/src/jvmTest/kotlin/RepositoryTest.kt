27741a5f3b97dd15337a197b3a830eaeecaac2fb
Marcel Bochtler <marcel.bochtler@bosch.com>
2025-02-20 15:22:53 +0100

    fix(api): Add basic host name validation
    Unfortunately, Ktor's `parseUrl()` does not validate the host of the
    URL. Add a check for allowed characters for the URL's host, according to
    the RFC 1738 [1].
    
    ```
    A sequence of domain labels separated by ".", each domain
    label starting and ending with an alphanumerical character and
    possibly also containing "-" characters. The rightmost domain
    label will never start with a digit, though, which
    syntactically distinguishes all domain names from the IP
    addresses.
    ```
    
    [1]: https://www.rfc-editor.org/rfc/rfc1738.html
    
    Signed-off-by: Marcel Bochtler <marcel.bochtler@bosch.com>

---
+++ api/v1/model/src/jvmTest/kotlin/RepositoryTest.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2025 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+
+import org.eclipse.apoapsis.ortserver.api.v1.model.Repository
+
+class RepositoryTest : StringSpec({
+    "isValidUrl()" should {
+        "validate the repository URL" {
+            val validUrls = listOf(
+                "http://example.com",
+                "https://example.com",
+                "https://example.com/path",
+                "https://example.com:8080",
+                "https://example.com/path?query=string",
+                "https://example.com/path#fragment",
+                "https://example.com/path?query=string#fragment",
+                "https://example.com/path#fragment?query=string",
+                "http://127.0.0.1",
+                "http://localhost"
+            )
+
+            validUrls.forAll { Repository.isValidUrl(it) shouldBe true }
+
+            val invalidUrls = listOf(
+                "https://exam ple.com/",
+                "invalidurl"
+            )
+
+            invalidUrls.forAll { Repository.isValidUrl(it) shouldBe false }
+        }
+    }
+})
