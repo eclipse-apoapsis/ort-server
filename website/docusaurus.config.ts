2cc40c239bb9995d53e9a6cd489953ba44d4b215
Martin Nonnenmacher <martin.nonnenmacher@bosch.com>
2025-02-27 15:23:47 +0100

    docs(website): Fix label of introduction link in footer
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.com>

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -136,7 +136,7 @@ const config: Config = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
+              label: 'Introduction',
               to: '/docs/intro',
             },
           ],
