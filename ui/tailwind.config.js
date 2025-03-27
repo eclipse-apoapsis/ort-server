a88bea12635090edc278f0d6dabc7e509f2bcb7b
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2025-03-27 12:17:12 +0200

    build(ui): Replace the animation library
    The current animation library for TailwindCSS [1] hasn't had any activity
    in two years, and `shadcn-ui` library has deprecated its usage. Replace 
    it with a TailwindCSS v4 compliant library [2], as instructed in [3].
    
    [1]: https://github.com/jamiebuilds/tailwindcss-animate
    [2]: https://github.com/Wombosvideo/tw-animate-css/
    [3]: https://ui.shadcn.com/docs/tailwind-v4
    
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -18,7 +18,6 @@
  */
 
 import tailwindcssTypography from '@tailwindcss/typography';
-import tailwindcssAnimate from 'tailwindcss-animate';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -73,16 +72,6 @@ export default {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
-      keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
-        },
-      },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
@@ -101,5 +90,5 @@ export default {
       },
     },
   },
-  plugins: [tailwindcssAnimate, tailwindcssTypography],
+  plugins: [tailwindcssTypography],
 };
