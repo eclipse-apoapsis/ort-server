5028e5d5bde4065700142f079589eaf614b8c2d0
Wolfgang Klenk <wolfgang.klenk2@bosch.com>
2025-04-01 09:57:08 +0200

    feat(ui): Introduce EllipsisIconButton component for action menus
    Add a reusable EllipsisIconButton component that renders a small,
    icon-only button with a horizontal ellipsis. This button is designed
    to be used as a trigger for dropdown or context menus in components
    like data tables.
    Using a single ellipsis button instead of multiple visible action
    buttons reduces visual clutter, improves clarity, and scales better as
    the number of available actions grows. It also improves consistency
    across components that require compact action triggers.
    
    Signed-off-by: Wolfgang Klenk <wolfgang.klenk2@bosch.com>

---
+++ ui/src/components/ellipsis-icon-button.tsx
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2025 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import { EllipsisIcon } from 'lucide-react';
+import { forwardRef } from 'react';
+
+import { cn } from '@/lib/utils';
+import { Button } from './ui/button';
+
+type EllipsisIconButtonProps = {
+  className?: string;
+  disabled?: boolean;
+};
+
+const EllipsisIconButton = forwardRef<
+  HTMLButtonElement,
+  EllipsisIconButtonProps
+>(({ className, ...props }, ref) => {
+  return (
+    <Button
+      size='sm'
+      variant='outline'
+      {...props}
+      className={cn('h-8 px-2', className)}
+      ref={ref}
+    >
+      <span className='sr-only'>Delete</span>
+      <EllipsisIcon size={16} />
+    </Button>
+  );
+});
+EllipsisIconButton.displayName = 'EllipsisIconButton';
+
+export { EllipsisIconButton };
