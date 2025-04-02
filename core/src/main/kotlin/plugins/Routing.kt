ca5031f6ca8c8889c747985c25d4cef470aa288d
Martin Nonnenmacher <martin.nonnenmacher@bosch.com>
2025-04-02 15:13:54 +0200

    feat(core): Integrate plugins route
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.com>

--- core/src/main/kotlin/plugins/Routing.kt
+++ core/src/main/kotlin/plugins/Routing.kt
@@ -25,6 +25,7 @@ import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 
 import org.eclipse.apoapsis.ortserver.components.authorization.SecurityConfigurations
+import org.eclipse.apoapsis.ortserver.components.pluginmanager.endpoints.getinstalledplugins.getInstalledPlugins
 import org.eclipse.apoapsis.ortserver.core.api.admin
 import org.eclipse.apoapsis.ortserver.core.api.authentication
 import org.eclipse.apoapsis.ortserver.core.api.downloads
@@ -42,6 +43,7 @@ fun Application.configureRouting() {
             healthChecks()
             downloads()
             authenticate(SecurityConfigurations.token) {
+                getInstalledPlugins()
                 admin()
                 organizations()
                 products()
