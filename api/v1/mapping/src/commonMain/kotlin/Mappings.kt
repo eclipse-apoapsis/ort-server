6bd81725ca78dcb64dac4566954977e10ba5922f
Kamil Bielecki <kamil.bielecki@pl.bosch.com>
2025-04-10 14:00:39 +0200

    feat(API): Extend ort run package API with curation info
    This commit extends packages list for ORT Run API endpoint
    with information on curations applied.
    
    resolves #2324
    
    Signed-off-by: Kamil Bielecki <kamil.bielecki@pl.bosch.com>

--- api/v1/mapping/src/commonMain/kotlin/Mappings.kt
+++ api/v1/mapping/src/commonMain/kotlin/Mappings.kt
@@ -27,6 +27,7 @@ import org.eclipse.apoapsis.ortserver.api.v1.model.AnalyzerJob as ApiAnalyzerJob
 import org.eclipse.apoapsis.ortserver.api.v1.model.AnalyzerJobConfiguration as ApiAnalyzerJobConfiguration
 import org.eclipse.apoapsis.ortserver.api.v1.model.ComparisonOperator as ApiComparisonOperator
 import org.eclipse.apoapsis.ortserver.api.v1.model.CredentialsType as ApiCredentialsType
+import org.eclipse.apoapsis.ortserver.api.v1.model.Curation as ApiCuration
 import org.eclipse.apoapsis.ortserver.api.v1.model.EcosystemStats as ApiEcosystemStats
 import org.eclipse.apoapsis.ortserver.api.v1.model.EnvironmentConfig as ApiEnvironmentConfig
 import org.eclipse.apoapsis.ortserver.api.v1.model.EnvironmentVariableDeclaration as ApiEnvironmentVariableDeclaration
@@ -153,6 +154,7 @@ import org.eclipse.apoapsis.ortserver.model.runs.ShortestDependencyPath
 import org.eclipse.apoapsis.ortserver.model.runs.VcsInfo
 import org.eclipse.apoapsis.ortserver.model.runs.advisor.Vulnerability
 import org.eclipse.apoapsis.ortserver.model.runs.advisor.VulnerabilityReference
+import org.eclipse.apoapsis.ortserver.model.runs.repository.PackageCurationData
 import org.eclipse.apoapsis.ortserver.model.util.ComparisonOperator
 import org.eclipse.apoapsis.ortserver.model.util.FilterOperatorAndValue
 import org.eclipse.apoapsis.ortserver.model.util.ListQueryParameters
@@ -872,7 +874,17 @@ fun PackageRunData.mapToApi() = ApiPackage(
     pkg.vcsProcessed.mapToApi(),
     pkg.isMetadataOnly,
     pkg.isModified,
-    shortestDependencyPaths.map { it.mapToApi() }
+    shortestDependencyPaths.map { it.mapToApi() },
+    curations.map { it.mapToApi() }
+)
+
+fun PackageCurationData.mapToApi(): ApiCuration = ApiCuration(
+    concludedLicense ?: "",
+    comment ?: "",
+    description ?: "",
+    homepageUrl ?: "",
+    authors?.toList() ?: emptyList(),
+    declaredLicenseMapping
 )
 
 fun ApiPackageFilters.mapToModel(): PackageFilters =
