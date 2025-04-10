ce6d0b4b7add986892c947d3f79e3ce937becdb1
Marcel Bochtler <git@bochtler.io>
2025-04-10 14:06:52 +0200

    feat(cli): Add progress bar if the `--wait` flag is used
    Add a progress bar to the CLI output when the `--wait` flag is used to
    provide live feedback to the user while waiting for the run to finish.
    
    Signed-off-by: Marcel Bochtler <git@bochtler.io>

--- cli/src/commonMain/kotlin/StartCommand.kt
+++ cli/src/commonMain/kotlin/StartCommand.kt
@@ -21,6 +21,7 @@ package org.eclipse.apoapsis.ortserver.cli
 
 import com.github.ajalt.clikt.command.SuspendingCliktCommand
 import com.github.ajalt.clikt.core.Context
+import com.github.ajalt.clikt.core.terminal
 import com.github.ajalt.clikt.parameters.groups.mutuallyExclusiveOptions
 import com.github.ajalt.clikt.parameters.groups.required
 import com.github.ajalt.clikt.parameters.options.convert
@@ -28,14 +29,25 @@ import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.long
+import com.github.ajalt.mordant.animation.coroutines.animateInCoroutine
+import com.github.ajalt.mordant.widgets.Spinner
+import com.github.ajalt.mordant.widgets.progress.percentage
+import com.github.ajalt.mordant.widgets.progress.progressBar
+import com.github.ajalt.mordant.widgets.progress.progressBarContextLayout
+import com.github.ajalt.mordant.widgets.progress.spinner
+import com.github.ajalt.mordant.widgets.progress.text
 
 import kotlin.time.Duration.Companion.seconds
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 import okio.Path.Companion.toPath
 
 import org.eclipse.apoapsis.ortserver.api.v1.model.CreateOrtRun
+import org.eclipse.apoapsis.ortserver.api.v1.model.JobConfigurations
+import org.eclipse.apoapsis.ortserver.api.v1.model.Jobs
 import org.eclipse.apoapsis.ortserver.api.v1.model.OrtRun
 import org.eclipse.apoapsis.ortserver.api.v1.model.OrtRunStatus
 import org.eclipse.apoapsis.ortserver.cli.model.AuthenticationError
@@ -46,6 +58,7 @@ import org.eclipse.apoapsis.ortserver.cli.utils.echoMessage
 import org.eclipse.apoapsis.ortserver.cli.utils.read
 import org.eclipse.apoapsis.ortserver.cli.utils.useJsonFormat
 import org.eclipse.apoapsis.ortserver.client.NotFoundException
+import org.eclipse.apoapsis.ortserver.client.OrtServerClient
 
 internal val POLL_INTERVAL = getEnv("POLL_INTERVAL")?.toLongOrNull()?.seconds ?: 10.seconds
 
@@ -100,15 +113,98 @@ class StartCommand : SuspendingCliktCommand(name = "start") {
         ContextStorage.saveLatestRunId(ortRun.id)
 
         if (wait) {
-            while (ortRun.isRunning()) {
-                delay(POLL_INTERVAL)
-                ortRun = client.repositories.getOrtRun(repositoryId, ortRun.index)
-                if (!useJsonFormat) echoMessage(ortRun.toPrintable())
+            if (useJsonFormat) {
+                while (ortRun.isRunning()) {
+                    delay(POLL_INTERVAL)
+                    ortRun = client.repositories.getOrtRun(repositoryId, ortRun.index)
+                }
+            } else {
+                echoMessage(ortRun.toPrintable())
+                ortRun = updateProgressBar(ortRun, client)
             }
         }
 
         echoMessage(ortRun.toPrintable())
     }
+
+    private fun updateProgressBar(ortRun: OrtRun, client: OrtServerClient): OrtRun = runBlocking {
+        val maxProgressBarValue = JobType.entries.count { it.isConfigured(ortRun.jobConfigs) }.withInitialState()
+
+        val progress = progressBarContextLayout {
+            text { context }
+            spinner(Spinner.Dots())
+            progressBar(width = 30)
+            percentage()
+        }.animateInCoroutine(
+            terminal,
+            context = "Starting",
+            total = maxProgressBarValue,
+            completed = 0
+        )
+
+        launch { progress.execute() }
+
+        var updatedOrtRun = ortRun.copy()
+        while (updatedOrtRun.isRunning()) {
+            delay(POLL_INTERVAL)
+            updatedOrtRun = client.repositories.getOrtRun(repositoryId, updatedOrtRun.index)
+            progress.update {
+                context =
+                    JobType.entries.reversed().find { it.isStarted(updatedOrtRun.jobs) }?.displayName ?: "Starting"
+                completed = JobType.entries.count { it.isFinished(updatedOrtRun.jobs) }.withInitialState()
+            }
+        }
+
+        progress.update {
+            context = "Finished"
+            completed = maxProgressBarValue
+        }
+
+        updatedOrtRun
+    }
+
+    private enum class JobType(val displayName: String) {
+        ANALYZER("Analyzing"),
+        ADVISOR("Advising"),
+        SCANNER("Scanning"),
+        EVALUATOR("Evaluating"),
+        REPORTER("Reporting"),
+        NOTIFIER("Notifying");
+
+        fun isConfigured(config: JobConfigurations) = when (this) {
+            ANALYZER -> true
+            ADVISOR -> config.advisor != null
+            SCANNER -> config.scanner != null
+            EVALUATOR -> config.evaluator != null
+            REPORTER -> config.reporter != null
+            NOTIFIER -> config.notifier != null
+        }
+
+        fun isStarted(jobs: Jobs) = when (this) {
+            ANALYZER -> jobs.analyzer != null
+            ADVISOR -> jobs.advisor != null
+            SCANNER -> jobs.scanner != null
+            EVALUATOR -> jobs.evaluator != null
+            REPORTER -> jobs.reporter != null
+            NOTIFIER -> jobs.notifier != null
+        }
+
+        fun isFinished(jobs: Jobs) = when (this) {
+            ANALYZER -> jobs.analyzer?.finishedAt != null
+            ADVISOR -> jobs.advisor?.finishedAt != null
+            SCANNER -> jobs.scanner?.finishedAt != null
+            EVALUATOR -> jobs.evaluator?.finishedAt != null
+            REPORTER -> jobs.reporter?.finishedAt != null
+            NOTIFIER -> jobs.notifier?.finishedAt != null
+        }
+    }
+
+    /**
+     * The first job status change is 'Starting' -> 'Analyzing'.
+     * Add the artificial 'Starting' state to the maximum possible progress bar value to correctly represent this in the
+     * progress bar.
+     */
+    private fun Int.withInitialState(): Long = (this + 1).toLong()
 }
 
 private fun OrtRun.isRunning() =
