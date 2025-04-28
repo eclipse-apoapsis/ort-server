8c9c07fdadb538690ca99d1159b01b7c6a067e64
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2025-04-28 13:59:39 +0300

    feat(ui): Add a minimal error component
    Add a component which will be shown whenever an error boundary is hit,
    instead of the boundary children. At the moment the component only shows
    the error message, but more functionality can be added to it as a
    follow-up.
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

---
+++ ui/src/components/error-component.tsx
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2025 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import { ErrorComponentProps } from '@tanstack/react-router';
+import { TriangleAlert } from 'lucide-react';
+
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+export const ErrorComponent = ({ error }: ErrorComponentProps) => {
+  return (
+    <Card className='flex h-full gap-4'>
+      <CardHeader>
+        <CardTitle className='flex gap-2 align-baseline'>
+          <TriangleAlert className='text-traffic-light-red size-5' />
+          <div>Oops, something went wrong...</div>
+        </CardTitle>
+        <CardDescription>{`Error: ${error.message}`}</CardDescription>
+      </CardHeader>
+    </Card>
+  );
+};
