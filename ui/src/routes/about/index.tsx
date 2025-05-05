13c59b96e8f3b6db0b3067ecdbb0ff4c46730c2b
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2025-05-05 15:15:27 +0300

    feat(ui): Add download links to OSC packets to "About" page
    If the ORT server version returned from the back-end includes a
    "-RC." string, assume it is a release candidate, for which there
    are no specific asset download links available in GitHub. In that
    case, use the latest release downloads page URL for download links;
    otherwise, use the explicit version tag.
    
    Resolves #2258.
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

--- ui/src/routes/about/index.tsx
+++ ui/src/routes/about/index.tsx
@@ -23,9 +23,23 @@ import { Fragment } from 'react';
 import { useVersionsServiceGetApiV1VersionsSuspense } from '@/api/queries/suspense';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
+import { GITHUB_LATEST_RELEASE_DOWNLOAD_URL } from '@/lib/constants';
 
 export const About = () => {
   const { data: versionData } = useVersionsServiceGetApiV1VersionsSuspense();
+
+  // Check if the ORT Server version is a release candidate (RC) version
+  // by looking for the '-RC.' substring in the version string.
+  // If it is, use the latest release download URL.
+  // Otherwise, use the specific version download URL.
+  const ortServerVersion = versionData['ORT Server'];
+  const isVersionCandidate = ortServerVersion?.includes('-RC.');
+  const baseUrl = isVersionCandidate
+    ? GITHUB_LATEST_RELEASE_DOWNLOAD_URL
+    : GITHUB_LATEST_RELEASE_DOWNLOAD_URL.replace(
+        'latest/download',
+        `download/${ortServerVersion}`
+      );
 
   return (
     <Card className='mx-auto w-full max-w-4xl'>
@@ -43,6 +57,51 @@ export const About = () => {
                 <div className='text-muted-foreground'>{value}</div>
               </Fragment>
             ))}
+          </div>
+          <h3 className='mt-8 mb-4 font-semibold'>
+            Download ORT Server Client CLI (osc)
+          </h3>
+          <div className='grid grid-cols-[auto_1fr] gap-x-8 gap-y-1'>
+            <div className='text-muted-foreground font-semibold'>Linux</div>
+            <a
+              href={`${baseUrl}/osc-cli-linux-x64.tar.gz`}
+              className='font-semibold text-blue-400 hover:underline'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              osc-cli-linux-x64
+            </a>
+            <div className='text-muted-foreground font-semibold'>
+              macOS (ARM64)
+            </div>
+            <a
+              href={`${baseUrl}/osc-cli-macos-arm64.tar.gz`}
+              className='font-semibold text-blue-400 hover:underline'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              osc-cli-macos-arm64
+            </a>
+            <div className='text-muted-foreground font-semibold'>
+              macOS (x64)
+            </div>
+            <a
+              href={`${baseUrl}/osc-cli-macos-x64.tar.gz`}
+              className='font-semibold text-blue-400 hover:underline'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              osc-cli-macos-x64
+            </a>
+            <div className='text-muted-foreground font-semibold'>Windows</div>
+            <a
+              href={`${baseUrl}/osc-cli-windows-x64.zip`}
+              className='font-semibold text-blue-400 hover:underline'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              osc-cli-windows-x64
+            </a>
           </div>
         </div>
       </CardContent>
