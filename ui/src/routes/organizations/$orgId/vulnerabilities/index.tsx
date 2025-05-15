8c7719f0f3a93046689131c6d151092d6b5edc8e
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2025-05-15 06:53:05 +0300

    refactor(ui): Refactor organization vulnerabilities table
    Earlier on, due to infinite rerendering problems, the organization 
    vulnerabilities page was required to be separated into two components: 
    - index page which handles data loading
    - table component which handles rendering of the table
    This somewhat artificial division introduced an unnecessary interface 
    between the two components.
    
    Infinite rerendering problem was already fixed in an earlier PR by 
    memoizing the table column definition [1], so remove the division to 
    two components to simplify code and enable easy implementation of 
    linking to organization vulnerabilities.
    
    [1]: https://tanstack.com/table/latest/docs/faq#how-do-i-stop-infinite-rendering-loops
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

--- ui/src/routes/organizations/$orgId/vulnerabilities/index.tsx
+++ ui/src/routes/organizations/$orgId/vulnerabilities/index.tsx
@@ -17,11 +17,27 @@
  * License-Filename: LICENSE
  */
 
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
+import {
+  createColumnHelper,
+  getCoreRowModel,
+  getExpandedRowModel,
+  Row,
+  useReactTable,
+} from '@tanstack/react-table';
+import { ChevronDown, ChevronUp } from 'lucide-react';
+import { useMemo } from 'react';
 
+import { useOrganizationsServiceGetApiV1OrganizationsByOrganizationIdVulnerabilities } from '@/api/queries';
 import { prefetchUseOrganizationsServiceGetApiV1OrganizationsByOrganizationId } from '@/api/queries/prefetch';
-import { useOrganizationsServiceGetApiV1OrganizationsByOrganizationIdVulnerabilitiesSuspense } from '@/api/queries/suspense';
+import { OrganizationVulnerability } from '@/api/requests';
+import { VulnerabilityMetrics } from '@/components/charts/vulnerability-metrics';
+import { DataTable } from '@/components/data-table/data-table';
 import { LoadingIndicator } from '@/components/loading-indicator';
+import { MarkdownRenderer } from '@/components/markdown-renderer';
+import { ToastError } from '@/components/toast-error';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
@@ -30,21 +46,241 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { getVulnerabilityRatingBackgroundColor } from '@/helpers/get-status-class';
+import {
+  convertToBackendSorting,
+  updateColumnSorting,
+} from '@/helpers/handle-multisort';
+import { identifierToString } from '@/helpers/identifier-to-string';
+import { compareVulnerabilityRating } from '@/helpers/sorting-functions';
+import { toast } from '@/lib/toast';
+import {
   paginationSearchParameterSchema,
   sortingSearchParameterSchema,
 } from '@/schemas';
-import { OrganizationVulnerabilityTable } from './-components/organization-vulnerability-table';
+
+const defaultPageSize = 10;
+
+const columnHelper = createColumnHelper<OrganizationVulnerability>();
+
+const renderSubComponent = ({
+  row,
+}: {
+  row: Row<OrganizationVulnerability>;
+}) => {
+  const vulnerability = row.original.vulnerability;
+
+  return (
+    <div className='flex flex-col gap-4'>
+      <VulnerabilityMetrics vulnerability={vulnerability} />
+      <div className='text-lg font-semibold'>Description</div>
+      <MarkdownRenderer
+        markdown={vulnerability.description || 'No description.'}
+      />
+      <div className='mt-2 text-lg font-semibold'>
+        Links to vulnerability references
+      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Severity</TableHead>
+            <TableHead>Scoring system</TableHead>
+            <TableHead>Score</TableHead>
+            <TableHead>Vector</TableHead>
+            <TableHead>Link</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {vulnerability.references
+            .sort((refA, refB) => (refB.score ?? 0) - (refA.score ?? 0))
+            .map((reference, index) => (
+              <TableRow key={index}>
+                <TableCell>{reference.severity || '-'}</TableCell>
+                <TableCell>{reference.scoringSystem || '-'}</TableCell>
+                <TableCell>{reference.score || '-'}</TableCell>
+                <TableCell>{reference.vector || '-'}</TableCell>
+                <TableCell>
+                  {
+                    <Link
+                      className='font-semibold break-all text-blue-400 hover:underline'
+                      to={reference.url}
+                      target='_blank'
+                    >
+                      {reference.url}
+                    </Link>
+                  }
+                </TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
 
 const OrganizationVulnerabilitiesComponent = () => {
   const params = Route.useParams();
+  const search = Route.useSearch();
+  const pageIndex = search.page ? search.page - 1 : 0;
+  const pageSize = search.pageSize ? search.pageSize : defaultPageSize;
 
-  const { data: vulnerabilities } =
-    useOrganizationsServiceGetApiV1OrganizationsByOrganizationIdVulnerabilitiesSuspense(
-      {
-        organizationId: Number.parseInt(params.orgId),
-        limit: 1,
-      }
-    );
+  const {
+    data: vulnerabilities,
+    error,
+    isPending,
+    isError,
+  } = useOrganizationsServiceGetApiV1OrganizationsByOrganizationIdVulnerabilities(
+    {
+      organizationId: Number.parseInt(params.orgId),
+      limit: pageSize,
+      offset: pageIndex * pageSize,
+      sort: convertToBackendSorting(search.sortBy),
+    }
+  );
+
+  // Prevent infinite rerenders by providing a stable reference to columns via memoization.
+  // https://tanstack.com/table/latest/docs/faq#solution-1-stable-references-with-usememo-or-usestate
+  const columns = useMemo(
+    () => [
+      columnHelper.display({
+        id: 'moreInfo',
+        header: 'Details',
+        size: 50,
+        cell: ({ row }) => {
+          return row.getCanExpand() ? (
+            <Button
+              variant='outline'
+              size='sm'
+              {...{
+                onClick: row.getToggleExpandedHandler(),
+                style: { cursor: 'pointer' },
+              }}
+            >
+              {row.getIsExpanded() ? (
+                <ChevronUp className='h-4 w-4' />
+              ) : (
+                <ChevronDown className='h-4 w-4' />
+              )}
+            </Button>
+          ) : (
+            'No info'
+          );
+        },
+        enableSorting: false,
+        enableColumnFilter: false,
+      }),
+      columnHelper.accessor('rating', {
+        id: 'rating',
+        header: 'Rating',
+        cell: ({ row }) => {
+          return (
+            <Badge
+              className={`${getVulnerabilityRatingBackgroundColor(row.getValue('rating'))}`}
+            >
+              {row.getValue('rating')}
+            </Badge>
+          );
+        },
+        sortingFn: (rowA, rowB) => {
+          return compareVulnerabilityRating(
+            rowA.getValue('rating'),
+            rowB.getValue('rating')
+          );
+        },
+        enableColumnFilter: false,
+      }),
+      columnHelper.accessor('repositoriesCount', {
+        id: 'repositoriesCount',
+        header: 'Repositories',
+        cell: ({ row }) => {
+          return <div>{row.getValue('repositoriesCount')}</div>;
+        },
+        enableColumnFilter: false,
+      }),
+      columnHelper.accessor(
+        (vuln) => {
+          return identifierToString(vuln.identifier);
+        },
+        {
+          id: 'identifier',
+          header: 'Package ID',
+          cell: ({ row }) => {
+            return (
+              <div className='font-semibold'>{row.getValue('identifier')}</div>
+            );
+          },
+          enableColumnFilter: false,
+        }
+      ),
+      columnHelper.accessor('vulnerability.externalId', {
+        id: 'externalId',
+        header: 'External ID',
+        cell: ({ row }) => (
+          <Badge className='bg-blue-300 whitespace-nowrap'>
+            {row.getValue('externalId')}
+          </Badge>
+        ),
+        enableColumnFilter: false,
+      }),
+      columnHelper.accessor('vulnerability.summary', {
+        id: 'summary',
+        header: 'Summary',
+        cell: ({ row }) => {
+          return (
+            <div className='text-muted-foreground italic'>
+              {row.original.vulnerability.summary}
+            </div>
+          );
+        },
+        enableSorting: false,
+        enableColumnFilter: false,
+      }),
+    ],
+    []
+  );
+
+  const table = useReactTable({
+    data: vulnerabilities?.data || [],
+    columns,
+    pageCount: Math.ceil(
+      (vulnerabilities?.pagination.totalCount ?? 0) / pageSize
+    ),
+    state: {
+      pagination: {
+        pageIndex,
+        pageSize,
+      },
+      sorting: search.sortBy,
+    },
+    getCoreRowModel: getCoreRowModel(),
+    getExpandedRowModel: getExpandedRowModel(),
+    getRowCanExpand: () => true,
+    manualPagination: true,
+  });
+
+  if (isPending) {
+    return <LoadingIndicator />;
+  }
+
+  if (isError) {
+    toast.error('Unable to load data', {
+      description: <ToastError error={error} />,
+      duration: Infinity,
+      cancel: {
+        label: 'Dismiss',
+        onClick: () => {},
+      },
+    });
+    return;
+  }
+
   const totalVulnerabilities = vulnerabilities.pagination.totalCount;
 
   return (
@@ -64,7 +300,28 @@ const OrganizationVulnerabilitiesComponent = () => {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <OrganizationVulnerabilityTable />
+        <DataTable
+          table={table}
+          renderSubComponent={renderSubComponent}
+          setCurrentPageOptions={(currentPage) => {
+            return {
+              search: { ...search, page: currentPage },
+            };
+          }}
+          setPageSizeOptions={(size) => {
+            return {
+              search: { ...search, page: 1, pageSize: size },
+            };
+          }}
+          setSortingOptions={(sortBy) => {
+            return {
+              search: {
+                ...search,
+                sortBy: updateColumnSorting(search.sortBy, sortBy),
+              },
+            };
+          }}
+        />
       </CardContent>
     </Card>
   );
