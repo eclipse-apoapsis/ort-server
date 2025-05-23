4966964c07cc2bae6eea88dde16ca39c4e480030
Sebastian Schuberth <sebastian@doubleopen.org>
2025-05-23 10:52:49 +0200

    chore: Use constructor initialization of specs
    Reduce nesting to improve overview.
    
    Signed-off-by: Sebastian Schuberth <sebastian@doubleopen.org>

--- config/local/src/test/kotlin/LocalConfigFileProviderTest.kt
+++ config/local/src/test/kotlin/LocalConfigFileProviderTest.kt
@@ -35,123 +35,121 @@ import org.eclipse.apoapsis.ortserver.config.local.LocalConfigFileProvider.Compa
 private val CONFIG_PATH = Path("config-file")
 private const val CONTENT = "content"
 
-class LocalConfigFileProviderTest : WordSpec() {
-    init {
-        "create" should {
-            "successfully create a provider instance" {
-                val directory = tempdir()
-                directory.resolve(CONFIG_PATH.path).createNewFile()
+class LocalConfigFileProviderTest : WordSpec({
+    "create" should {
+        "successfully create a provider instance" {
+            val directory = tempdir()
+            directory.resolve(CONFIG_PATH.path).createNewFile()
 
-                val config = ConfigFactory.parseMap(
-                    mapOf(
-                        CONFIG_DIR to directory.absolutePath
-                    )
+            val config = ConfigFactory.parseMap(
+                mapOf(
+                    CONFIG_DIR to directory.absolutePath
                 )
+            )
 
-                val provider = LocalConfigFileProvider.create(config)
+            val provider = LocalConfigFileProvider.create(config)
 
-                provider.contains(Context(""), CONFIG_PATH) shouldBe true
+            provider.contains(Context(""), CONFIG_PATH) shouldBe true
+        }
+    }
+
+    "resolveContext" should {
+        "always resolve to an empty context" {
+            val provider = LocalConfigFileProvider(tempdir())
+
+            provider.resolveContext(Context("")).name shouldBe ""
+            provider.resolveContext(Context("context")).name shouldBe ""
+        }
+    }
+
+    "getFile" should {
+        "successfully provide a file" {
+            val directory = tempdir()
+            val file = directory.resolve(CONFIG_PATH.path)
+            file.writeText(CONTENT)
+            val provider = LocalConfigFileProvider(directory)
+
+            val fileContent = provider.getFile(Context(""), CONFIG_PATH)
+                .bufferedReader(Charsets.UTF_8).use { it.readText() }
+
+            fileContent shouldBe CONTENT
+        }
+
+        "throw an exception if the file cannot be found" {
+            val directory = tempdir()
+            val provider = LocalConfigFileProvider(directory)
+
+            shouldThrow<ConfigException> {
+                provider.getFile(Context(""), CONFIG_PATH)
             }
         }
 
-        "resolveContext" should {
-            "always resolve to an empty context" {
-                val provider = LocalConfigFileProvider(tempdir())
+        "throw an exception if the path refers a directory" {
+            val directory = tempdir()
+            directory.resolve(CONFIG_PATH.path).mkdir()
+            val provider = LocalConfigFileProvider(directory)
 
-                provider.resolveContext(Context("")).name shouldBe ""
-                provider.resolveContext(Context("context")).name shouldBe ""
-            }
-        }
-
-        "getFile" should {
-            "successfully provide a file" {
-                val directory = tempdir()
-                val file = directory.resolve(CONFIG_PATH.path)
-                file.writeText(CONTENT)
-                val provider = LocalConfigFileProvider(directory)
-
-                val fileContent = provider.getFile(Context(""), CONFIG_PATH)
-                    .bufferedReader(Charsets.UTF_8).use { it.readText() }
-
-                fileContent shouldBe CONTENT
-            }
-
-            "throw an exception if the file cannot be found" {
-                val directory = tempdir()
-                val provider = LocalConfigFileProvider(directory)
-
-                shouldThrow<ConfigException> {
-                    provider.getFile(Context(""), CONFIG_PATH)
-                }
-            }
-
-            "throw an exception if the path refers a directory" {
-                val directory = tempdir()
-                directory.resolve(CONFIG_PATH.path).mkdir()
-                val provider = LocalConfigFileProvider(directory)
-
-                shouldThrow<ConfigException> {
-                    provider.getFile(Context(""), CONFIG_PATH)
-                }
-            }
-        }
-
-        "contains" should {
-            "return `true` if the config file is present" {
-                val directory = tempdir()
-                directory.resolve(CONFIG_PATH.path).createNewFile()
-                val provider = LocalConfigFileProvider(directory)
-
-                provider.contains(Context(""), CONFIG_PATH) shouldBe true
-            }
-
-            "return `false` if the path refers to a directory" {
-                val directory = tempdir()
-                directory.resolve(CONFIG_PATH.path).mkdir()
-                val provider = LocalConfigFileProvider(directory)
-
-                provider.contains(Context(""), CONFIG_PATH) shouldBe false
-            }
-
-            "return `false` if a the file cannot be found" {
-                val directory = tempdir()
-                val provider = LocalConfigFileProvider(directory)
-
-                provider.contains(Context(""), CONFIG_PATH) shouldBe false
-            }
-        }
-
-        "listFiles" should {
-            "return a list of files inside a given directory" {
-                val directory = tempdir()
-                val files = listOf("file-1", "file-2", "file-3")
-                files.forEach { directory.resolve(it).createNewFile() }
-                val provider = LocalConfigFileProvider(directory)
-
-                val listFiles = provider.listFiles(Context(""), Path(""))
-
-                val expectedFiles = files.map { Path(directory.resolve(it).absolutePath) }
-                listFiles shouldContainExactlyInAnyOrder expectedFiles
-            }
-
-            "throw an exception if the path does not exist" {
-                val directory = tempdir()
-                val provider = LocalConfigFileProvider(directory)
-
-                shouldThrow<ConfigException> {
-                    provider.listFiles(Context(""), CONFIG_PATH)
-                }
-            }
-
-            "throw an exception if the path does not refer a directory" {
-                val directory = tempdir()
-                directory.resolve(CONFIG_PATH.path).createNewFile()
-                val provider = LocalConfigFileProvider(directory)
-
-                shouldThrow<ConfigException> {
-                    provider.listFiles(Context(""), CONFIG_PATH)
-                }
+            shouldThrow<ConfigException> {
+                provider.getFile(Context(""), CONFIG_PATH)
             }
         }
     }
-}
+
+    "contains" should {
+        "return `true` if the config file is present" {
+            val directory = tempdir()
+            directory.resolve(CONFIG_PATH.path).createNewFile()
+            val provider = LocalConfigFileProvider(directory)
+
+            provider.contains(Context(""), CONFIG_PATH) shouldBe true
+        }
+
+        "return `false` if the path refers to a directory" {
+            val directory = tempdir()
+            directory.resolve(CONFIG_PATH.path).mkdir()
+            val provider = LocalConfigFileProvider(directory)
+
+            provider.contains(Context(""), CONFIG_PATH) shouldBe false
+        }
+
+        "return `false` if a the file cannot be found" {
+            val directory = tempdir()
+            val provider = LocalConfigFileProvider(directory)
+
+            provider.contains(Context(""), CONFIG_PATH) shouldBe false
+        }
+    }
+
+    "listFiles" should {
+        "return a list of files inside a given directory" {
+            val directory = tempdir()
+            val files = listOf("file-1", "file-2", "file-3")
+            files.forEach { directory.resolve(it).createNewFile() }
+            val provider = LocalConfigFileProvider(directory)
+
+            val listFiles = provider.listFiles(Context(""), Path(""))
+
+            val expectedFiles = files.map { Path(directory.resolve(it).absolutePath) }
+            listFiles shouldContainExactlyInAnyOrder expectedFiles
+        }
+
+        "throw an exception if the path does not exist" {
+            val directory = tempdir()
+            val provider = LocalConfigFileProvider(directory)
+
+            shouldThrow<ConfigException> {
+                provider.listFiles(Context(""), CONFIG_PATH)
+            }
+        }
+
+        "throw an exception if the path does not refer a directory" {
+            val directory = tempdir()
+            directory.resolve(CONFIG_PATH.path).createNewFile()
+            val provider = LocalConfigFileProvider(directory)
+
+            shouldThrow<ConfigException> {
+                provider.listFiles(Context(""), CONFIG_PATH)
+            }
+        }
+    }
+})
