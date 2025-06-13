c5206c958df5278032d2539d65bf27402f8e48a9
Martin Nonnenmacher <martin.nonnenmacher@bosch.com>
2025-06-13 13:13:06 +0200

    refactor(ktor-utils): Use Ktor exceptions instead of custom ones
    Use exceptions from Ktor instead of custom exceptions when validating
    request parameters.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@bosch.com>

--- shared/ktor-utils/src/main/kotlin/ParameterHelpers.kt
+++ shared/ktor-utils/src/main/kotlin/ParameterHelpers.kt
@@ -20,20 +20,19 @@
 package org.eclipse.apoapsis.ortserver.shared.ktorutils
 
 import io.ktor.server.application.ApplicationCall
+import io.ktor.server.plugins.MissingRequestParameterException
+import io.ktor.server.plugins.ParameterConversionException
 
 /**
- * Get the parameter from this [ApplicationCall].
+ * Get the parameter from this [ApplicationCall] or throw an exception if the parameter is null.
  */
-fun ApplicationCall.requireParameter(name: String) = requireNotNull(parameters[name]) {
-    "Parameter '$name' cannot be null."
-}
+fun ApplicationCall.requireParameter(name: String) = parameters[name] ?: throw MissingRequestParameterException(name)
 
 /**
- * Get the ID parameter from this [ApplicationCall]. Throw a [UrlPathFormatException] if the parameter is null or not a
- * valid ID.
+ * Get the ID parameter from this [ApplicationCall] or throw an exception if the parameter is null or not a valid ID.
  */
 fun ApplicationCall.requireIdParameter(name: String): Long {
     val id = requireParameter(name).toLongOrNull()
 
-    return if (id != null && id > 0) id else throw UrlPathFormatException("Invalid entity ID: '${parameters[name]}'.")
+    return if (id != null && id > 0) id else throw ParameterConversionException(name, "ID")
 }
