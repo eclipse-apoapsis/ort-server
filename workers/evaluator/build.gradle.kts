8d197e730767c25ae5dcba2a963f7cba8bede1e1
Marcel Bochtler <marcel.bochtler@bosch.com>
2025-07-17 10:04:40 +0200

    chore(compose): Configure debugging options for all workers
    Also expose the debug ports in the Docker compose configuration.
    
    Signed-off-by: Marcel Bochtler <marcel.bochtler@bosch.com>

--- workers/evaluator/build.gradle.kts
+++ workers/evaluator/build.gradle.kts
@@ -82,5 +82,9 @@ jib {
     container {
         mainClass = "org.eclipse.apoapsis.ortserver.workers.evaluator.EntrypointKt"
         creationTime.set("USE_CURRENT_TIMESTAMP")
+
+        if (System.getProperty("idea.active").toBoolean()) {
+            jvmFlags = listOf("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5060")
+        }
     }
 }
