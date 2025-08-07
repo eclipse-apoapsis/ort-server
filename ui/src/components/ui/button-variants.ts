5a73c4feef6d4932d69792d8b86ae3bb5c6ca28f
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2025-08-07 07:40:46 +0300

    feat(ui): Add a new button size variant "narrow"
    This size variant will be mainly used for the sorting and filtering icons
    in table headers, to make the icons stay closer together. This improves
    the UX by more clearly separating table headers from a "grouped" set of
    header actions like sorting and filtering.
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

--- ui/src/components/ui/button-variants.ts
+++ ui/src/components/ui/button-variants.ts
@@ -28,6 +28,7 @@ export const buttonVariants = cva(
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
+        narrow: 'h-8 rounded-md px-1.5 has-[>svg]:px-1',
         sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
         lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
         icon: 'size-9',
