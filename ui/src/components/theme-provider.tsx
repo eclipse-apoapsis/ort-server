32d898393e9c4fe584c83b2db5388c9365f19d81
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2025-10-07 13:49:05 +0300

    refactor(ui): Simplify the theme provider
    Remove an unnecessary type and state from the theme provider. This aligns
    the theme provider with the current shadcn-ui instructions in [1].
    
    [1]: https://ui.shadcn.com/docs/dark-mode/vite
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

--- ui/src/components/theme-provider.tsx
+++ ui/src/components/theme-provider.tsx
@@ -21,8 +21,7 @@ import { useEffect, useState } from 'react';
 
 import { ThemeProviderContext } from '@/components/theme-provider-context';
 
-export type Theme = 'dark' | 'light';
-export type SelectedTheme = 'dark' | 'light' | 'system';
+export type Theme = 'dark' | 'light' | 'system';
 
 type ThemeProviderProps = {
   children: React.ReactNode;
@@ -36,37 +35,33 @@ export function ThemeProvider({
   storageKey = 'vite-ui-theme',
   ...props
 }: ThemeProviderProps) {
-  const [selectedTheme, setSelectedTheme] = useState<SelectedTheme>(
-    () => (localStorage.getItem(storageKey) as SelectedTheme) || defaultTheme
+  const [theme, setTheme] = useState<Theme>(
+    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
   );
-  const [activeTheme, setActiveTheme] = useState<Theme>('light');
 
   useEffect(() => {
     const root = window.document.documentElement;
 
     root.classList.remove('light', 'dark');
 
-    if (selectedTheme === 'system') {
+    if (theme === 'system') {
       const systemTheme = window.matchMedia('(prefers-color-scheme: dark)')
         .matches
         ? 'dark'
         : 'light';
 
       root.classList.add(systemTheme);
-      setActiveTheme(systemTheme);
       return;
     }
 
-    root.classList.add(selectedTheme);
-    setActiveTheme(selectedTheme);
-  }, [selectedTheme]);
+    root.classList.add(theme);
+  }, [theme]);
 
   const value = {
-    activeTheme: activeTheme,
-    selectedTheme,
-    setTheme: (theme: SelectedTheme) => {
+    theme,
+    setTheme: (theme: Theme) => {
       localStorage.setItem(storageKey, theme);
-      setSelectedTheme(theme);
+      setTheme(theme);
     },
   };
 
