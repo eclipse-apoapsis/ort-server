3348b47fe7db1f4aef078155e2bde3644c5372b4
Oliver Heger <oliver.heger@bosch.com>
2025-10-14 14:35:06 +0200

    feat(authorization): Add enum classes representing roles
    These classes define a default set of permissions for the standard roles
    READER, WRITER, and ADMIN on all levels of the hierarchy. They are more
    or less stripped-down versions from the old authorization component.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.com>

---
+++ components/authorization/backend/src/main/kotlin/rights/ProductRole.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2025 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.eclipse.apoapsis.ortserver.components.authorization.rights
+
+/**
+ * This enum contains the available roles for [products][org.eclipse.apoapsis.ortserver.model.Product]. It
+ * maps the permissions available for a product to the default roles [READER], [WRITER], and [ADMIN].
+ */
+enum class ProductRole(
+    override val organizationPermissions: Set<OrganizationPermission> = setOf(OrganizationPermission.READ),
+    override val productPermissions: Set<ProductPermission>,
+    override val repositoryPermissions: Set<RepositoryPermission> = emptySet()
+) : Role {
+    /** A role that grants read permissions for a [org.eclipse.apoapsis.ortserver.model.Product]. */
+    READER(
+        productPermissions = setOf(
+            ProductPermission.READ,
+            ProductPermission.READ_REPOSITORIES
+        )
+    ),
+
+    /** A role that grants write permissions for a [org.eclipse.apoapsis.ortserver.model.Product]. */
+    WRITER(
+        productPermissions = setOf(
+            ProductPermission.READ,
+            ProductPermission.WRITE,
+            ProductPermission.READ_REPOSITORIES,
+            ProductPermission.CREATE_REPOSITORY,
+            ProductPermission.TRIGGER_ORT_RUN
+        )
+    ),
+
+    /**
+     * A role that grants all permissions for a [org.eclipse.apoapsis.ortserver.model.Product]. Note that this
+     * inherits to lower levels in the hierarchy.
+     */
+    ADMIN(
+        productPermissions = ProductPermission.entries.toSet(),
+        repositoryPermissions = RepositoryPermission.entries.toSet()
+    )
+}
---
+++ components/authorization/backend/src/main/kotlin/rights/RepositoryRole.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2025 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.eclipse.apoapsis.ortserver.components.authorization.rights
+
+/**
+ * This enum contains the available roles for [repositories][org.eclipse.apoapsis.ortserver.model.Repository]. It
+ * maps the permissions available for a product to the default roles [READER], [WRITER], and [ADMIN].
+ */
+enum class RepositoryRole(
+    override val organizationPermissions: Set<OrganizationPermission> = setOf(OrganizationPermission.READ),
+    override val productPermissions: Set<ProductPermission> = setOf(ProductPermission.READ),
+    override val repositoryPermissions: Set<RepositoryPermission>
+) : Role {
+    /** A role that grants read permissions for a [org.eclipse.apoapsis.ortserver.model.Repository]. */
+    READER(
+        repositoryPermissions = setOf(
+            RepositoryPermission.READ,
+            RepositoryPermission.READ_ORT_RUNS
+        )
+    ),
+
+    /** A role that grants write permissions for a [org.eclipse.apoapsis.ortserver.model.Repository]. */
+    WRITER(
+        repositoryPermissions = setOf(
+            RepositoryPermission.READ,
+            RepositoryPermission.WRITE,
+            RepositoryPermission.READ_ORT_RUNS,
+            RepositoryPermission.TRIGGER_ORT_RUN
+        )
+    ),
+
+    /** A role that grants all permissions for a [org.eclipse.apoapsis.ortserver.model.Repository]. */
+    ADMIN(
+        repositoryPermissions = RepositoryPermission.entries.toSet()
+    )
+}
