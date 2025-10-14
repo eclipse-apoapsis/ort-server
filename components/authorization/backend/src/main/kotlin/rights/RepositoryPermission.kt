c8e1dfb9c349c94f2a0332419f8fc8db65d7e9f0
Oliver Heger <oliver.heger@bosch.com>
2025-10-14 12:28:17 +0200

    feat(authorization): Add enum classes representing permissions
    These classes define the permissions that can be granted to users on the
    different levels of the hierarchy. They are stripped-down versions from
    the old authorization component and now only contain constants for
    permissions.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.com>

---
+++ components/authorization/backend/src/main/kotlin/rights/RepositoryPermission.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2023 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.eclipse.apoapsis.ortserver.components.authorization.rights
+
+import org.eclipse.apoapsis.ortserver.model.OrtRun
+import org.eclipse.apoapsis.ortserver.model.Repository
+
+/**
+ * This enum contains the available permissions for [repositories][Repository]. These permissions are used by the API to
+ * control access to the [Repository] endpoints.
+ */
+enum class RepositoryPermission {
+    /** Permission to read the [Repository] details. */
+    READ,
+
+    /** Permission to write the [Repository] details. */
+    WRITE,
+
+    /** Permission to write the [Repository] secrets. */
+    WRITE_SECRETS,
+
+    /** Permission to manage [Repository] groups. */
+    MANAGE_GROUPS,
+
+    /** Permission to read the list of [OrtRun]s of the [Repository]. */
+    READ_ORT_RUNS,
+
+    /** Permission to trigger an [OrtRun] for the [Repository]. */
+    TRIGGER_ORT_RUN,
+
+    /** Permission to delete the [Repository]. */
+    DELETE
+}
