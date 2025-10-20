a5d1e9a0a1fb4171fc3172f8c99fd46211c0335b
Oliver Heger <oliver.heger@bosch.com>
2025-10-20 09:11:48 +0200

    feat(authorization): Add `OrtServerPrincipal` class
    This class holds information about the current user and his or her access
    rights. An instance is created during authentication.
    
    Signed-off-by: Oliver Heger <oliver.heger@bosch.com>

---
+++ components/authorization/backend/src/test/kotlin/routes/OrtServerPrincipalTest.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2025 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.eclipse.apoapsis.ortserver.components.authorization.routes
+
+import com.auth0.jwt.interfaces.Payload
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+import io.mockk.every
+import io.mockk.mockk
+
+import org.eclipse.apoapsis.ortserver.components.authorization.rights.EffectiveRole
+
+class OrtServerPrincipalTest : StringSpec({
+    "An instance should be created correctly from a JWT payload" {
+        val userId = "0x93847-973498-734987"
+        val username = "jdoe"
+        val fullName = "John Doe"
+        val payload = mockk<Payload> {
+            every { subject } returns userId
+            every { getClaim("preferred_username").asString() } returns username
+            every { getClaim("name").asString() } returns fullName
+        }
+        val effectiveRole = mockk<EffectiveRole>()
+
+        val principal = OrtServerPrincipal.create(payload, effectiveRole)
+
+        principal.userId shouldBe userId
+        principal.username shouldBe username
+        principal.fullName shouldBe fullName
+        principal.effectiveRole shouldBe effectiveRole
+    }
+})
