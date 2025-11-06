5863fa01a671e04c03753984dcac806532826abc
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2025-11-06 11:35:59 +0200

    chore(ui): Switch a deprecated Vite plugin
    `TanStackRouterVite` plugin is deprecated since the UI project was started
    and configures, so switch it to get rid of "deprecated" warnings [1].
    
    [1]: https://tanstack.com/router/latest/docs/framework/react/installation/with-vite
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -19,13 +19,13 @@
 
 import path from 'path';
 
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
+import { tanstackRouter } from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), tanstackRouter()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
