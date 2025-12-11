d2504dd5205ac726db99c50166e7b89e7bc021a1
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2025-12-11 12:16:00 +0200

    feat(ui): Add a page for searching ORT runs with a package
    The search returns the results as a "column" table, paginated and sortable.
    Some columns act as links.
    
    Add the package search option also to the sidebar menu of a product.
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

---
+++ ui/src/routes/organizations/$orgId/products/$productId/search-package/index.tsx
@@ -0,0 +1,266 @@
+/*
+ * Copyright (C) 2025 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+import { useQuery, useSuspenseQuery } from '@tanstack/react-query';
+import { createFileRoute, Link } from '@tanstack/react-router';
+import {
+  createColumnHelper,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import { useMemo, useState } from 'react';
+import z from 'zod';
+
+import {
+  getRepositoryOptions,
+  getRunsWithPackageOptions,
+} from '@/api/@tanstack/react-query.gen';
+import { RunWithPackage } from '@/api/types.gen';
+import { BreakableString } from '@/components/breakable-string';
+import { DataTable } from '@/components/data-table/data-table';
+import { RegexForm } from '@/components/form/regex-form';
+import { LoadingIndicator } from '@/components/loading-indicator';
+import { TimestampWithUTC } from '@/components/timestamp-with-utc';
+import { ToastError } from '@/components/toast-error';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { updateColumnSorting } from '@/helpers/handle-multisort';
+import { toast } from '@/lib/toast';
+import {
+  paginationSearchParameterSchema,
+  sortingSearchParameterSchema,
+} from '@/schemas';
+
+const defaultPageSize = 10;
+const columnHelper = createColumnHelper<RunWithPackage>();
+
+function SearchPackageComponent() {
+  const params = Route.useParams();
+  const search = Route.useSearch();
+  const [identifier, setIdentifier] = useState('');
+
+  const columns = [
+    columnHelper.accessor('createdAt', {
+      header: 'Created At',
+      cell: ({ row }) => (
+        <TimestampWithUTC timestamp={row.original.createdAt} />
+      ),
+      size: 95,
+    }),
+    columnHelper.accessor('repositoryId', {
+      header: 'Repository',
+      cell: function CellComponent({ row }) {
+        const { data: repo } = useSuspenseQuery({
+          ...getRepositoryOptions({
+            path: { repositoryId: row.original.repositoryId },
+          }),
+        });
+
+        return (
+          <div>
+            <Link
+              className='block text-blue-400 hover:underline'
+              to={
+                '/organizations/$orgId/products/$productId/repositories/$repoId/runs'
+              }
+              params={{
+                orgId: row.original.organizationId.toString(),
+                productId: row.original.productId.toString(),
+                repoId: row.original.repositoryId.toString(),
+              }}
+            >
+              {repo.url}
+            </Link>
+          </div>
+        );
+      },
+    }),
+    columnHelper.accessor('revision', {
+      header: 'Revision',
+      size: 100,
+    }),
+    columnHelper.accessor('ortRunIndex', {
+      header: 'Run Index',
+      size: 50,
+      cell: function CellComponent({ row }) {
+        return (
+          <div>
+            <Link
+              className='block text-blue-400 hover:underline'
+              to={
+                '/organizations/$orgId/products/$productId/repositories/$repoId'
+              }
+              params={{
+                orgId: row.original.organizationId.toString(),
+                productId: row.original.productId.toString(),
+                repoId: row.original.repositoryId.toString(),
+              }}
+            >
+              {row.original.ortRunIndex}
+            </Link>
+          </div>
+        );
+      },
+    }),
+    columnHelper.accessor('packageId', {
+      header: 'Matching Package',
+      cell: function CellComponent({ row }) {
+        return (
+          <Link
+            className='font-semibold text-blue-400 hover:underline'
+            to='/organizations/$orgId/products/$productId/repositories/$repoId/runs/$runIndex/packages'
+            params={{
+              orgId: row.original.organizationId.toString(),
+              productId: row.original.productId.toString(),
+              repoId: row.original.repositoryId.toString(),
+              runIndex: row.original.ortRunIndex.toString(),
+            }}
+            search={{ pkgId: row.original.packageId, marked: '0' }}
+          >
+            <BreakableString text={row.original.packageId} />
+          </Link>
+        );
+      },
+    }),
+  ];
+
+  const pageIndex = useMemo(
+    () => (search.page ? search.page - 1 : 0),
+    [search.page]
+  );
+
+  const pageSize = useMemo(
+    () => (search.pageSize ? search.pageSize : defaultPageSize),
+    [search.pageSize]
+  );
+
+  const sortBy = useMemo(
+    () => (search.sortBy ? search.sortBy : undefined),
+    [search.sortBy]
+  );
+
+  const {
+    data: runsWithPackage,
+    isPending: isRunsPending,
+    isError: isRunsError,
+    error: runsError,
+  } = useQuery({
+    ...getRunsWithPackageOptions({
+      query: {
+        identifier: identifier,
+        productId: Number.parseInt(params.productId),
+      },
+    }),
+    enabled: identifier !== '',
+  });
+
+  const table = useReactTable({
+    data: runsWithPackage || [],
+    columns,
+    state: {
+      pagination: {
+        pageIndex,
+        pageSize,
+      },
+      sorting: sortBy,
+    },
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+  });
+
+  if (isRunsError) {
+    toast.error('Unable to load data', {
+      description: <ToastError error={runsError} />,
+      duration: Infinity,
+      cancel: {
+        label: 'Dismiss',
+        onClick: () => {},
+      },
+    });
+  }
+
+  return (
+    <Card className='h-fit'>
+      <CardHeader>
+        <CardTitle>Search Package</CardTitle>
+        <CardDescription>
+          Search for ORT runs in repositories of this product that contain
+          matching packages.
+        </CardDescription>
+      </CardHeader>
+      <RegexForm
+        className='mx-12'
+        description='You can also use a regular expression in search.'
+        initialValue=''
+        onRegexChange={(regex) => {
+          setIdentifier(regex);
+        }}
+      />
+      <CardContent>
+        {isRunsPending && identifier ? (
+          <LoadingIndicator />
+        ) : (
+          <DataTable
+            table={table}
+            setCurrentPageOptions={(currentPage) => {
+              return {
+                to: Route.to,
+                search: { ...search, page: currentPage },
+              };
+            }}
+            setPageSizeOptions={(size) => {
+              return {
+                to: Route.to,
+                search: { ...search, page: 1, pageSize: size },
+              };
+            }}
+            setSortingOptions={(sortBy) => {
+              return {
+                to: Route.to,
+                search: {
+                  ...search,
+                  sortBy: updateColumnSorting(search.sortBy, sortBy),
+                },
+              };
+            }}
+          />
+        )}
+      </CardContent>
+    </Card>
+  );
+}
+
+export const Route = createFileRoute(
+  '/organizations/$orgId/products/$productId/search-package/'
+)({
+  validateSearch: z.object({
+    ...paginationSearchParameterSchema.shape,
+    ...sortingSearchParameterSchema.shape,
+  }),
+  component: SearchPackageComponent,
+  pendingComponent: LoadingIndicator,
+});
