34045f8333661ca4b5d2cc5a20a26e64677abf97
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2026-03-31 06:43:15 +0300

    feat(resolutions): Define rule violation resolution events
    Define the events required for managing rule violation resolutions. At
    least in the initial implementation, rule violation resolutions
    will only be possible on the level of repositories.
    
    `messageHash` in`RuleViolationResolutionEvent` will act as a stable
    technical identifier of the resolution and is the SHA-1 representation
    of the message.
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

---
+++ components/resolutions/backend/src/main/kotlin/ruleviolations/RuleViolationResolutionEvent.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2026 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.eclipse.apoapsis.ortserver.components.resolutions.ruleviolations
+
+import kotlin.time.Clock
+import kotlin.time.Instant
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+import org.eclipse.apoapsis.ortserver.model.RepositoryId
+import org.eclipse.apoapsis.ortserver.model.runs.repository.RuleViolationResolutionReason
+
+internal data class RuleViolationResolutionEvent(
+    val repositoryId: RepositoryId,
+    val messageHash: String,
+    val version: Long,
+    val payload: RuleViolationResolutionEventPayload,
+    val createdBy: String,
+    val createdAt: Instant = Clock.System.now()
+)
+
+@Serializable
+internal sealed interface RuleViolationResolutionEventPayload
+
+@Serializable
+@SerialName("Created")
+internal class Created(
+    val message: String,
+    val reason: RuleViolationResolutionReason,
+    val comment: String
+) : RuleViolationResolutionEventPayload
+
+@Serializable
+@SerialName("Updated")
+internal class Updated(
+    val reason: RuleViolationResolutionReason?,
+    val comment: String?
+) : RuleViolationResolutionEventPayload
+
+@Serializable
+@SerialName("Deleted")
+internal object Deleted : RuleViolationResolutionEventPayload
