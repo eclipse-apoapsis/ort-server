ef32cab4caf63428fe0be6da03d355b5441eb8f5
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2026-04-02 16:17:54 +0300

    feat(service): Add a service to return dependency graphs
    The graphs are amended with the package IDs instead of numeric indexes.
    Also curated purls are returned for the use of the UI's "show purls instead
    of ORT IDs" user preference.
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

---
+++ components/dependency-graph/backend/build.gradle.kts
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2026 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+plugins {
+    id("ort-server-kotlin-component-backend-conventions")
+    id("ort-server-publication-conventions")
+}
+
+group = "org.eclipse.apoapsis.ortserver.components.dependencygraph"
+
+dependencies {
+    api(projects.components.dependencyGraph.dependencyGraphApiModel)
+
+    implementation(projects.dao)
+    implementation(projects.model)
+
+    implementation(libs.exposedJdbc)
+
+    routesImplementation(projects.components.authorization.authorizationBackend)
+    routesImplementation(projects.components.dependencyGraph.dependencyGraphApiModel)
+    routesImplementation(projects.shared.apiMappings)
+    routesImplementation(projects.shared.apiModel)
+    routesImplementation(projects.shared.ktorUtils)
+
+    routesImplementation(ktorLibs.server.core)
+    routesImplementation(libs.ktorOpenApi)
+
+    testImplementation(testFixtures(projects.dao))
+    testImplementation(testFixtures(projects.shared.ktorUtils))
+
+    testImplementation(ktorLibs.serialization.kotlinx.json)
+    testImplementation(ktorLibs.server.auth)
+    testImplementation(ktorLibs.server.contentNegotiation)
+    testImplementation(ktorLibs.server.statusPages)
+    testImplementation(ktorLibs.server.testHost)
+    testImplementation(libs.kotestAssertionsCore)
+    testImplementation(libs.kotestAssertionsKtor)
+    testImplementation(libs.kotestRunnerJunit5)
+    testImplementation(libs.kotlinxSerializationJson)
+}
