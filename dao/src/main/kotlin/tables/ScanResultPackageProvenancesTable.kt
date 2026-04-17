d12c60bc1777064bc7ecd866bfb723b31ca36786
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2026-04-17 08:03:34 +0300

    feat(dao): Add an Exposed table definition for the junction table
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.org>

---
+++ dao/src/main/kotlin/tables/ScanResultPackageProvenancesTable.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2026 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package org.eclipse.apoapsis.ortserver.dao.tables
+
+import org.jetbrains.exposed.v1.core.ReferenceOption
+import org.jetbrains.exposed.v1.core.Table
+import org.jetbrains.exposed.v1.core.and
+import org.jetbrains.exposed.v1.core.eq
+import org.jetbrains.exposed.v1.jdbc.insert
+import org.jetbrains.exposed.v1.jdbc.selectAll
+
+/**
+ * A junction table to link [ScanResultsTable] with [PackageProvenancesTable].
+ */
+object ScanResultPackageProvenancesTable : Table("scan_result_package_provenances") {
+    val scanResultId = reference("scan_result_id", ScanResultsTable, onDelete = ReferenceOption.CASCADE)
+    val packageProvenanceId =
+        reference("package_provenance_id", PackageProvenancesTable, onDelete = ReferenceOption.CASCADE)
+
+    override val primaryKey: PrimaryKey
+        get() = PrimaryKey(scanResultId, packageProvenanceId, name = "${tableName}_pkey")
+
+    fun insertIfNotExists(scanResultId: Long, packageProvenanceId: Long) {
+        val exists = selectAll().where {
+            ScanResultPackageProvenancesTable.scanResultId eq scanResultId and
+                    (ScanResultPackageProvenancesTable.packageProvenanceId eq packageProvenanceId)
+        }.count() > 0
+
+        if (!exists) {
+            insert {
+                it[ScanResultPackageProvenancesTable.scanResultId] = scanResultId
+                it[ScanResultPackageProvenancesTable.packageProvenanceId] = packageProvenanceId
+            }
+        }
+    }
+}
