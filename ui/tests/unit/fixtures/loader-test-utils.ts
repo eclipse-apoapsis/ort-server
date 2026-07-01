b56d0538df205e7399abf2ce6ece8d0d0b4b467d
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2026-07-01 13:47:37 +0300

    test(ui): Add utilities for new route tests
    To reliably verify fixes, refactors and possible performance optimizations
    for the UI routes, new tests will be needed in `ui/tests/unit/routes`
    directory in future commits.
    
    Add some common utility types and functions to `ui/tests/unit/fixtures`
    which will be used to assist in route testing.
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.io>

---
+++ ui/tests/unit/fixtures/loader-test-utils.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2026 The ORT Server Authors (See <https://github.com/eclipse-apoapsis/ort-server/blob/main/NOTICE>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+type QueryKeyRequest = {
+  _id?: string;
+  path?: Record<string, unknown>;
+  query?: Record<string, unknown>;
+};
+
+type QueryOptionsWithQueryKey = {
+  queryKey: readonly [QueryKeyRequest, ...unknown[]];
+};
+
+export const getQueryKeyRequest = (options: unknown): QueryKeyRequest =>
+  (options as QueryOptionsWithQueryKey).queryKey[0];
+
+export const createDeferred = <T = void>() => {
+  let resolve!: (value: T | PromiseLike<T>) => void;
+  let reject!: (reason?: unknown) => void;
+
+  const promise = new Promise<T>((resolvePromise, rejectPromise) => {
+    resolve = resolvePromise;
+    reject = rejectPromise;
+  });
+
+  return { promise, resolve, reject };
+};
