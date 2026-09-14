f76f23323ffaef507f480a0f5b69912ee811c6d7
Jyrki Keisala <jyrki.keisala@doubleopen.org>
2026-09-14 10:39:36 +0300

    feat(ui): Show role descriptions in the role selector
    Make the user management pages more compact by showing the role
    descriptions in the role drop-down instead of above the form.
    
    Resolves #5963.
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@doubleopen.io>

--- ui/src/routes/organizations/$orgId/products/$productId/users/index.tsx
+++ ui/src/routes/organizations/$orgId/products/$productId/users/index.tsx
@@ -24,7 +24,7 @@ import {
   useSuspenseQuery,
 } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
-import { Eye, Loader2, Pen, Shield } from 'lucide-react';
+import { Loader2 } from 'lucide-react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
@@ -120,24 +120,9 @@ const ManageUsers = () => {
     <Card>
       <CardHeader>
         <CardTitle>Manage Users</CardTitle>
-        <CardDescription className='flex flex-col gap-2'>
-          <span>
-            Assign or remove roles to users in the{' '}
-            <span className='font-semibold'>{product.name}</span> product.
-          </span>
-          <div className='grid grid-cols-[auto_1fr] items-center gap-x-1 gap-y-1'>
-            <Eye size={16} />
-            <span>READERS can view the product and its repositories.</span>
-            <Pen size={16} />
-            <span>
-              WRITERS can view and edit the product and its repositories.
-            </span>
-            <Shield size={16} />
-            <span>
-              ADMINS can view, edit, and delete the product and its
-              repositories.
-            </span>
-          </div>
+        <CardDescription>
+          Assign or remove roles to users in the{' '}
+          <span className='font-semibold'>{product.name}</span> product.
         </CardDescription>
       </CardHeader>
       <CardContent className='space-y-4'>
@@ -166,7 +151,11 @@ const ManageUsers = () => {
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Role</FormLabel>
-                    <RoleSelect value={field.value} onChange={field.onChange} />
+                    <RoleSelect
+                      value={field.value}
+                      onChange={field.onChange}
+                      level='product'
+                    />
                     <FormMessage />
                   </FormItem>
                 )}
